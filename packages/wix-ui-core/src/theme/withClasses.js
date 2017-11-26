c4c4e287dbe69436e0727dbbbc779a5ed9d7db40
Lior Belinsky <liorbe@wix.com>
2017-11-27 00:11:59 +0200

    arr rtl support for components

--- packages/wix-ui-core/src/theme/withClasses.js
+++ packages/wix-ui-core/src/theme/withClasses.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {findDOMNode} from 'react-dom';
 import {object} from 'prop-types';
 import uniqueId from 'lodash/uniqueId';
 import {generateClasses, detachStyleSheetFromDom} from './DOMStyleRenderer';
@@ -13,12 +14,43 @@ export function withClasses(CoreComponent, styles) {
     constructor(props) {
       super(props);
       this.id = uniqueId();
-      this.state = {classes: generateClasses(styles(props.theme), this.id)};
+      this.state = {
+        rtl: false,
+        classes: generateClasses({
+          styles: styles(props.theme),
+          componentId: this.id,
+          rtl: false
+        })
+      };
+    }
+
+    componentDidMount() {
+      let domNode = findDOMNode(this);
+      while (domNode) {
+        if (isContainingRtlClass(domNode)) {
+          this.setState({
+            rtl: true,
+            classes: generateClasses({
+              styles: styles(this.props.theme),
+              componentId: this.id,
+              rtl: true
+            })
+          });
+          return;
+        }
+        domNode = domNode.parentElement;
+      }
     }
 
     componentWillReceiveProps(nextProps) {
       if (this.props.theme !== nextProps.theme) {
-        this.setState({classes: generateClasses(styles(nextProps.theme), this.id)});
+        this.setState({
+          classes: generateClasses({
+            styles: styles(nextProps.theme),
+            componentId: this.id,
+            rtl: this.state.rtl
+          })
+        });
       }
     }
 
@@ -36,4 +68,9 @@ export function withClasses(CoreComponent, styles) {
   }
 
   return ThemedComponent;
+}
+
+function isContainingRtlClass(domNode) {
+  const className = domNode.getAttribute('class');
+  return !!className && className.split(' ').indexOf('rtl') !== -1;
 }
