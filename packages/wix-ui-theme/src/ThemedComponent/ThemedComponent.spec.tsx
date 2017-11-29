cfb17881147d17c13fd23a1b38942656b1b618e8
Lior Belinsky <liorbe@wix.com>
2017-11-30 00:35:20 +0200

    add test for ThemedComponent

--- packages/wix-ui-theme/src/ThemedComponent/ThemedComponent.spec.tsx
+++ packages/wix-ui-theme/src/ThemedComponent/ThemedComponent.spec.tsx
@@ -1,7 +1,81 @@
+import * as React from 'react';
+import {ThemedComponent} from './';
+import {mount} from 'enzyme';
 import {expect} from 'chai';
 
-describe('', () => {
-  it('', () => {
-    expect(true).to.equal(true);
+describe('ThemedComponent', () => {
+
+  it('should pass the calculated theme object to the wrapped component', () => {
+    const Component = ({theme}): any => <div>{theme.color}</div>;
+
+    const wrapper = mount(
+      <ThemedComponent theme={() => ({color: 'green'})}>
+        <Component/>
+      </ThemedComponent>
+    );
+
+    expect(wrapper.html()).to.equal('<div>green</div>');
+  });
+
+  it('should keep the original props of the wrapped component', () => {
+    const Component = ({theme, anotherProp}): any => <div>{anotherProp}</div>;
+
+    const wrapper = mount(
+      <ThemedComponent theme={() => ({color: 'green'})}>
+        <Component anotherProp="Hello"/>
+      </ThemedComponent>
+    );
+
+    expect(wrapper.html()).to.equal('<div>Hello</div>');
+  });
+
+  it('should pass the calculated theme object when the given theme is an object', () => {
+    const Component = ({theme}): any => <div>{theme.color}</div>;
+
+    const wrapper = mount(
+      <ThemedComponent theme={{color: 'green'}}>
+        <Component/>
+      </ThemedComponent>
+    );
+
+    expect(wrapper.html()).to.equal('<div>green</div>');
+  });
+
+  it('should calculate the theme with the additional props that was given to the wrapper', () => {
+    const Component = ({theme}): any => <div>{theme.color}</div>;
+
+    const wrapper = mount(
+      <ThemedComponent theme={({color}) => ({color})} color="green">
+        <Component/>
+      </ThemedComponent>
+    );
+
+    expect(wrapper.html()).to.equal('<div>green</div>');
+  });
+
+  it('should re-calculate the theme when the additional prop for the wrapper changes', () => {
+    const Component = ({theme}): any => <div>{theme.color}</div>;
+
+    const wrapper = mount(
+      <ThemedComponent theme={({color}) => ({color})} color="green">
+        <Component/>
+      </ThemedComponent>
+    );
+
+    wrapper.setProps({color: 'red'});
+    expect(wrapper.html()).to.equal('<div>red</div>');
+  });
+
+  it('should not re-calculate the theme when the component get\'s updated but not due to a theme dependant change', () => {
+    const Component = ({theme, x}): any => <div>{x}</div>;
+
+    const wrapper = mount(
+      <ThemedComponent theme={({color}) => ({color})} color="green">
+        <Component x="green"/>
+      </ThemedComponent>
+    );
+
+    wrapper.setProps({});
+    expect(wrapper.html()).to.equal('<div>green</div>');
   });
 });
