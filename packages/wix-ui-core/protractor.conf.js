b7ccebc141bbcfd83a9c177ab4e63ca361491ee3
Amir Yonatan <amiry@wix.com>
2017-12-13 18:58:26 +0200

    Changed servers

--- packages/wix-ui-core/protractor.conf.js
+++ packages/wix-ui-core/protractor.conf.js
@@ -1,7 +1,15 @@
+const {server} = require('./scripts/e2e-server');
+
 exports.config = {
   baseUrl: `http://localhost:6006/`,
   specs: ['src/**/*.e2e.ts'],
+  beforeLaunch() {
+    server.start();
+  },
   onPrepare() {
     browser.ignoreSynchronization = true;
+  },
+  afterLaunch() {
+    server.stop();
   }
 };
--- packages/wix-ui-core/scripts/e2e-server.js
+++ packages/wix-ui-core/scripts/e2e-server.js
@@ -3,9 +3,17 @@ const app = express();
 
 const port = 6006;
 const storybookPath = './storybook-static';
+let server = app.use('/', express.static(storybookPath));
 
-app
-  .use('/', express.static(storybookPath))
-  .listen(port, () => {
-    console.log(`e2e sandbox running at http://localhost:${port}, serving ${storybookPath}`);
-  });
+module.exports.server = {
+  start: () => {
+    server = server.listen(port, () => {
+      console.log(`e2e sandbox running at http://localhost:${port}, serving ${storybookPath}`);
+    });
+  },
+  stop: () => {
+    server.close(() => {
+      console.log('Finished all requests');
+    });
+  }
+};
