f19c0e4bc8359d1d85ce5d5f4fda737fd401778c
Amir Yonatan <amiry@wix.com>
2017-12-13 19:13:01 +0200

    Adding e2e config

--- packages/wix-ui-santa/protractor.conf.js
+++ packages/wix-ui-santa/protractor.conf.js
@@ -2,7 +2,7 @@ const {server} = require('./scripts/e2e-server');
 
 exports.config = {
   baseUrl: `http://localhost:6006/`,
-  specs: ['src/**/*.e2e.ts'],
+  specs: ['src/**/*.e2e.ts', 'src/**/*.e2e.js'],
   beforeLaunch() {
     server.start();
   },
