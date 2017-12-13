960d028874c444aaa1ad7da7ef526c0c9ac1b05f
Yaron Nachshon <yaronn@wix.com>
2017-12-13 16:58:19 +0200

    chore(Text Core): add tests

---
+++ packages/wix-ui-core/src/components/Text/Text.driver.ts
@@ -0,0 +1,25 @@
+import {DomTestDriver} from './../../DOMStyleRenderer/domTest.driver';
+
+const getClasses = (element) => {
+  const rawCssClasses = element.getAttribute('class');
+  let cssClasses = [];
+  if (rawCssClasses !== null) {
+    cssClasses = element.getAttribute('class').split(' ');
+  }
+  return cssClasses;
+};
+
+export const textDriverFactory = ({element, componentInstance}) => {
+  let domTestDriver = null;
+  if (componentInstance) {
+    domTestDriver =  new DomTestDriver({componentId: componentInstance.id} );
+  }
+
+  return {
+    exists: () => !!element,
+    isEllipsis: className => getClasses(element).indexOf('ellipsis') !== -1,
+    hasTitleAttribute: () => element.getAttribute('title') !== null,
+    getFontFamily: () =>  domTestDriver.getCssValue({className: 'root', property: 'font-family'})
+    getFontFamily: () =>  domTestDriver.getCssValue({className: 'root', property: 'font-family'})
+  };
+};
