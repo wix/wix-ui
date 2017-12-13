50da38dfc44230e651ce00785f43ddec3413e10a
Amir Yonatan <amiry@wix.com>
2017-12-13 12:35:40 +0200

    Remove wallaby

--- packages/wix-ui-core/src/components/Input/Input.e2e.ts
+++ packages/wix-ui-core/src/components/Input/Input.e2e.ts
@@ -3,7 +3,7 @@ import {browser} from 'protractor';
 import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils';
 import {inputTestkitFactory} from '../../testkit/protractor';
 
-describe.skip('Input', () => {
+describe('Input', () => {
   const storyUrl = getStoryUrl('Components', 'Input');
 
   beforeEach(() => {
@@ -11,7 +11,7 @@ describe.skip('Input', () => {
   });
 
   eyes.it('should enter text to inpux', () => {
-    const dataHook = 'story-button-enabled';
+    const dataHook = 'story-input';
     const driver = inputTestkitFactory({dataHook});
 
     waitForVisibilityOf(driver.element(), 'Cannot find Input')
