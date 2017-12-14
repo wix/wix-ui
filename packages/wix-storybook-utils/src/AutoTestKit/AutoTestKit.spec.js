caa6223ddb9c8bd6190aa062653df5582eca88b9
Arnoldas Sescila <arnoldass@wix.com>
2017-12-14 16:28:06 +0200

    Clean up

--- packages/wix-storybook-utils/src/AutoTestKit/AutoTestKit.spec.js
+++ packages/wix-storybook-utils/src/AutoTestKit/AutoTestKit.spec.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {mount} from 'enzyme';
 import fs from 'fs';
 import path from 'path';
-import DriverParser from './DriverParser';
-import ParsedBadgeTestKitJSON from './parsedBadgeTestKit';
+
 import AutoTestKit from './AutoTestKit';
 
-const fakeBadgeTestKitFileName = 'BadgeDriverString.txt';
-const fakeInputTestKitFileName = 'InputDriverString.txt';
+const fakeBadgeTestKitFilePath = 'mock-testkits/BadgeDriverString.txt';
+const fakeInputTestKitFilePath = 'mock-testkits/InputDriverString.txt';
 
 const getFakeTestKitFile = fileName =>
   fs.readFileSync(path.resolve(path.join(__dirname, fileName)), 'utf8');
@@ -33,27 +32,18 @@ const createDriver = wrapper => {
 
 describe('AutoTestKit', () => {
 
-  describe('badge testKit', () => {
-    it('should return exactly the object is expected', () => {
-      const BadgeTestKit = getFakeTestKitFile(fakeBadgeTestKitFileName);
-      const result = new DriverParser(BadgeTestKit).parse();
-
-      expect(result).toEqual(ParsedBadgeTestKitJSON);
-    });
-  });
-
-  describe('badge testKit', () => {
+  describe('Badge testKit', () => {
     it('should have seven methods', () => {
-      const BadgeTestKit = getFakeTestKitFile(fakeBadgeTestKitFileName);
+      const BadgeTestKit = getFakeTestKitFile(fakeBadgeTestKitFilePath);
       const driver = createDriver(render(BadgeTestKit));
       expect(driver.getMethodsCount()).toEqual(7);
       expect(driver.getMethodAt(0).getDescription()).toEqual(' Something  Something ');
     });
   });
 
-  describe('input testKit', () => {
+  describe('Input testKit', () => {
     it('should have fifty methods', () => {
-      const InputTestKit = getFakeTestKitFile(fakeInputTestKitFileName);
+      const InputTestKit = getFakeTestKitFile(fakeInputTestKitFilePath);
       const driver = createDriver(render(InputTestKit));
       expect(driver.getMethodsCount()).toEqual(50);
       expect(driver.getMethodAt(2).getName()).toEqual('blur');
--- packages/wix-storybook-utils/src/AutoTestKit/index.js
+++ packages/wix-storybook-utils/src/AutoTestKit/index.js
@@ -1,13 +1,3 @@
-// const fs = require('fs');
-// const path = require('path');
-// const DriverParser = require('./DriverParser');
-
-// const BadgeTestKit = fs.readFileSync(path.resolve(path.join(__dirname, 'BadgeDriverString.txt')));
-// const InputTestKit = fs.readFileSync(path.resolve(path.join(__dirname, 'InputDriverString.txt')));
-
-// const result = new DriverParser(BadgeTestKit).parse();
-// console.log(JSON.stringify(result, null, 2));
-
 import AutoTestKit from './AutoTestKit';
 
 export default AutoTestKit;
