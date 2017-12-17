2d19b82e384261860aebdb2fea8f7835908a2e76
Lior Belinsky <liorbe@wix.com>
2017-12-17 22:12:01 +0200

    wix-ui-core gw refactor

--- packages/wix-ui-core/src/components/Button/styles.ts
+++ packages/wix-ui-core/src/components/Button/styles.ts
@@ -1,17 +1,18 @@
 import {core, ButtonTheme} from './theme';
 import * as defaultsDeep from 'lodash/defaultsDeep';
 
+const contentSelector = '& [data-class="button-content"]';
+
 export const styles = (theme: ButtonTheme) => {
   theme = (defaultsDeep(theme, core) as ButtonTheme);
 
   return {
     button: {
-      color: theme.color,
-      background: theme.backgroundColor,
-      borderColor: theme.borderColor,
+      minWidth: theme.minWidth,
+      width: theme.width,
       height: theme.height,
-      borderRadius: theme.borderRadius,
       padding: theme.padding,
+      borderRadius: theme.borderRadius,
 
       fontFamily: theme.fontFamily,
       fontSize: theme.fontSize,
@@ -19,6 +20,10 @@ export const styles = (theme: ButtonTheme) => {
       fontStyle: theme.fontStyle,
       fontWeight: theme.fontWeight,
       textDecoration: theme.textDecoration,
+
+      color: theme.color,
+      background: theme.backgroundColor,
+      borderColor: theme.borderColor,
 
       boxSizing: 'border-box',
       '-webkit-font-smoothing': 'antialiased',
@@ -29,16 +34,43 @@ export const styles = (theme: ButtonTheme) => {
 
       transition: 'background-color 100ms linear, border-color 100ms linear, color 100ms linear',
 
+      [contentSelector]: {
+        display: 'block',
+        height: '100%',
+        padding: theme.contentPadding,
+        color: theme.color,
+        textDecoration: theme.textDecoration
+      },
+
       '&:hover': {
         color: theme.hover.color,
         backgroundColor: theme.hover.backgroundColor,
-        borderColor: theme.hover.borderColor
+        borderColor: theme.hover.borderColor,
+
+        [contentSelector]: {
+          color: theme.hover.color
+        }
+      },
+
+      '&:active': {
+        color: theme.active.color,
+        backgroundColor: theme.active.backgroundColor,
+        borderColor: theme.active.borderColor,
+
+        [contentSelector]: {
+          color: theme.active.color
+        }
       },
 
       '&:disabled': {
         pointerEvents: 'none',
+        color: theme.disabled.color,
         backgroundColor: theme.disabled.backgroundColor,
-        borderColor: theme.disabled.borderColor
+        borderColor: theme.disabled.borderColor,
+
+        [contentSelector]: {
+          color: theme.disabled.color
+        }
       }
     }
   };
