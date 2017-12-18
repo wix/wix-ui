d07c5d611d90633ffc4822f423d9763b378bb0da
Rokas Mikalkevičius <rokasm@wix.com>
2017-12-18 15:18:25 +0200

    Add styles

--- packages/wix-ui-core/src/components/Badge/styles.ts
+++ packages/wix-ui-core/src/components/Badge/styles.ts
@@ -1,1 +1,29 @@
-export const styles = () => ({});
+import {core, BadgeTheme} from './theme';
+import * as defaultsDeep from 'lodash/defaultsDeep';
+  
+export const styles = (theme: BadgeTheme) => {
+  theme = (defaultsDeep(theme, core) as BadgeTheme);
+
+  return {
+    button: {
+      minWidth: theme.minWidth,
+      width: theme.width,
+      height: theme.height,
+      padding: theme.padding,
+      borderRadius: theme.borderRadius,
+
+      fontFamily: theme.fontFamily,
+      fontSize: theme.fontSize,
+      lineHeight: theme.lineHeight,
+      fontStyle: theme.fontStyle,
+      fontWeight: theme.fontWeight,
+      textDecoration: theme.textDecoration,
+
+      color: theme.color,
+      background: theme.backgroundColor,
+      borderColor: theme.borderColor,
+
+      '&:hover': theme.hover
+    }
+  };
+};
--- packages/wix-ui-core/src/components/Badge/theme.ts
+++ packages/wix-ui-core/src/components/Badge/theme.ts
@@ -1,30 +1,35 @@
 import {palette} from '../../palette';
 
-type BadgeState = {
-  color?: string;
-  backgroundColor?: string;
-  borderColor?: string;
-}
+type BadgeState = Partial<{
+  color: string;
+  backgroundColor: string;
+  borderColor: string;
+}>;
 
-export type BadgeTheme = BadgeState & {
-  minWidth?: string;
-  width?: string;
-  height?: string;
-  padding?: string;
-  contentPadding?: string;
-  borderRadius?: string;
+export type BadgeTheme = Partial<BadgeState & {
+  minWidth: string;
+  width: string;
+  height: string;
+  padding: string;
+  contentPadding: string;
+  borderRadius: string;
+  border: string;
+  outline: string;
+  boxSizing: string;
 
-  fontFamily?: string;
-  fontSize?: string;
-  lineHeight?: string;
-  fontStyle?: string;
-  fontWeight?: string;
-  textDecoration?: string;
+  fontFamily: string;
+  fontSize: string;
+  lineHeight: string;
+  fontStyle: string;
+  fontWeight: string;
+  textDecoration: string;
+  textAlign: string;
 
-  hover?: BadgeState
-  active?: BadgeState
-  disabled?: BadgeState
-};
+  cursor: string;
+  '-webkit-font-smoothing': string;
+
+  hover: BadgeState;
+}>;
 
 const stateStyle = {
   color: palette.white,
@@ -36,6 +41,9 @@ export const core: BadgeTheme = {
   height: '24px',
   padding: '6px 12px',
   borderRadius: '2px',
+  border: '1px solid',
+  outline: 'none',
+  boxSizing: 'border-box',
 
   fontFamily: `“HelveticaNeueW01-65Medi”, “HelveticaNeueW02-65Medi”, “HelveticaNeueW10-65Medi”, "sans-serif"`,
   fontSize: '10px',
@@ -43,10 +51,11 @@ export const core: BadgeTheme = {
   fontStyle: 'normal',
   fontWeight: 'normal',
   textDecoration: 'none',
+  textAlign: 'center',
+
+  cursor: 'pointer',
+  '-webkit-font-smoothing': 'antialiased',
 
   ...stateStyle,
-
-  hover: stateStyle,
-  disabled: stateStyle,
-  active: stateStyle
+  hover: stateStyle
 };
