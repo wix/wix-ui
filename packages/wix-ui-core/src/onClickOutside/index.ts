1dc9f67db1fa49f8dd31c5d91c9e1298095db11d
Amir Yonatan <amiry@wix.com>
2017-12-24 16:58:55 +0200

    Added outside click

--- packages/wix-ui-core/src/onClickOutside/index.ts
+++ packages/wix-ui-core/src/onClickOutside/index.ts
@@ -1,5 +1,4 @@
 import onClickOutside from 'react-onclickoutside';
 const hoistNonReactStatics = require('hoist-non-react-statics');
 
-const onClickOutsideHOC = Component => hoistNonReactStatics(onClickOutside(Component), Component, {inner: true});
-export default onClickOutsideHOC;
+export default Component => hoistNonReactStatics(onClickOutside(Component), Component, {inner: true});
