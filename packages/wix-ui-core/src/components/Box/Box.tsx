15845c82920bb6b768d5f0960514166b9fd99d2b
Michael Salaverry <michaels@wix.com>
2017-12-31 15:45:45 +0200

    added prop-types

--- packages/wix-ui-core/src/components/Box/Box.tsx
+++ packages/wix-ui-core/src/components/Box/Box.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {object, any} from 'prop-types';
 import {createHOC} from '../../createHOC';
 
 interface BoxProps {
@@ -8,7 +9,17 @@ interface BoxProps {
   };
 }
 
+/**
+ * Box
+ */
 class Box extends React.PureComponent<BoxProps> {
+  static propTypes = {
+    /** any nodes to be rendered */
+    children: any,
+    /** the class being applied to make it a flex box with flex children */
+    classes: object.isRequired
+  };
+
   render() {
     const {children, classes} = this.props;
     return (
