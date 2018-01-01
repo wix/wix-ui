d00da808241094b810c82d5c6ae0f4d9444bfa36
Michael Salaverry <michaels@wix.com>
2018-01-01 13:40:07 +0200

    added tests for spacing - https://github.com/wix/wix-ui/pull/126#discussion_r159144436

--- packages/wix-ui-core/src/components/Box/Box.driver.ts
+++ packages/wix-ui-core/src/components/Box/Box.driver.ts
@@ -1,4 +1,8 @@
-export const boxDriverFactory = ({element, componentInstance}) => {
+export interface BoxDriverFactoryInput {
+  element: HTMLElement;
+  componentInstance: any;
+}
+export const boxDriverFactory = ({element, componentInstance}: BoxDriverFactoryInput) => {
   return {
     /** check if element exists */
     exists: () => !!element,
@@ -6,5 +10,6 @@ export const boxDriverFactory = ({element, componentInstance}) => {
     getFlexDirection: () => window.getComputedStyle(element).flexDirection,
     /** return box item alignment value */
     getAlignment: () => window.getComputedStyle(element).alignItems,
+    getChildStyle: (child: number): CSSStyleDeclaration => window.getComputedStyle(element.children[child])
   };
 };
--- packages/wix-ui-core/src/components/Box/Box.spec.tsx
+++ packages/wix-ui-core/src/components/Box/Box.spec.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {mount} from 'enzyme';
 import {boxDriverFactory} from './Box.driver';
 import {createDriverFactory} from 'wix-ui-test-utils';
+import * as times from 'lodash/times';
 import Box from './';
 
 describe('Box', () => {
@@ -19,11 +20,12 @@ describe('Box', () => {
 
 describe('Box styling', () => {
   const createDriver = createDriverFactory(boxDriverFactory);
+  const boxMaker = (vertical: boolean = false, numBoxes: number = 1) => (<Box theme={{vertical}}>{times(numBoxes, (index) => (<div key={index}>{index}</div>))}</Box>);
   describe('Box default', () => {
-    const defaultBox = (<Box><div>1</div></Box>);
+    const defaultBox = boxMaker(false, 1);
 
     it('with unspecified vertical orientation should be horizontal by default', () => {
-      const driver = createDriver(defaultBox);
+      const driver = createDriver(<Box><div>1</div></Box>);
       expect(driver.getFlexDirection()).toBe('row');
     });
     it('should have alignment bottom by default', () => {
@@ -34,12 +36,24 @@ describe('Box styling', () => {
       const driver = createDriver(<Box theme={{vertical: false}}><div>1</div></Box>);
       expect(driver.getFlexDirection()).toBe('row');
     });
+    it('should have a default spacing of 0', () => {
+      const testBox = boxMaker(false, 2);
+      const driver = createDriver(testBox);
+      const childStyle = driver.getChildStyle(0);
+      expect(childStyle.marginRight).toBe('0px');
+    });
   });
   describe('Box: vertical variant', () => {
-    const defaultVerticalBox = (<Box theme={{vertical: true}}><div>1</div></Box>);
+    const defaultVerticalBox = boxMaker(true, 1);
     it('should use flex direction column', () => {
       const driver = createDriver(defaultVerticalBox);
       expect(driver.getFlexDirection()).toBe('column');
+    });
+    it('should have a default spacing of 20px', () => {
+      const testBox = boxMaker(true, 2);
+      const driver = createDriver(testBox);
+      const childStyle = driver.getChildStyle(0);
+      expect(childStyle.marginBottom).toBe('20px');
     });
   });
 });
