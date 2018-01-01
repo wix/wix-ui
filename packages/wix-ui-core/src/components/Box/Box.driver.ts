a7497e060314e7196e635c5343933898884c310a
Michael Salaverry <michaels@wix.com>
2018-01-01 11:42:37 +0200

    removed dependency on DomTestkit for Box Driver - https://github.com/wix/wix-ui/pull/126#discussion_r159144405

--- packages/wix-ui-core/src/components/Box/Box.driver.ts
+++ packages/wix-ui-core/src/components/Box/Box.driver.ts
@@ -1,17 +1,11 @@
-import {DomTestkit} from 'wix-ui-jss/domTestkit';
-
 export const boxDriverFactory = ({element, componentInstance}) => {
-  let domTestkit = null;
-  if (componentInstance) {
-    domTestkit =  new DomTestkit({componentId: componentInstance.id} );
-  }
 
   return {
     /** check if element exists */
     exists: () => !!element,
     /** return box flex direction value */
-    getFlexDirection: () => domTestkit.getCssValue({className: 'boxRoot', property: 'flex-direction'}),
+    getFlexDirection: () => window.getComputedStyle(element).flexDirection,
     /** return box item alignment value */
-    getAlignment: () => domTestkit.getCssValue({className: 'boxRoot', property: 'align-items'})
+    getAlignment: () => window.getComputedStyle(element).alignItems,
   };
 };
