c3f6bd40261e0735f388c3a1efc561a2f009c19b
Lior Zisman <liorzi@wix.com>
2018-01-01 13:37:37 +0200

    using a more clever solution

--- packages/wix-ui-core/src/components/Popover/index.tsx
+++ packages/wix-ui-core/src/components/Popover/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Manager, Target, Popper, Arrow} from 'react-popper';
 import {bool, string, func} from 'prop-types';
 import PopperJS from 'popper.js';
-import {buildChildrenObject, createComponentThatRendersItsChildren, ElementProps} from '../../utils';
+import {buildChildrenObject, createComponentThatRendersItsChildren} from '../../utils';
 
 export interface PopoverProps {
   shown: boolean;
@@ -10,62 +10,53 @@ export interface PopoverProps {
   onMouseLeave?: React.MouseEventHandler<HTMLDivElement>;
 }
 
-export interface PopoverState {
-  selfShown: boolean;
-}
-
 export interface SharedPopoverProps {
   placement: PopperJS.Placement;
 }
 
-export default class Popover extends React.Component<PopoverProps & SharedPopoverProps, PopoverState> {
+export type PopoverType = React.SFC<PopoverProps & SharedPopoverProps> & {
+  Element?: React.SFC;
+  Content?: React.SFC;
+};
 
-  constructor(props: PopoverProps & SharedPopoverProps) {
-    super(props);
-    this.state = {selfShown: false};
-  }
+const Popover: PopoverType = ({placement, shown, onMouseEnter, onMouseLeave, children}) => {
+  const childrenObject = buildChildrenObject(children, {Element: null, Content: null});
+  return (
+    <Manager
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      style={{display: 'inline-block'}}>
+      <Target data-hook="popover-element">
+        {childrenObject.Element}
+      </Target>
+      {
+        shown &&
+        <Popper data-hook="popover-content" placement={placement}>
+          <Arrow/>
+          {childrenObject.Content}
+        </Popper>
+      }
+    </Manager>
+  );
+};
 
-  static defaultProps = {
-    shown: false,
-    placement: 'auto'
-  };
+Popover.defaultProps = {
+  shown: false,
+  placement: 'auto'
+};
 
-  static propTypes = {
-    /** The location to display the content */
-    placement: string.isRequired,
-    /** Is the popover content shown */
-    shown: bool.isRequired,
-    /** Event handler for onMouseEnter event */
-    onMouseEnter: func,
-    /** Event handler for onMouseLeave event */
-    onMouseLeave: func
-  };
+Popover.propTypes = {
+  /** The location to display the content */
+  placement: string.isRequired,
+  /** Is the popover content shown */
+  shown: bool.isRequired,
+  /** Event handler for onMouseEnter event */
+  onMouseEnter: func,
+  /** Event handler for onMouseLeave event */
+  onMouseLeave: func
+};
 
-  static Element: React.SFC<ElementProps> = createComponentThatRendersItsChildren('Popover.Element');
-  static Content: React.SFC<ElementProps> = createComponentThatRendersItsChildren('Popover.Content');
+Popover.Element = createComponentThatRendersItsChildren('Popover.Element');
+Popover.Content = createComponentThatRendersItsChildren('Popover.Content');
 
-  render() {
-    const {placement, shown, onMouseEnter, onMouseLeave, children} = this.props;
-    const childrenObject = buildChildrenObject(children, {Element: null, Content: null});
-
-    return (
-      <Manager
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        style={{display: 'inline-block'}}>
-        <Target data-hook="popover-element">
-          {childrenObject.Element}
-        </Target>
-        {
-          (shown || this.state.selfShown) &&
-            <Popper data-hook="popover-content" placement={placement}
-                onMouseEnter={() => this.setState({selfShown: true})}
-                onMouseLeave={() => this.setState({selfShown: false})}>
-              <Arrow/>
-              {childrenObject.Content}
-            </Popper>
-        }
-      </Manager>
-    );
-  }
-}
+export default Popover;
