8d673be4488119a0aee01b6fb0d86408b1cb21f2
Ronen Stern <ronens@wix.com>
2018-01-03 12:30:54 +0200

    wix-ui-icons: Add new icons

---
+++ packages/wix-ui-icons/stories/Icons/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import Markdown from 'wix-storybook-utils/Markdown';
+import CodeExample from 'wix-storybook-utils/CodeExample';
+
+import Readme from '../../src/Icons/README.md';
+
+import AllIcons from './AllIcons';
+import AllIconsRaw from '!raw-loader!./AllIcons';
+
+import CustomeIcon from './CustomIcon';
+import CustomeIconRaw from '!raw-loader!./CustomIcon';
+
+import RTLIcon from './RTLIcon';
+import RTLIconRaw from '!raw-loader!./RTLIcon';
+
+storiesOf('Common', module)
+  .add('Icons', () => (
+    <div>
+      <Markdown source={Readme}/>
+
+      <CodeExample title="All Icons" code={AllIconsRaw}>
+        <AllIcons/>
+      </CodeExample>
+      <CodeExample title="Custom Icon" code={CustomeIconRaw}>
+        <CustomeIcon/>
+      </CodeExample>
+      <CodeExample title="RTL Icon" code={RTLIconRaw}>
+        <RTLIcon/>
+      </CodeExample>
+    </div>
+  ));
---
+++ packages/wix-ui-icons/stories/index.tsx
@@ -0,0 +1,12 @@
+// import * as React from 'react';
+// import {storiesOf} from '@storybook/react';
+// import Hello from '../src/Hello';
+//
+// storiesOf('Hello', module)
+//   .add('default view', () => (
+//     <Hello name="world"/>
+//   ))
+//   .add('with emojis', () => (
+//     <Hello name="😀 😎 👍"/>
+//   ));
+import './Icons';
