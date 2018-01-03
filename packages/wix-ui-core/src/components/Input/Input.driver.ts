da287d1f4a7548004afaec437c59c3d6f7aab705
Lior Belinsky <liorbe@wix.com>
2018-01-03 17:48:14 +0200

    Update Input.driver.ts

--- packages/wix-ui-core/src/components/Input/Input.driver.ts
+++ packages/wix-ui-core/src/components/Input/Input.driver.ts
@@ -1,25 +1,22 @@
-export const inputDriverFactory = ({element, eventTrigger}) => {
-  return {
-    exists: () => !!element,
-    isDisabled: () => element.disabled,
-    isFocus: () => document.activeElement === element,
-    isReadOnly: () => element.readOnly,
-    getMaxLength: () => element.maxLength,
-    getPlaceholder: () => element.placeholder,
-    getAutocomplete: () => element.getAttribute('autocomplete'),
-    isRequired: () => element.required,
-    blur: () => eventTrigger.blur(element),
-    click: () => eventTrigger.click(element),
-    doubleClick: () => eventTrigger.doubleClick(element),
-    focus: () => eventTrigger.focus(element),
-    keyDown: () => eventTrigger.keyDown(element),
-    keyUp: () => eventTrigger.keyUp(element),
-    getTabIndex: () => element.tabIndex,
-    getType: () => element.type,
-    getValue: () => element.value,
-    setValue: value => {
-      element.value = value;
-      eventTrigger.change(element);
-    }
-  };
-};
+export const inputDriverFactory = ({element, eventTrigger}) => ({
+  exists: () => !!element,
+  isDisabled: () => element.disabled,
+  isFocus: () => document.activeElement === element,
+  isReadOnly: () => element.readOnly,
+  getPlaceholder: () => element.placeholder,
+  getAutocomplete: () => element.getAttribute('autocomplete'),
+  isRequired: () => element.required,
+  blur: () => eventTrigger.blur(element),
+  click: () => eventTrigger.click(element),
+  doubleClick: () => eventTrigger.doubleClick(element),
+  focus: () => eventTrigger.focus(element),
+  keyDown: () => eventTrigger.keyDown(element),
+  keyUp: () => eventTrigger.keyUp(element),
+  getTabIndex: () => element.tabIndex,
+  getType: () => element.type,
+  getValue: () => element.value,
+  setValue: value => {
+    element.value = value;
+    eventTrigger.change(element);
+  }
+});
