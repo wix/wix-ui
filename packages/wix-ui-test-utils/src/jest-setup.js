cb8526402d726d3e0736b5f0eed75d76ecd3dd7d
Eran Shabi <eran514@gmail.com>
2018-01-14 11:01:52 +0200

    fix imports

--- packages/wix-ui-test-utils/src/jest-setup.js
+++ packages/wix-ui-test-utils/src/jest-setup.js
@@ -1,7 +1,7 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-15');
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({adapter: new Adapter()});
 
 const originConsoleError = console.error;
 let errors = [];
