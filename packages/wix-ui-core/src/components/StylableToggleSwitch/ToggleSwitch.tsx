5c28914ec3c6390e326067b690a6136548b63d15
Lior Belinsky <liorbe@wix.com>
2018-01-15 18:58:59 +0200

    Update ToggleSwitch.tsx

--- packages/wix-ui-core/src/components/StylableToggleSwitch/ToggleSwitch.tsx
+++ packages/wix-ui-core/src/components/StylableToggleSwitch/ToggleSwitch.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {bool, func, string} from 'prop-types';
 import * as uniqueId from 'lodash/uniqueId';
-import {bool, func, object, string} from 'prop-types';
 import {getViewBox, getPathDescription} from '../ToggleSwitch/utils';
 import style from './ToggleSwitch.st.css';
 
