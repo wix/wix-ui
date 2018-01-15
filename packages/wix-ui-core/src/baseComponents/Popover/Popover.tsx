bee714aebbfe76b202e3feef35ecbe2693b0f9b9
Amir Yonatan <amiry@wix.com>
2018-01-15 17:52:32 +0200

    Convert popover to stylable

--- packages/wix-ui-core/src/baseComponents/Popover/Popover.tsx
+++ packages/wix-ui-core/src/baseComponents/Popover/Popover.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
 import PopperJS from 'popper.js';
 import {Manager, Target, Popper, Arrow} from 'react-popper';
-import {bool, string, func} from 'prop-types';
 import {buildChildrenObject, createComponentThatRendersItsChildren} from '../../utils';
-import {createHOC} from '../../createHOC';
-
-export type PopoverClasses = {
-  popoverContent: string;
-  arrow: string;
-};
+import style from './Popover.st.css';
 
 export type Placement = PopperJS.Placement;
 
@@ -17,7 +11,6 @@ export interface PopoverProps {
   shown: boolean;
   onMouseEnter?: React.MouseEventHandler<HTMLDivElement>;
   onMouseLeave?: React.MouseEventHandler<HTMLDivElement>;
-  classes?: PopoverClasses;
   arrowStyle?: string;
   showArrow?: boolean;
 }
@@ -27,22 +20,22 @@ export type PopoverType = React.SFC<PopoverProps> & {
   Content?: React.SFC;
 };
 
-const Popover: PopoverType =
-  ({placement, shown, onMouseEnter, onMouseLeave, children, arrowStyle, classes, showArrow}) => {
-
+export const Popover: PopoverType = (props) => {
+  const {placement, shown, onMouseEnter, onMouseLeave, children, arrowStyle, showArrow} = props;
   const childrenObject = buildChildrenObject(children, {Element: null, Content: null});
+
   return (
     <Manager
+      {...style('root', {}, props)}
       onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      style={{display: 'inline-block', position: 'relative'}}>
+      onMouseLeave={onMouseLeave}>
       <Target data-hook="popover-element">
         {childrenObject.Element}
       </Target>
       {
         shown &&
-          <Popper data-hook="popover-content" placement={placement} className={classes.popoverContent}>
-            {showArrow && <Arrow data-hook="popover-arrow" className={`${classes.arrow} ${arrowStyle}`}/>}
+          <Popper data-hook="popover-content" placement={placement} className={style.popoverContent}>
+            {showArrow && <Arrow data-hook="popover-arrow" className={`${style.arrow} ${arrowStyle}`}/>}
             {childrenObject.Content}
           </Popper>
       }
@@ -54,20 +47,5 @@ Popover.defaultProps = {
   placement: 'auto'
 };
 
-Popover.propTypes = {
-  /** The location to display the content */
-  placement: string,
-  /** Is the popover content shown */
-  shown: bool,
-  /** Event handler for onMouseEnter event */
-  onMouseEnter: func,
-  /** Event handler for onMouseLeave event */
-  onMouseLeave: func,
-  /** Should display arrow with the content */
-  showArrow: bool
-};
-
 Popover.Element = createComponentThatRendersItsChildren('Popover.Element');
 Popover.Content = createComponentThatRendersItsChildren('Popover.Content');
-
-export default createHOC(Popover);
