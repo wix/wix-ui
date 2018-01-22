293727f10d49743e88671a98743c14c30189144e
Amir Yonatan <amiry@wix.com>
2018-01-22 11:25:24 +0200

    Convert divider to stylable

--- packages/wix-ui-core/src/components/Divider/Divider.tsx
+++ packages/wix-ui-core/src/components/Divider/Divider.tsx
@@ -1,32 +1,26 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
-import {object, bool, any} from 'prop-types';
-import {createHOC} from '../../createHOC';
-
-export interface DividerClasses {
-    divider: string;
-    vertical: string;
-}
+import style from './DividerStyle.st.css';
+import {bool, any} from 'prop-types';
 
 export interface DividerProps {
-    classes?: DividerClasses;
     vertical?: boolean;
     children?: any;
 }
 
-const Divider: React.SFC<DividerProps> = ({classes, children, vertical}) => (
+/**
+ * Divider
+ */
+export const Divider: React.SFC<DividerProps> = ({children, vertical}) => (
     children ?
         <div>{children}</div> :
-        <div className={classNames(classes.divider, {[classes.vertical]: vertical})} />
+        <div className={classNames(style.divider, {[style.vertical]: vertical})} />
 );
 
+Divider.displayName = 'Divider';
 Divider.propTypes = {
-    /** The classes used to style the Divider */
-    classes: object.isRequired,
     /** Is the direction of the divider vertical */
     vertical: bool,
     /** Optional custom divider */
     children: any
 };
-
-export default createHOC(Divider);
