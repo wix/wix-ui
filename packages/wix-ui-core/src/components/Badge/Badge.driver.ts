ff67119f0e1a300a644b18927a8393c9c08fb502
Shlomi Toussia-Cohen <shlomitc@wix.com>
2018-01-23 10:39:42 +0200

    minor

--- packages/wix-ui-core/src/components/Badge/Badge.driver.ts
+++ packages/wix-ui-core/src/components/Badge/Badge.driver.ts
@@ -5,7 +5,6 @@ export const badgeDriverFactory = ({element}) => {
     /** returns elements type attribute */
     getType: () => element.getAttribute('type'),
     /** returns elements innerHtml */
-    getContent: () => element.innerHTML,
-    /** styles element for css properties */
+    getContent: () => element.innerHTML
   };
 };
