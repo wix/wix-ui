d790db86b4f3d93896eae8ab9ddbe3dba66175c7
Amir Yonatan <amiry@wix.com>
2018-01-24 10:27:41 +0200

    Fixed errors

--- packages/wix-ui-test-utils/src/index.ts
+++ packages/wix-ui-test-utils/src/index.ts
@@ -3,7 +3,7 @@ export {isClassExists, makeControlled, sleep} from './helpers';
 export {getStoryUrl, scrollToElement, waitForVisibilityOf} from './protractor-helpers';
 
 export {testkitFactoryCreator, isTestkitExists} from './testkit-helpers/vanilla';
-export {enzymeTestkitFactoryCreator, isEnzymeTestkitExists} from './testkit-helpers/enzyme';
+export {enzymeTestkitFactoryCreator, isEnzymeTestkitExists, WrapperData} from './testkit-helpers/enzyme';
 export {protractorTestkitFactoryCreator} from './testkit-helpers/protractor';
 export {default as baseProtractorConfig} from './config/protractor.conf';
 export * from 'react-dom/test-utils';
