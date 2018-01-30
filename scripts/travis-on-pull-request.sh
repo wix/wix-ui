1a92f9f150b76cab01a69af3411c2de4e178ff75
OmerB <omerb@wix.com>
2018-01-30 14:26:51 +0200

    another test

--- scripts/travis-on-pull-request.sh
+++ scripts/travis-on-pull-request.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 echo 'started storybook copy'
-echo 'this is the current working directory:'
-pwd
 echo $greet
-cd /home/travis/build/wix/wix-ui/packages/wix-ui-core
+cd /home/travis/build/wix/wix-ui/packages/wix-ui-core/
 pwd
-ls -l | grep temp
 mkdir temp
 cp -R storybook-static/* temp
+echo ls -l | grep temp
 echo 'finished storybook copy'
