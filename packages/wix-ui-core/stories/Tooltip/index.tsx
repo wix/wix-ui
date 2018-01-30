74c6330d26f79bf8e231d476c906afddc78f69b6
Lior Zisman <liorzi@wix.com>
2018-01-30 15:02:45 +0200

    working2

--- packages/wix-ui-core/stories/Tooltip/index.tsx
+++ packages/wix-ui-core/stories/Tooltip/index.tsx
@@ -4,9 +4,7 @@ import createStory from '../create-story';
 import {Tooltip} from '../../src/components/Tooltip';
 import * as TooltipSource from '!raw-loader!../../src/components/Tooltip/index.tsx';
 
-export const story = () => {
-  debugger;
-  return createStory({
+export const story = () => createStory({
     category: 'Components',
     name: 'Tooltip',
     storyName: 'Tooltip',
@@ -19,4 +17,3 @@ export const story = () => {
     }),
     source: TooltipSource
   });
-}
