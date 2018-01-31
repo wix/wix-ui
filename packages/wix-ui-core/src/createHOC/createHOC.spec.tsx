e906dc3120d1914bfb139cf08f1fbb8dca51043f
guybe <guybe@wix.com>
2018-01-31 14:21:08 +0200

    wix-ui-core: changing createHOC to use hoist-non-react-methods instead of hoise-non-react-statics

--- packages/wix-ui-core/src/createHOC/createHOC.spec.tsx
+++ packages/wix-ui-core/src/createHOC/createHOC.spec.tsx
@@ -1,37 +1,146 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import {createHOC} from './';
+import {renderIntoDocument, findRenderedComponentWithType} from 'react-dom/test-utils';
 import {mount} from 'enzyme';
 
 describe('createHOC function', () => {
 
   type ComponentProps = {
-    children?: any,
+    id?: string,
     dataClass?: string,
     dataHook?: string
   };
 
-  const Component: React.SFC<ComponentProps> = ({children}) => React.createElement('div', {children});
+  // Regular component with state
+  class ChildComponent extends React.Component<ComponentProps, {id: string}> {
+    constructor(props) {
+      super(props);
+      this.state = {id : props.id};
+      this.boundMethod = this.boundMethod.bind(this);
+    }
 
-  const HOCComponent = createHOC(Component);
+    static staticVariable = 'staticVariable';
+    static staticMethod() { return 'staticMethod'; }
+    unboundMethod() { return 'unboundMethod'; }
+    boundMethod() { return this.state.id; }
+    render() { return <div>Hello</div>; }
+  }
 
-  const render = (Comp: any) => mount(Comp, {attachTo: document.createElement('div')});
+  // Pure component without state
+  class PureChildComponent extends React.PureComponent<ComponentProps> {
+    id: string;
 
-  let wrapper;
+    constructor(props) {
+      super(props);
+      this.id = props.id;
+      this.boundMethod = this.boundMethod.bind(this);
+    }
 
-  afterEach(() => wrapper.detach());
+    static staticVariable = 'staticVariable';
+    static staticMethod() { return 'staticMethod'; }
+    unboundMethod() { return 'unboundMethod'; }
+    boundMethod() { return this.id; }
+    render() { return <div>Hello</div>; }
+  }
 
-  it('should render the wrapped component', () => {
-    wrapper = render(<HOCComponent>Hello</HOCComponent>);
-    expect(wrapper.html()).toBe('<div>Hello</div>');
+  // Stateless component
+  const StatelessChildComponent = () => (<div>stateless</div>);
+
+  describe('Regular components', () => {
+    const HOCComponent = createHOC(ChildComponent);
+
+    it('should render the wrapped component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent/>);
+      expect(findRenderedComponentWithType(wrapper, ChildComponent)).toBeTruthy();
+    });
+
+    it('should place data-hook on the root of the component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent dataHook="my-data-hook"/>);
+      expect(ReactDOM.findDOMNode(wrapper).getAttribute('data-hook')).toBe('my-data-hook');
+    });
+
+    it('should place data-class on the root of the component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent dataClass="my-data-class"/>);
+      expect(ReactDOM.findDOMNode(wrapper).getAttribute('data-class')).toBe('my-data-class');
+    });
+
+    describe('hoisting', () => {
+      it('should hoist static methods', () => {
+        expect(HOCComponent.staticMethod).toBeDefined();
+        expect(HOCComponent.staticMethod()).toEqual('staticMethod');
+      });
+
+      it('should hoist static variables', () => {
+        expect(HOCComponent.staticVariable).toEqual('staticVariable');
+      });
+
+      it('should hoist prototype methods from child to HOC and bind them', () => {
+        const wrapper = renderIntoDocument(<HOCComponent id="some_id"/>);
+        expect(wrapper.unboundMethod).toBeDefined();
+        expect(wrapper.unboundMethod()).toEqual('unboundMethod');
+        expect(wrapper.boundMethod).toBeDefined();
+        expect(wrapper.boundMethod()).toEqual('some_id');
+      });
+    });
   });
 
-  it('should place data-hook on the root of the component', () => {
-    wrapper = render(<HOCComponent dataHook="my-data">Hello</HOCComponent>);
-    expect(wrapper.getDOMNode().getAttribute('data-hook')).toBe('my-data');
+  describe('Pure components', () => {
+    const HOCComponent = createHOC(PureChildComponent);
+
+    it('should render the wrapped component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent />);
+      expect(findRenderedComponentWithType(wrapper, PureChildComponent)).toBeTruthy();
+    });
+
+    it('should place data-hook on the root of the component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent dataHook="my-data-hook" />);
+      expect(ReactDOM.findDOMNode(wrapper).getAttribute('data-hook')).toBe('my-data-hook');
+    });
+
+    it('should place data-class on the root of the component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent dataClass="my-data-class" />);
+      expect(ReactDOM.findDOMNode(wrapper).getAttribute('data-class')).toBe('my-data-class');
+    });
+
+    describe('hoisting', () => {
+      it('should hoist static methods', () => {
+        expect(HOCComponent.staticMethod).toBeDefined();
+        expect(HOCComponent.staticMethod()).toEqual('staticMethod');
+      });
+
+      it('should hoist static variables', () => {
+        expect(HOCComponent.staticVariable).toEqual('staticVariable');
+      });
+
+      it('should hoist prototype methods from child to HOC and bind them', () => {
+        const wrapper = renderIntoDocument(<HOCComponent id="some_id" />);
+        expect(wrapper.unboundMethod).toBeDefined();
+        expect(wrapper.unboundMethod()).toEqual('unboundMethod');
+        expect(wrapper.boundMethod).toBeDefined();
+        expect(wrapper.boundMethod()).toEqual('some_id');
+      });
+    });
   });
 
-  it('should place data-class on the root of the component', () => {
-    wrapper = render(<HOCComponent dataClass="my-data">Hello</HOCComponent>);
-    expect(wrapper.getDOMNode().getAttribute('data-class')).toBe('my-data');
+  describe('Stateless components', () => {
+    const HOCComponent = createHOC(StatelessChildComponent);
+
+    it('should render the wrapped component', () => {
+      const wrapper = mount(<HOCComponent />);
+      expect(wrapper.containsMatchingElement(<StatelessChildComponent/>)).toBeTruthy();
+    });
+
+    it('should place data-hook on the root of the component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent dataHook="my-data-hook" />);
+      expect(ReactDOM.findDOMNode(wrapper).getAttribute('data-hook')).toBe('my-data-hook');
+    });
+
+    it('should place data-class on the root of the component', () => {
+      const wrapper = renderIntoDocument(<HOCComponent dataClass="my-data-class" />);
+      expect(ReactDOM.findDOMNode(wrapper).getAttribute('data-class')).toBe('my-data-class');
+    });
+
+    // Nothing to hoist on stateless components
   });
 });
