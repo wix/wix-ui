6fec594414b8d9c23263b76fbd813b0502e68cb5
Kieran Williams <kieranw@wix.com>
2018-03-18 16:40:48 +0200

    Made VBox story nicer

--- packages/wix-ui-core/stories/VBox/VBox-story.tsx
+++ packages/wix-ui-core/stories/VBox/VBox-story.tsx
@@ -5,21 +5,56 @@ import commonStyle from '../../src/components/StylableVBox/VBoxStyle.st.css';
 export class VBoxStory extends React.Component<{}, {}> {
   render() {
     return (
-      <div style={{width: '50px', border: '2px solid green'}}>
-        <VBox
-          {...commonStyle('root') }
-          data-hook="storybook-vbox"
-          horizontalAlignment="center"
-          spacing={100}
-        >
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
-        </VBox>
+      <div>
+        <h1>VBox with center alignment and 100px spacing</h1>
+        <div style={{width: '300px', border: '2px solid green'}}>
+          <VBox
+            {...commonStyle('root') }
+            data-hook="storybook-vbox"
+            horizontalAlignment="center"
+            spacing={100}
+          >
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+          </VBox>
+        </div>
+        <h1>VBox with left alignment and 50px spacing</h1>
+        <div style={{width: '300px', border: '2px solid green'}}>
+          <VBox
+            {...commonStyle('root') }
+            horizontalAlignment="left"
+            spacing={50}
+          >
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+          </VBox>
+        </div>
+        <h1>VBox with right alignment and 20px spacing</h1>
+        <div style={{width: '300px', border: '2px solid green'}}>
+          <VBox
+            {...commonStyle('root') }
+            horizontalAlignment="right"
+            spacing={20}
+          >
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+            <div style={{height: '50px', width: '50px', backgroundColor: 'lightgrey'}}>hello</div>
+          </VBox>
+        </div>
       </div>
     );
   }
