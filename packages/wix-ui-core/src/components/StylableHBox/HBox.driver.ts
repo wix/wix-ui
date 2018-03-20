d542ee7bb8845fb98019f6b09b91601038d46b34
Kieran Williams <kieranw@wix.com>
2018-03-20 10:50:01 +0200

    Removed style file

--- packages/wix-ui-core/src/components/StylableHBox/HBox.driver.ts
+++ packages/wix-ui-core/src/components/StylableHBox/HBox.driver.ts
@@ -1,9 +1,4 @@
-import {StylableDOMUtil} from 'stylable/test-utils';
-import style from './HBox.st.css';
-
 export const hboxDriverFactory = ({element}) => {
-  const stylableDOMUtil = new StylableDOMUtil(style);
-
   return {
     /** check if element exists */
     exists: () => !!element,
