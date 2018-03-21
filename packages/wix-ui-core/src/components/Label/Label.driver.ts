01c042dc0f6330bb9fde3bf4aebab4c398d818b9
Kieran Williams <kieranw@wix.com>
2018-03-21 14:51:31 +0200

    Changed driver method name

--- packages/wix-ui-core/src/components/Label/Label.driver.ts
+++ packages/wix-ui-core/src/components/Label/Label.driver.ts
@@ -2,8 +2,8 @@ export const labelDriverFactory = ({element}) => {
   return {
     /** check if element exists */
     exists: () => !!element,
-    /** get the rendered content */
-    getChildren: () => element.innerHTML,
+    /** get the label's text */
+    getLabelText: () => element.innerHTML,
     /** get the id of the component */
     getId: () => element.getAttribute('id'),
     /** get the "for" attribute of the component */
