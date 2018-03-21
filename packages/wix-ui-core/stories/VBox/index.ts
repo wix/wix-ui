b0d1f6b1f451c25a04bd59b95c939304e9627a7a
Kieran Williams <kieranw@wix.com>
2018-03-21 11:37:36 +0200

    Improved story and added better styling

--- packages/wix-ui-core/stories/VBox/index.ts
+++ packages/wix-ui-core/stories/VBox/index.ts
@@ -1,8 +1,11 @@
-import 'react';
+import * as React from 'react';
 import createStory from '../create-story';
 
 import {VBox} from '../../src/components/StylableVBox';
 import * as VBoxSource from '!raw-loader!../../src/components/StylableVBox/VBox.tsx';
+import style from './style.st.css';
+
+const children = new Array(5).fill(undefined).map(() => React.createElement('div', {}, 'hello'));
 
 export const story = () => createStory({
   category: 'Components',
@@ -10,8 +13,9 @@ export const story = () => createStory({
   storyName: 'StylableVBox',
   component: VBox,
   componentProps: (setState) => ({
+    ...style('root'),
     'data-hook': 'storybook-vbox',
-    children: '<div>Hello</div><div>hello</div>'
+    children
   }),
   source: VBoxSource
 });
