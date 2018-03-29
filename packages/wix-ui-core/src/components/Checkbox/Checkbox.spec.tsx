69468c197df6420c03f33bd1ee5376f0d7d0b6fa
Daniel Strokovsky <danielstr@wix.com>
2018-03-29 15:01:49 +0300

    removed commented test

--- packages/wix-ui-core/src/components/Checkbox/Checkbox.spec.tsx
+++ packages/wix-ui-core/src/components/Checkbox/Checkbox.spec.tsx
@@ -50,16 +50,6 @@ describe('Checkbox', () => {
       expect(onChange).toHaveBeenCalledWith(expect.objectContaining({checked: true}));
     });
 
-    // it('calls onChange when spacebar is passed', () => {
-    //   const onChange = jest.fn();
-    //   const checkbox = createDriver(<Checkbox onChange={onChange}/>);
-
-    //   checkbox.keyDown(' ');
-
-    //   expect(onChange).toHaveBeenCalledTimes(1);
-    //   expect(onChange).toHaveBeenCalledWith(expect.objectContaining({checked: true}));
-    // });
-
     it('displays the given custom tick icon when checked', () => {
       const checkbox = createDriver(
         <Checkbox
