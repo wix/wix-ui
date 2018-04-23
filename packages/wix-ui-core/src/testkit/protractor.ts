0a199ceb55e3a9b2f3d4e5ecbb53db57ec672aaf
Erez Makavy <erezm@wix.com>
2018-04-23 09:38:37 +0300

    Add typescript types for all protractor drivers

--- packages/wix-ui-core/src/testkit/protractor.ts
+++ packages/wix-ui-core/src/testkit/protractor.ts
@@ -1,66 +1,66 @@
 import {protractorTestkitFactoryCreator} from 'wix-ui-test-utils/protractor';
 
 //JSS
-import {popoverDriverFactory} from '../baseComponents/Popover/Popover.protractor.driver';
+import {popoverDriverFactory, PopoverDriver} from '../baseComponents/Popover/Popover.protractor.driver';
 export const popoverTestkitFactory = protractorTestkitFactoryCreator(popoverDriverFactory);
 
-import {inputDriverFactory} from '../components/Input/Input.protractor.driver';
+import {inputDriverFactory, InputDriver} from '../components/Input/Input.protractor.driver';
 export const inputTestkitFactory = protractorTestkitFactoryCreator(inputDriverFactory);
 
-import {paginationDriverFactory} from '../components/Pagination/Pagination.protractor.driver';
+import {paginationDriverFactory, PaginationDriver} from '../components/Pagination/Pagination.protractor.driver';
 export const paginationTestkitFactory = protractorTestkitFactoryCreator(paginationDriverFactory);
 
-import {badgeDriverFactory} from '../components/Badge/Badge.protractor.driver';
+import {badgeDriverFactory, BadgeDriver} from '../components/Badge/Badge.protractor.driver';
 export const badgeTestkitFactory = protractorTestkitFactoryCreator(badgeDriverFactory);
 
-import {tooltipDriverFactory} from '../components/Tooltip/Tooltip.protractor.driver';
+import {tooltipDriverFactory, TooltipDriver} from '../components/Tooltip/Tooltip.protractor.driver';
 export const tooltipTestkitFactory = protractorTestkitFactoryCreator(tooltipDriverFactory);
 
-import {dividerDriverFactory} from '../components/Divider/Divider.protractor.driver';
+import {dividerDriverFactory, DividerDriver} from '../components/Divider/Divider.protractor.driver';
 export const dividerTestkitFactory = protractorTestkitFactoryCreator(dividerDriverFactory);
 
-import {googleMapsIframeClientDriverFactory} from '../clients/GoogleMaps/GoogleMapsIframeClient.protractor.driver';
+import {googleMapsIframeClientDriverFactory, GoogleMapsIframeClientDriver} from '../clients/GoogleMaps/GoogleMapsIframeClient.protractor.driver';
 export const googleMapsIframeClientTestkitFactory = protractorTestkitFactoryCreator(googleMapsIframeClientDriverFactory);
 
-import {checkboxDriverFactory} from '../components/Checkbox/Checkbox.protractor.driver';
+import {checkboxDriverFactory, CheckboxDriver} from '../components/Checkbox/Checkbox.protractor.driver';
 export const checkboxTestkitFactory = protractorTestkitFactoryCreator(checkboxDriverFactory);
 
 //Stylable
-import {textDriverFactory} from '../components/StylableText/Text.protractor.driver';
+import {textDriverFactory, TextDriver} from '../components/StylableText/Text.protractor.driver';
 export const textTestkitFactory = protractorTestkitFactoryCreator(textDriverFactory);
 
-import {radioButtonDriverFactory} from '../components/RadioButton/RadioButton.protractor.driver';
+import {radioButtonDriverFactory, RadioButtonDriver} from '../components/RadioButton/RadioButton.protractor.driver';
 export const radioButtonTestkitFactory = protractorTestkitFactoryCreator(radioButtonDriverFactory);
 
-import {badgeDriverFactory as stylableBadgeDriverFactory} from '../components/StylableBadge/Badge.protractor.driver';
+import {badgeDriverFactory as stylableBadgeDriverFactory, BadgeDriver as StylableBadgeDriver} from '../components/StylableBadge/Badge.protractor.driver';
 export const stylablebadgeTestkitFactory = protractorTestkitFactoryCreator(stylableBadgeDriverFactory);
 
-import {autocompleteDriverFactory} from '../components/Autocomplete/Autocomplete.protractor.driver';
+import {autocompleteDriverFactory, AutocompleteDriver} from '../components/Autocomplete/Autocomplete.protractor.driver';
 export const autocompleteTestkitFactory = protractorTestkitFactoryCreator(autocompleteDriverFactory);
 
-import {hBoxDriverFactory} from '../components/HBox/HBox.protractor.driver';
+import {hBoxDriverFactory, HBoxDriver} from '../components/HBox/HBox.protractor.driver';
 export const hBoxTestkitFactory = protractorTestkitFactoryCreator(hBoxDriverFactory);
 
-import {sliderDriverFactory} from '../components/Slider/Slider.protractor.driver';
+import {sliderDriverFactory, SliderDriver} from '../components/Slider/Slider.protractor.driver';
 export const sliderTestkitFactory = protractorTestkitFactoryCreator(sliderDriverFactory);
 
-import {addressInputDriverFactory} from '../components/AddressInput/AddressInput.protractor.driver';
+import {addressInputDriverFactory, AddressInputDriver} from '../components/AddressInput/AddressInput.protractor.driver';
 export const addressInputTestkitFactory = protractorTestkitFactoryCreator(addressInputDriverFactory);
 
-import {labelDriverFactory} from '../components/Label/Label.protractor.driver';
+import {labelDriverFactory, LabelDriver} from '../components/Label/Label.protractor.driver';
 export const labelTestkitFactory = protractorTestkitFactoryCreator(labelDriverFactory);
 
-import {timePickerDriverFactory} from '../components/TimePicker/TimePicker.protractor.driver';
+import {timePickerDriverFactory, TimePickerDriver} from '../components/TimePicker/TimePicker.protractor.driver';
 export const timePickerTestkitFactory = protractorTestkitFactoryCreator(timePickerDriverFactory);
 
-import {toggleSwitchDriverFactory} from '../components/ToggleSwitch/ToggleSwitch.protractor.driver';
+import {toggleSwitchDriverFactory, ToggleSwitchDriver} from '../components/ToggleSwitch/ToggleSwitch.protractor.driver';
 export const toggleSwitchTestkitFactory = protractorTestkitFactoryCreator(toggleSwitchDriverFactory);
 
-import {labelWithOptionsDriverFactory} from '../components/LabelWithOptions/LabelWithOptions.protractor.driver';
+import {labelWithOptionsDriverFactory, LabelWithOptionsDriver} from '../components/LabelWithOptions/LabelWithOptions.protractor.driver';
 export const labelWithOptionsTestkitFactory = protractorTestkitFactoryCreator(labelWithOptionsDriverFactory);
 
-import {vBoxDriverFactory} from '../components/VBox/VBox.protractor.driver';
+import {vBoxDriverFactory, VBoxDriver} from '../components/VBox/VBox.protractor.driver';
 export const vBoxTestkitFactory = protractorTestkitFactoryCreator(vBoxDriverFactory);
 
-import {thumbnailDriverFactory} from '../components/Thumbnail/Thumbnail.protractor.driver';
+import {thumbnailDriverFactory, ThumbnailDriver} from '../components/Thumbnail/Thumbnail.protractor.driver';
 export const thumbnailTestkitFactory = protractorTestkitFactoryCreator(thumbnailDriverFactory);
