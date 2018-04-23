fb950dcf4c970d195a0fe30688639be37aa9cb94
Erez Makavy <erezm@wix.com>
2018-04-23 10:21:35 +0300

    GoogleMapsIframeClientDriver - fix return type

--- packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsIframeClient.protractor.driver.ts
+++ packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsIframeClient.protractor.driver.ts
@@ -2,7 +2,7 @@ import {BaseDriver, DriverFactory} from './../../common/BaseDriver.protractor';
 import {ElementFinder} from 'protractor';
 
 export interface GoogleMapsIframeClientDriver extends BaseDriver {
-  getParsedResults: () => Promise<string>;
+  getParsedResults: () => Promise<any>;
   getResultsElementWrapper: () => ElementFinder;
   enterText: (text: string) => Promise<void>;
   selectByValue: (value: string) => Promise<void>;
