0393fe047e4a8f020214ef42a681ca9c77986e42
Gilad Artzi <gilada@wix.com>
2018-04-26 16:17:03 +0300

    Removing AddressIcon

--- packages/wix-ui-core/src/components/AddressInput/AddressIcon.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const AddressIcon = ({className}) => {
-    return <svg xmlns="http://www.w3.org/2000/svg" className={className} width="12" height="15" viewBox="0 0 12 15">
-        <path fill="#000" fill-rule="nonzero" d="M5.885 8.182c-1.56 0-2.647-1.22-2.647-2.727 0-1.506 1.214-2.728 2.773-2.728 1.559 0 2.681 1.222 2.681 2.728 0 1.506-1.248 2.727-2.807 2.727M0 5.773C0 10.777 6 15 6 15s6-4.223 6-9.227C12 2.432 9.305 0 6.026 0 2.746 0 0 2.432 0 5.773z"/>
-    </svg>;
-};
