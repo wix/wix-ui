966d172b4c0d904b001bc4087ee734b3fe1e8d25
Ronen Stern <ronens@wix.com>
2018-05-06 14:04:34 +0300

    Use common postPRBuild and change npm script hook name

--- packages/wix-ui-core/postPRBuild.sh
+++ packages/wix-ui-core/postPRBuild.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
-
-export NVM_DIR="$HOME/.nvm"; . "$NVM_DIR/nvm.sh";
-if [ -f .nvmrc ]; then
-  echo "Running nvm use"
-  nvm use &> /dev/null || exit 1
-fi
-
-echo "Now using node $(node -v) (npm v$(npm -v))"
-echo "pwd:" `pwd`
-
-npm run --if-present postPRBuild
+/opt/bin/postPRBuild.sh
