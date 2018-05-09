f0e5393c52eb94367b581295060d641877f36f45
Alexey Lebedev <alexeyl@wix.com>
2018-05-09 14:47:49 +0300

    Remove Text component

--- packages/wix-ui-core/stories/index.tsx
+++ packages/wix-ui-core/stories/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
-import {Text} from '../src/components/Text';
 import {PaginationStory} from './Pagination/pagination-story';
 import {DividerStory} from './Divider/divider-story';
 import {GoogleMapsIframeClientStory} from './clients/GoogleMapsIframeClient-story';
@@ -34,11 +33,6 @@ import './Thumbnail.story';
 import './Popover.story';
 
 storiesOf('Components', module)
-  .add('Text', () => (
-    <Text ellipsis>
-      Hello World
-    </Text>
-  ))
   .add('Pagination', () => <PaginationStory/>)
   .add('GoogleMapsIframeClient', () => <GoogleMapsIframeClientStory/>)
   .add('Divider', () => <DividerStory/>)
