1f49a72ed4f36e6ad7311d1f1082d783608e3cbf
Erez Makavy <erezm@wix.com>
2018-05-24 09:43:04 +0300

    extract component function

--- packages/wix-ui-core/stories/index.tsx
+++ packages/wix-ui-core/stories/index.tsx
@@ -7,6 +7,8 @@ import {CheckboxStory} from './Checkbox/Checkbox-story';
 import {TooltipStory} from './Tooltip/custom';
 import {RadioButtonStory} from './RadioButton';
 
+const Components = storiesOf('Components', module);
+
 // baseComponents
 import './InputWithOptions.story';
 
@@ -15,23 +17,23 @@ import './AddressInput/index.story';
 import './AddressInput/E2E';
 import './Autocomplete.story';
 import './Button.story';
-storiesOf('Components', module).add('Checkbox', () => <CheckboxStory/>);
-storiesOf('Components', module).add('Divider', () => <DividerStory/>);
-storiesOf('Components', module).add('GoogleMapsIframeClient', () => <GoogleMapsIframeClientStory/>);
+Components.add('Checkbox', () => <CheckboxStory/>);
+Components.add('Divider', () => <DividerStory/>);
+Components.add('GoogleMapsIframeClient', () => <GoogleMapsIframeClientStory/>);
 import './Input/Input.story';
 import './IconWithOptions.story';
 import './Label.story';
 import './LabelWithOptions.story';
 import './Link.story';
-storiesOf('Components', module).add('Pagination', () => <PaginationStory/>);
+Components.add('Pagination', () => <PaginationStory/>);
 import './Popover.story';
-storiesOf('Components', module).add('RadioButton', () => <RadioButtonStory/>);
+Components.add('RadioButton', () => <RadioButtonStory/>);
 import './Slider.story';
 import './StylableBadge.story';
 import './Thumbnail.story';
 import './TimePicker/index.story';
 import './ToggleSwitch/index.story';
 import './Tooltip/index.story';
-storiesOf('Components', module).add('Tooltip Custom', () => <TooltipStory/>);
+Components.add('Tooltip Custom', () => <TooltipStory/>);
 import './Video.story';
 
