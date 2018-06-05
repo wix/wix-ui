287a43727ba512adba86868ff6dd432f2083c887
Alexey Lebedev <alisey.login@gmail.com>
2018-06-05 09:06:36 +0300

    Indexify all the things

--- packages/wix-ui-test-utils/src/dom/index.ts
+++ packages/wix-ui-test-utils/src/dom/index.ts
@@ -1,1 +1,7 @@
-export * from './hook';
+export function queryHook<E extends Element = Element>(baseElement: ParentNode, hook: string): E | null {
+  return baseElement.querySelector(`[data-hook~=${hook}]`);
+}
+
+export function queryHookAll<E extends Element = Element>(baseElement: ParentNode, hook: string): NodeListOf<E> {
+  return baseElement.querySelectorAll(`[data-hook~=${hook}]`);
+}
