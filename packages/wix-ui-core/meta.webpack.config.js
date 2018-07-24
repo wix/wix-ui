726cc0e70378c30bed182c43affc3f4a0285111a
Kieran Williams <kieranw@wix.com>
2018-07-24 17:05:42 +0300

    Don't ignore meta.webpack.config.js

---
+++ packages/wix-ui-core/meta.webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const glob = require('glob');
+const StylableWebpackPlugin = require('stylable-webpack-plugin');
+
+const packagePath = __dirname;
+const metaGlob = 'src/**/*.meta.ts?(x)';
+
+module.exports = {
+  context: packagePath,
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              declaration: false
+            }
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.st\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+  },
+  plugins: [
+    new StylableWebpackPlugin()
+  ]
+};
