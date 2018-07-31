b373c9cf330574e03deae5a68c2e45b8243a0486
Lior Belinsky <liorbe@wix.com>
2018-07-31 16:29:13 +0300

    wix-ui-core: Popover - update internal structure

--- packages/wix-ui-core/src/components/Popover/Popover.tsx
+++ packages/wix-ui-core/src/components/Popover/Popover.tsx
@@ -184,7 +184,9 @@ export class Popover extends React.Component<PopoverType, PopoverState> {
     const {moveBy, appendTo, placement, showArrow, moveArrowTo} = this.props;
     const modifiers = createModifiers({moveBy, appendTo});
 
-    let popper = (
+    const renderContent = () => <div key="popover-content" className={style.popoverContent}>{childrenObject.Content}</div>;
+
+    const popper = (
       <Popper
         data-hook="popover-content"
         modifiers={modifiers}
@@ -200,9 +202,9 @@ export class Popover extends React.Component<PopoverType, PopoverState> {
                 className={style.arrow}
                 style={getArrowShift(moveArrowTo, placement)}
               />,
-              <div key="popover-content" className={style.popoverContent}>{childrenObject.Content}</div>
+              renderContent()
             ] :
-            childrenObject.Content
+            renderContent()
         }
       </Popper>
     );
