dd5afe571e76911d2fce1db4f19d883940008631
Kieran Williams <kieranw@wix.com>
2018-08-02 16:26:03 +0300

    More nooop

--- packages/wix-ui-core/src/components/Tooltip/Tooltip.meta.tsx
+++ packages/wix-ui-core/src/components/Tooltip/Tooltip.meta.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
 import {TooltipComponent} from './Tooltip';
 import Registry from '@ui-autotools/registry';
+const noop = require('lodash/noop');
 
 const tooltipMetadata = Registry.getComponentMetadata(TooltipComponent);
 tooltipMetadata
   .addSim({
     title: 'Simulation with default props',
     props: {
-      // tslint:disable-next-line:no-empty
-      disableOnClickOutside: () => {},
-      // tslint:disable-next-line:no-empty
-      enableOnClickOutside: () => {},
+      disableOnClickOutside: noop,
+      enableOnClickOutside: noop,
       children: [<div key="1" />]
     }
   });
