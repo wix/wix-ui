57fdcaf8703aac7a40818aadb80063168eb3320b
Kieran Williams <kieranw@wix.com>
2018-08-02 16:25:20 +0300

    Timepicker is not compliant

--- packages/wix-ui-core/src/components/TimePicker/TimePicker.meta.ts
+++ packages/wix-ui-core/src/components/TimePicker/TimePicker.meta.ts
@@ -2,6 +2,8 @@ import {TimePicker} from './TimePicker';
 import Registry from '@ui-autotools/registry';
 
 const timePickerMetadata = Registry.getComponentMetadata(TimePicker);
+timePickerMetadata.reactStrictModeCompliant = false;
+
 timePickerMetadata
   .addSim({
     title: 'Simulation with default props',
