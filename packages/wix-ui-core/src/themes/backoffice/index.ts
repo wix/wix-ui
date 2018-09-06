0c7b59c410600e176465466a3790255972f83527
mykas <mykolass@wix.com>
2018-09-06 16:59:17 +0300

    Merge

--- packages/wix-ui-core/src/themes/backoffice/index.ts
+++ packages/wix-ui-core/src/themes/backoffice/index.ts
@@ -1,4 +1,4 @@
-import Button from "./buttonv2/buttonv2.st.css";
+import Button from "./buttonv2/base-buttonv2.st.css";
 import TextButton from "./button/text-button.st.css";
 import IconButton from "./button/icon-button.st.css";
 import BOTheme from "./theme.st.css";
