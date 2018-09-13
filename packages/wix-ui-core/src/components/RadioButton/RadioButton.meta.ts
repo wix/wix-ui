89527e2f7f0e84010b62a1cc4ced9183985b7958
argshook <argshook@gmail.com>
2018-09-13 15:02:25 +0300

    --

--- packages/wix-ui-core/src/components/RadioButton/RadioButton.meta.ts
+++ packages/wix-ui-core/src/components/RadioButton/RadioButton.meta.ts
@@ -2,6 +2,7 @@ import {RadioButton} from './RadioButton';
 import Registry from '@ui-autotools/registry';
 
 const radioButtonMetadata = Registry.getComponentMetadata(RadioButton);
+
 radioButtonMetadata.addSim({
   title: 'Simulation with default props',
   props: {
