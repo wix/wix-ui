047ed1b97e989951388ee1754a95a6a29b7e0a07
Lior Belinsky <liorbe@wix.com>
2018-09-17 16:35:34 +0300

    refactor protractor unidriver creator

--- packages/wix-ui-test-utils/src/protractor/protractor.ts
+++ packages/wix-ui-test-utils/src/protractor/protractor.ts
@@ -1,6 +1,13 @@
 import {$, ElementFinder} from 'protractor';
 import {protractorUniDriver, UniDriver} from 'unidriver';
 
-export function protractorTestkitFactoryCreator<T> (driverFactory: (base: UniDriver) => T) {
-  return (obj: {dataHook: string}) => protractorUniDriver(() => driverFactory($(`[data-hook='${obj.dataHook}']`)));
+export function protractorTestkitFactoryCreator<T> (driverFactory: (e: ElementFinder) => T) {
+  return (obj: {dataHook: string}) => driverFactory($(`[data-hook='${obj.dataHook}']`));
+}
+
+export function protractorUniTestkitFactoryCreator<T> (driverFactory: (base: UniDriver) => T) {
+  return (obj: {dataHook: string}) => {
+    const base = protractorUniDriver(() => ($(`[data-hook='${obj.dataHook}']`)));
+    return driverFactory(base);
+  };
 }
