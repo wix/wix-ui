3b07689ec4c52c62d0bdaa2f2f513fb075c895af
Lior Belinsky <liorbe@wix.com>
2018-09-20 13:40:10 +0300

    add puppeteer unidriver infrastructure

--- packages/wix-ui-test-utils/src/puppeteer/puppeteer.ts
+++ packages/wix-ui-test-utils/src/puppeteer/puppeteer.ts
@@ -1,5 +1,14 @@
 import {Page, ElementHandle} from 'puppeteer';
+import {BaseUniDriver} from '../driver-factory/createDriverFactory';
+import {UniDriver, pupUniDriver} from 'unidriver';
 
 export function puppeteerTestkitFactoryCreator<T> (driverFactory: (e: ElementHandle | null, page: Page) => T) {
   return async (obj: {dataHook: string, page: Page}) => driverFactory(await obj.page.$(`[data-hook='${obj.dataHook}']`), obj.page);
+}
+
+export function puppeteerUniTestkitFactoryCreator<T extends BaseUniDriver> (driverFactory: (base: UniDriver) => T) {
+  return async (obj: {dataHook: string, page: Page}) => {
+    const base = pupUniDriver(await obj.page.$(`[data-hook='${obj.dataHook}']`), obj.page);
+    return driverFactory(base);
+  };
 }
