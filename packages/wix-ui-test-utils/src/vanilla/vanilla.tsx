93e549b6fb883041cc1aa13599ab0fd1b63ece04
Lior Belinsky <liorbe@wix.com>
2018-09-20 12:10:02 +0300

    add BaseUniDriver

--- packages/wix-ui-test-utils/src/vanilla/vanilla.tsx
+++ packages/wix-ui-test-utils/src/vanilla/vanilla.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactTestUtils from 'react-dom/test-utils';
 import {reactEventTrigger} from '../react-helpers';
-import {DriverFactory, BaseDriver} from '../driver-factory';
+import {DriverFactory, BaseDriver, BaseUniDriver} from '../driver-factory';
 import {UniDriver, reactUniDriver} from 'unidriver';
 
 export function testkitFactoryCreator<T extends BaseDriver> (driverFactory: DriverFactory<T>) {
@@ -12,7 +12,7 @@ export function testkitFactoryCreator<T extends BaseDriver> (driverFactory: Driv
   };
 }
 
-export function uniTestkitFactoryCreator<T extends BaseDriver> (driverFactory: (base: UniDriver) => T) {
+export function uniTestkitFactoryCreator<T extends BaseUniDriver> (driverFactory: (base: UniDriver) => T) {
   return (obj: {wrapper: HTMLElement, dataHook: string}) => {
     const element = obj.wrapper.querySelector(`[data-hook='${obj.dataHook}']`) as Element;
     const base = reactUniDriver(element);
