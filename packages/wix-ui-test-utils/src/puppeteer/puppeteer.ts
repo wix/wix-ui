10e001094f247e7bc0317e7f304bbaf091d2e2d3
Lior Belinsky <liorbe@wix.com>
2018-09-25 11:04:05 +0300

    add puppeteer adapter

--- packages/wix-ui-test-utils/src/puppeteer/puppeteer.ts
+++ packages/wix-ui-test-utils/src/puppeteer/puppeteer.ts
@@ -1,6 +1,9 @@
 import {Page, ElementHandle} from 'puppeteer';
 import {BaseUniDriver} from '../driver-factory/createDriverFactory';
-import {UniDriver, pupUniDriver} from 'unidriver';
+import {UniDriver} from 'unidriver';
+import {pupUniDriver} from 'unidriver/dist/lib/puppeteer';
+//TODO: change it to import {pupUniDriver} from 'unidriver/puppeteer';
+// Waiting for https://github.com/wix-incubator/unidriver/pull/7 to be merged
 
 export function puppeteerTestkitFactoryCreator<T> (driverFactory: (e: ElementHandle | null, page: Page) => T) {
   return async (obj: {dataHook: string, page: Page}) => driverFactory(await obj.page.$(`[data-hook='${obj.dataHook}']`), obj.page);
@@ -8,7 +11,7 @@ export function puppeteerTestkitFactoryCreator<T> (driverFactory: (e: ElementHan
 
 export function puppeteerUniTestkitFactoryCreator<T extends BaseUniDriver> (driverFactory: (base: UniDriver) => T) {
   return async (obj: {dataHook: string, page: Page}) => {
-    const base = pupUniDriver(await obj.page.$(`[data-hook='${obj.dataHook}']`), obj.page);
+    const base = pupUniDriver(async () => await obj.page.$(`[data-hook='${obj.dataHook}']`));
     return driverFactory(base);
   };
 }
