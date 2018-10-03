46e0a69b34dcbcb22ed297dfbde6516332239360
Kieran Williams <kieranw@wix.com>
2018-10-03 17:28:08 +0300

    Adding snap tests for relevant components

--- packages/wix-ui-core/src/components/Checkbox/Checkbox.meta.ts
+++ packages/wix-ui-core/src/components/Checkbox/Checkbox.meta.ts
@@ -1,7 +1,17 @@
 import {Checkbox} from './Checkbox';
 import Registry from '@ui-autotools/registry';
+import style from './CheckboxStyle.st.css';
 
 const checkboxMetadata = Registry.getComponentMetadata(Checkbox);
+
+checkboxMetadata.exportedFrom({
+  path: 'src/components/Checkbox/Checkbox',
+  exportName: 'ButtonNext',
+  baseStylePath: 'src/components/Checkbox/Checkbox.st.css'
+});
+
+checkboxMetadata.addStyle(style, {name: 'style', path: 'src/components/Checkbox/CheckboxStyle.st.css'});
+
 checkboxMetadata
   .addSim({
     title: 'Simulation with default props',
