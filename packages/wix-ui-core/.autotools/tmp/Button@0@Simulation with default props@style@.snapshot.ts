91c3c582e9547a82034ef922dd5213524d0965a8
Kieran Williams <kieranw@wix.com>
2018-10-03 17:38:19 +0300

    Adding snap tests for relevant components

---
+++ packages/wix-ui-core/.autotools/tmp/Button@0@Simulation with default props@style@.snapshot.ts
@@ -0,0 +1,3 @@
+import style from '../../src/components/Button/ButtonStyle.st.css';
+import {Button} from '../../src/components/Button/Button';
+export default {comp: Button, name: 'Button', style};
---
+++ packages/wix-ui-core/.autotools/tmp/ButtonNext@0@Simulation with default props@style@.snapshot.ts
@@ -0,0 +1,3 @@
+import style from '../../src/components/Checkbox/CheckboxStyle.st.css';
+import {ButtonNext} from '../../src/components/Checkbox/Checkbox';
+export default {comp: ButtonNext, name: 'ButtonNext', style};
