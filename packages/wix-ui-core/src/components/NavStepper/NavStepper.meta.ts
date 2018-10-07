a20daef6bf62d1da49752fe134c37d7ab9c380e2
Kieran Williams <kieranw@wix.com>
2018-10-07 11:32:47 +0300

    Remove added space

--- packages/wix-ui-core/src/components/NavStepper/NavStepper.meta.ts
+++ packages/wix-ui-core/src/components/NavStepper/NavStepper.meta.ts
@@ -2,7 +2,6 @@ import {NavStepper} from './NavStepper';
 import Registry from '@ui-autotools/registry';
 
 const navStepperMetadata = Registry.getComponentMetadata(NavStepper);
-
 navStepperMetadata
   .addSim({
     title: 'Simulation with default props',
