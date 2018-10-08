342667577aa2f9eaca4f8ada11b9399fb147c2cf
mykas <mykolass@wix.com>
2018-10-08 11:39:25 +0300

    Initial button theme structure

---
+++ packages/wix-ui-core/src/themes/backoffice/index.ts
@@ -0,0 +1,10 @@
+import ButtonNext from "./button/button.st.css";
+import BackOfficeTheme from "./theme.st.css";
+
+export default {
+  BackOfficeTheme,
+  ButtonNext
+};
+
+export const buttonNext = ButtonNext;
+export const backOfficeTheme = BackOfficeTheme;
