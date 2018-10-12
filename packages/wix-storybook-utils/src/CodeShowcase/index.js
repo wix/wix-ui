63cc061df732c4c9d257e6120eba43601474f4cf
mykas <mykolass@wix.com>
2018-10-12 10:25:00 +0300

    Redesign of codeshowcase

--- packages/wix-storybook-utils/src/CodeShowcase/index.js
+++ packages/wix-storybook-utils/src/CodeShowcase/index.js
@@ -1,34 +1,117 @@
 import React from 'react';
-import {string, node, bool} from 'prop-types';
-import classNames from 'classnames';
-import style from './CodeShowcase.st.css';
-import Markdown from '../Markdown';
 
-const toCodeBlock = (code, type = 'html') =>
-  ['```' + type, code.trim(), '```'].join('\n');
+import {string, node, bool, object} from 'prop-types';
+import styleclass from './CodeShowcase.st.css';
+import SyntaxHighlighter, {
+  registerLanguage
+} from 'react-syntax-highlighter/prism-light';
+import jsx from 'react-syntax-highlighter/languages/prism/jsx';
+import vs from 'react-syntax-highlighter/styles/prism/vs';
+import {iconShow} from './components/Show';
+import {iconHide} from './components/Hide';
 
-export default class CodeShowcase extends React.Component {
-  static propTypes = {
-    title: string,
-    children: node,
-    code: string,
-    inverted: bool,
-    className: string
-  };
-  static defaultProps = {
-    title: 'Example',
-    inverted: false
-  };
-  render() {
-    const {title, children, code, inverted, className} = this.props;
-    return (
-      <div className={classNames(style.root, className)}>
-        <h3 className={style.title}>{title}</h3>
-        <div className={style.content}>
-          <div {...style('show', {inverted}, this.props)}>{children}</div>
-          <Markdown className={style.markdown} source={toCodeBlock(code)}/>
+registerLanguage('jsx', jsx);
+
+const List = ({children}) => (
+  <div className={styleclass.demoItems}>
+    {Array.isArray(children) ?
+      children.map((child, index) => (
+        <div key={index} style={{marginRight: '8px', marginBottom: '12px'}}>
+          {child}
         </div>
+      )) :
+      children}
+  </div>
+);
+
+List.propTypes = {
+  children: node
+};
+
+const customHighlighterStyle = {
+  padding: '16px 32px',
+  border: 'none',
+  fontFamily: `"HelveticaNeueW01-45Ligh","sans-serif"`,
+  fontSize: '1.2em'
+};
+
+class CodeShowcase extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    };
+  }
+
+  toggleCodeShow = () => {
+    this.setState({show: !this.state.show});
+  };
+
+  render() {
+    const {
+      code,
+      title,
+      description,
+      link,
+      inverted,
+      theme,
+      style,
+      children
+    } = this.props;
+    const {show} = this.state;
+    return (
+      <div
+        style={style}
+        {...styleclass('root', {inverted}, {className: theme})}
+      >
+        <section className={styleclass.demo}>
+          <List>{children}</List>
+        </section>
+        <section className={styleclass.meta}>
+          <a href={link} className={styleclass.title}>
+            {title}
+          </a>
+          <div className={styleclass.description}>{description}</div>
+          <span
+            className={show ? styleclass.iconHide : styleclass.iconShow}
+            onClick={this.toggleCodeShow}
+          >
+            {show ? iconHide : iconShow}
+          </span>
+        </section>
+        <section className={show ? styleclass.code : styleclass.codeHide}>
+          <SyntaxHighlighter
+            customStyle={customHighlighterStyle}
+            language="jsx"
+            codeTagProps={{
+              style: {fontFamily: `monospace`}
+            }}
+            style={vs}
+          >
+            {code}
+          </SyntaxHighlighter>
+        </section>
       </div>
     );
   }
 }
+
+CodeShowcase.propTypes = {
+  title: string,
+  children: node,
+  code: string,
+  inverted: bool,
+  description: string,
+  link: string,
+  theme: string,
+  style: object,
+  className: string
+};
+
+CodeShowcase.defaultProps = {
+  title: 'Example',
+  inverted: false,
+  code: ''
+};
+
+export default CodeShowcase;
