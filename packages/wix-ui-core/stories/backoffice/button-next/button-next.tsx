0c57e7e6646f3faffac231cd39f3f04e7fca9caa
mykas <mykolass@wix.com>
2018-10-16 16:02:36 +0300

    storybook component parsing fix

--- packages/wix-ui-core/stories/backoffice/button-next/button-next.tsx
+++ packages/wix-ui-core/stories/backoffice/button-next/button-next.tsx
@@ -2,10 +2,13 @@ import * as React from "react";
 import { backofficeTheme } from "../../../src/themes/backoffice";
 import { ButtonNext as ButtonNextCore } from "../../../src/components/button-next";
 
-export function ButtonNext(props) {
-  return (
-    <div className={backofficeTheme}>
-      <ButtonNextCore {...props} />
-    </div>
-  );
+export class ButtonNext extends React.Component {
+  displayName = "ButtonNext";
+  render() {
+    return (
+      <div className={backofficeTheme}>
+        <ButtonNextCore {...this.props} />
+      </div>
+    );
+  }
 }
