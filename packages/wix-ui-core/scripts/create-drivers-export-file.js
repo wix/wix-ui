1fcde46087ced502efd5492fb752f742c7831f26
rashellek <rashellek@wix.com>
2018-10-21 11:45:24 +0300

    scripts fixes

--- packages/wix-ui-core/scripts/create-drivers-export-file.js
+++ packages/wix-ui-core/scripts/create-drivers-export-file.js
@@ -2,11 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
 
-const fileContent = glob.sync('./src/components/**/**.driver.*').map(p => 'export * from "' + p.replace(p.slice(p.lastIndexOf('.')),'') + '";\n').join('')
+const exportedDrivers = glob.sync('./src/components/**/*.driver.*').filter(p => !p.includes('.private'))
+const fileContent = exportedDrivers.map(p =>'export * from "' + path.dirname(p) + path.basename(p, '.ts') + '";\n').join('')
 console.log(fileContent)
-const filepath = "drivers.js";
+const filepath = "drivers.js"; // created in the root of ui-core
 
-fs.writeFile(filepath, fileContent, (err) => {
+fs.writeFileSync(filepath, fileContent, (err) => {
     if (err) throw err;
-    console.log("The file was succesfully saved!");
+    console.log("The extansions file was succesfully saved!");
 }); 
+
