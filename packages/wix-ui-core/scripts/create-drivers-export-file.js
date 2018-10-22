49c81a800265a61651fd7b5d3418876a6ea38840
rashellek <rashellek@wix.com>
2018-10-22 11:53:38 +0300

    changed the script to be a generic function for different patterns/test platforms

--- packages/wix-ui-core/scripts/create-drivers-export-file.js
+++ packages/wix-ui-core/scripts/create-drivers-export-file.js
@@ -10,31 +10,29 @@ function toCamelCase( str ){
 }
 
 function createExports(fileName, pattern){
+  //const exportedDrivers = glob.sync('./src/components/**/*.driver.*').filter(p => !p.includes('.private'))
+  const exportedDrivers = glob.sync(`./src/components/**/*.${pattern}.*`).filter(p => !p.includes('.private'))
+
+  const exportNames = exportedDrivers.map(p => path.basename(p, '.ts'))
+  const camelCasedExportNames = exportNames.map(n => toCamelCase(n.split('.').join('-')))
   
-}
-
-const exportedDrivers = glob.sync('./src/components/**/*.driver.*').filter(p => !p.includes('.private'))
-
-const exportNames = exportedDrivers.map(p => path.basename(p, '.ts'))
-const camelCasedExportNames = exportNames.map(n => toCamelCase(n.split('.').join('-')))
-
-const trimmedExportedDriversPaths = exportedDrivers.map(p => './' + path.join('dist',path.parse(p).dir, path.parse(p).name))
-
-const jsFileContent = camelCasedExportNames.map((exportName, i) => `module.exports.${exportName} = require('${trimmedExportedDriversPaths[i]}');`).join('\n')
-
-const typeScriptFileContent = trimmedExportedDriversPaths.map(p =>`export * from '${p}';`).join('\n')
-
-
-const jsfilePath = "drivers.js"
-const typeScriptFilePath = "drivers.d.ts"; // created in the root of ui-core
-
-fs.writeFile(jsfilePath, jsFileContent, (err) => {
+  const exportedDriversPaths = exportedDrivers.map(p => './' + path.join('dist',path.parse(p).dir, path.parse(p).name))
+ 
+  const jsfilePath = `${fileName}.js `;
+  const typeScriptFilePath =  `${fileName}.d.ts`; // created in the root of ui-core
+  const jsFileContent = camelCasedExportNames.map((exportName, i) => `module.exports.${exportName} = require('${exportedDriversPaths[i]}');`).join('\n')
+  const typeScriptFileContent = exportedDriversPaths.map(p =>`export * from '${p}';`).join('\n')
+  
+  fs.writeFileSync(jsfilePath, jsFileContent, (err) => {
+      if (err) throw err;
+      console.log("The js extansions file was succesfully saved!");
+  }); 
+  
+  fs.writeFileSync(typeScriptFilePath, typeScriptFileContent, (err) => {
     if (err) throw err;
-    console.log("The extansions file was succesfully saved!");
-}); 
+    console.log("The ts extansions file was succesfully saved!");
+  }); 
+};
 
-fs.writeFileSync(typeScriptFilePath, typeScriptFileContent, (err) => {
-  if (err) throw err;
-  console.log("The second extansions file was succesfully saved!");
-}); 
-
+// createExports('drivers', 'driver')
+createExports('protractorDrivers', 'protractor.driver');
