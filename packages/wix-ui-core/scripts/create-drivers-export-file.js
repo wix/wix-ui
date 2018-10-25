a29d19e5b53381dc1fcf86549335c5c4718d58ff
rashellek <rashellek@wix.com>
2018-10-25 14:12:47 +0300

    added ;

--- packages/wix-ui-core/scripts/create-drivers-export-file.js
+++ packages/wix-ui-core/scripts/create-drivers-export-file.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 const glob = require('glob');
 
 function filterExports(exportPath,pattern){
-  const compName = path.parse(exportPath).name.split('.')[0] // Gets the component name from the driver path.
-  return !exportPath.includes('.private') && (path.parse(exportPath).name).includes(compName + pattern) // Private drivers are not exposed.
-}
+  const compName = path.parse(exportPath).name.split('.')[0]; // Gets the component name from the driver path.
+  return !exportPath.includes('.private') && (path.parse(exportPath).name).includes(compName + pattern); // Private drivers are not exposed.
+};
 
 function createExports(fileName, pattern){
   const driversDir = './drivers';
   !fs.existsSync(driversDir) && fs.mkdirSync(driversDir); // Creates the drivers folder in the root of ui-core
 
-  const compDirPath = './src/components/**/*.driver.*' // The path for ui-core components.
-  const exportedDrivers = glob.sync(compDirPath).filter(exportPath => filterExports(exportPath,pattern))
-  const formattedExportedDrivers = exportedDrivers.map(p => './' + path.join('dist',path.parse(p).dir, path.parse(p).name))
+  const compDirPath = './src/components/**/*.driver.*'; // The path for ui-core components.
+  const exportedDrivers = glob.sync(compDirPath).filter(exportPath => filterExports(exportPath,pattern));
+  const formattedExportedDrivers = exportedDrivers.map(p => './' + path.join('dist',path.parse(p).dir, path.parse(p).name));
   
   const jsCommonfilePath = `${driversDir}/${fileName}.js`;
   const jsCommonFileContent = `module.exports = {${formattedExportedDrivers.map(exportPath => `...require('${exportPath}')`).join(',\n')}};`
@@ -23,12 +23,12 @@ function createExports(fileName, pattern){
   }); 
   
   const typeScriptFilePath =  `${driversDir}/${fileName}.d.ts`; 
-  const typeScriptFileContent = formattedExportedDrivers.map(exportPath =>`export * from '${exportPath}';`).join('\n')
+  const typeScriptFileContent = formattedExportedDrivers.map(exportPath =>`export * from '${exportPath}';`).join('\n');
 
   fs.writeFileSync(typeScriptFilePath, typeScriptFileContent, (err) => {
     if (err) throw err;
   }); 
 };
 
-createExports('drivers', '.driver')
+createExports('drivers', '.driver');
 createExports('protractorDrivers', '.protractor.driver');
