46d7d79771f848a7148362c2a98102eacfa9272d
rashellek <rashellek@wix.com>
2018-10-25 14:11:15 +0300

    unique patterns added to script

--- packages/wix-ui-core/scripts/create-drivers-export-file.js
+++ packages/wix-ui-core/scripts/create-drivers-export-file.js
@@ -2,34 +2,33 @@ const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
 
-function toCamelCase( str ){
-  str= str[0].toLowerCase() + str.slice(1)
-  return str.replace(/-([a-z])/g, function (m, w) {
-    return w.toUpperCase();
-  });
+function filterExports(exportPath,pattern){
+  const compName = path.parse(exportPath).name.split('.')[0] // Gets the component name from the driver path.
+  return !exportPath.includes('.private') && (path.parse(exportPath).name).includes(compName + pattern) // Private drivers are not exposed.
 }
 
 function createExports(fileName, pattern){
-  const exportedDrivers = glob.sync(`./src/components/**/*.${pattern}.*`).filter(p => !p.includes('.private'))
+  const driversDir = './drivers';
+  !fs.existsSync(driversDir) && fs.mkdirSync(driversDir); // Creates the drivers folder in the root of ui-core
 
-  const exportNames = exportedDrivers.map(p => path.basename(p, '.ts'))
-  const camelCasedExportNames = exportNames.map(n => toCamelCase(n.split('.').join('-')))
+  const compDirPath = './src/components/**/*.driver.*' // The path for ui-core components.
+  const exportedDrivers = glob.sync(compDirPath).filter(exportPath => filterExports(exportPath,pattern))
+  const formattedExportedDrivers = exportedDrivers.map(p => './' + path.join('dist',path.parse(p).dir, path.parse(p).name))
   
-  const exportedDriversPaths = exportedDrivers.map(p => './' + path.join('dist',path.parse(p).dir, path.parse(p).name))
- 
-  const jsfilePath = `${fileName}.js`;
-  const typeScriptFilePath =  `${fileName}.d.ts`; // created in the root of ui-core
-  const jsFileContent = camelCasedExportNames.map((exportName, i) => `module.exports.${exportName} = require('${exportedDriversPaths[i]}');`).join('\n')
-  const typeScriptFileContent = exportedDriversPaths.map(p =>`export * from '${p}';`).join('\n')
+  const jsCommonfilePath = `${driversDir}/${fileName}.js`;
+  const jsCommonFileContent = `module.exports = {${formattedExportedDrivers.map(exportPath => `...require('${exportPath}')`).join(',\n')}};`
   
-  fs.writeFileSync(jsfilePath, jsFileContent, (err) => {
+  fs.writeFileSync(jsCommonfilePath, jsCommonFileContent, (err) => {
       if (err) throw err;
   }); 
   
+  const typeScriptFilePath =  `${driversDir}/${fileName}.d.ts`; 
+  const typeScriptFileContent = formattedExportedDrivers.map(exportPath =>`export * from '${exportPath}';`).join('\n')
+
   fs.writeFileSync(typeScriptFilePath, typeScriptFileContent, (err) => {
     if (err) throw err;
   }); 
 };
 
-createExports('drivers', 'driver')
-createExports('protractorDrivers', 'protractor.driver');
+createExports('drivers', '.driver')
+createExports('protractorDrivers', '.protractor.driver');
