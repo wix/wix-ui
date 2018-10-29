c76c513db6fcd270bedf4ce49ee6dd0e8547f33a
argshook <argshook@gmail.com>
2018-10-29 10:33:47 +0200

    fix(LiveCodeExample): remove wix-style-react dep

--- packages/wix-storybook-utils/stories/index.story.js
+++ packages/wix-storybook-utils/stories/index.story.js
@@ -8,7 +8,7 @@ const showcase = `<button className={button.one}>one</button>
 <button className={button.three}>three</button>`;
 
 const exampleScope = {
-  Button: props => <button {...props} />
+  Button: props => <button {...props}/>
 };
 
 const ExampleShowcase = () => (
@@ -49,7 +49,7 @@ export default {
 
   examples: (
     <div>
-      <ExampleShowcase />
+      <ExampleShowcase/>
 
       <LiveCodeExample
         scope={exampleScope}
