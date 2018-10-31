f5f2605c5dddfb0c5d8d8d00b99289ef94dd39c9
mykas <mykolass@wix.com>
2018-10-31 09:57:23 +0200

    Move external depedency to source code

--- packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsBasicClient.spec.ts
+++ packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsBasicClient.spec.ts
@@ -1,23 +1,32 @@
-import { GoogleMapsBasicClient } from './GoogleMapsBasicClient';
-const flushPromises = require('flush-promises');
+import { GoogleMapsBasicClient } from "./GoogleMapsBasicClient";
 
-const LANG = 'en';
-const CLIENT_ID = 'client-id';
+const LANG = "en";
+const CLIENT_ID = "client-id";
 const EXPECTED_URL = `//maps.googleapis.com/maps/api/js?libraries=places&client=${CLIENT_ID}&callback=initMap&language=${LANG}`;
 
-describe('GoogleMapsBasicClient', () => {
+const scheduler =
+  typeof setImmediate === "function"
+    ? data => setImmediate(data)
+    : data => setTimeout(data);
+
+const flushPromises = () =>
+  new Promise(resolve => {
+    scheduler(resolve);
+  });
+
+describe("GoogleMapsBasicClient", () => {
   const appendChildSpy = jest
-    .spyOn(document.body, 'appendChild')
+    .spyOn(document.body, "appendChild")
     .mockImplementation(() => null);
 
-  let status = 'OK';
+  let status = "OK";
 
   const getStatus = () => status;
   const setStatus = newStatus => (status = newStatus);
 
   const setUpGoogleMapsMock = () => {
     const getPlacePredictions = jest.fn((request, callback) => {
-      const result = 'result ' + request.input;
+      const result = "result " + request.input;
       setTimeout(() => callback(result, getStatus()), 10);
     });
     const geocode = jest.fn((request, callback) => {
@@ -26,20 +35,20 @@ describe('GoogleMapsBasicClient', () => {
           geometry: {
             location: {
               lat: () => 1,
-              lng: () => 2,
-            },
-          },
-        },
+              lng: () => 2
+            }
+          }
+        }
       ];
       setTimeout(() => callback(result, getStatus()), 10);
     });
     (window as any).google = {
       maps: {
         places: {
-          AutocompleteService: jest.fn(() => ({ getPlacePredictions })),
+          AutocompleteService: jest.fn(() => ({ getPlacePredictions }))
         },
-        Geocoder: jest.fn(() => ({ geocode })),
-      },
+        Geocoder: jest.fn(() => ({ geocode }))
+      }
     };
     return { mock: (window as any).google, getPlacePredictions, geocode };
   };
@@ -48,15 +57,15 @@ describe('GoogleMapsBasicClient', () => {
     appendChildSpy.mockClear();
     delete (window as any).google;
     delete (window as any).initMap;
-    setStatus('OK');
+    setStatus("OK");
   });
 
   afterAll(() => {
     (appendChildSpy as any).mockRestore();
   });
 
-  describe('Initialization', () => {
-    it('should append google maps api script tag', () => {
+  describe("Initialization", () => {
+    it("should append google maps api script tag", () => {
       const client = new GoogleMapsBasicClient();
       client.loadScript(CLIENT_ID, LANG);
 
@@ -64,23 +73,23 @@ describe('GoogleMapsBasicClient', () => {
       expect(firstCall.src.indexOf(EXPECTED_URL)).not.toBe(-1);
     });
 
-    it('should not append google maps api script tag in cased it is already loaded', () => {
+    it("should not append google maps api script tag in cased it is already loaded", () => {
       const client = new GoogleMapsBasicClient();
       setUpGoogleMapsMock();
       client.loadScript(CLIENT_ID, LANG);
       expect(appendChildSpy).not.toHaveBeenCalled();
     });
 
-    it('should create a global callback function for google maps init', () => {
+    it("should create a global callback function for google maps init", () => {
       const client = new GoogleMapsBasicClient();
       expect((window as any).initMap).not.toBeDefined();
       client.loadScript(CLIENT_ID, LANG);
       expect((window as any).initMap).toBeDefined();
     });
 
-    it('should wait until script is fully loaded to initiate the client', async () => {
+    it("should wait until script is fully loaded to initiate the client", async () => {
       const client = new GoogleMapsBasicClient();
-      client.autocomplete(CLIENT_ID, LANG, 'tel aviv');
+      client.autocomplete(CLIENT_ID, LANG, "tel aviv");
 
       const { mock } = setUpGoogleMapsMock();
       expect(mock.maps.places.AutocompleteService).not.toHaveBeenCalled();
@@ -90,115 +99,115 @@ describe('GoogleMapsBasicClient', () => {
       expect(mock.maps.Geocoder).toHaveBeenCalled();
     });
 
-    it('should instantiate inner objects if google maps api is already loaded (only once)', () => {
+    it("should instantiate inner objects if google maps api is already loaded (only once)", () => {
       const client = new GoogleMapsBasicClient();
       const { mock } = setUpGoogleMapsMock();
 
-      client.autocomplete(CLIENT_ID, LANG, 'tel aviv');
+      client.autocomplete(CLIENT_ID, LANG, "tel aviv");
       expect(mock.maps.places.AutocompleteService).toHaveBeenCalled();
       expect(mock.maps.Geocoder).toHaveBeenCalled();
 
-      client.autocomplete(CLIENT_ID, LANG, 'tel aviv');
+      client.autocomplete(CLIENT_ID, LANG, "tel aviv");
       expect(mock.maps.places.AutocompleteService).toHaveBeenCalledTimes(1);
       expect(mock.maps.Geocoder).toHaveBeenCalledTimes(1);
     });
 
-    it('should append script tag only once', () => {
+    it("should append script tag only once", () => {
       const client = new GoogleMapsBasicClient();
-      client.autocomplete(CLIENT_ID, LANG, 'tel aviv');
-      client.autocomplete(CLIENT_ID, LANG, 'tel aviv yafo');
+      client.autocomplete(CLIENT_ID, LANG, "tel aviv");
+      client.autocomplete(CLIENT_ID, LANG, "tel aviv yafo");
       expect(appendChildSpy).toHaveBeenCalledTimes(1);
     });
   });
 
-  describe('Functionality', () => {
-    describe('autocomplete', () => {
-      it('should call getPlacePredictions and return a promise', async () => {
+  describe("Functionality", () => {
+    describe("autocomplete", () => {
+      it("should call getPlacePredictions and return a promise", async () => {
         const client = new GoogleMapsBasicClient();
-        const result = client.autocomplete(CLIENT_ID, LANG, 'tel aviv');
+        const result = client.autocomplete(CLIENT_ID, LANG, "tel aviv");
         const { getPlacePredictions } = setUpGoogleMapsMock();
         (window as any).initMap();
         await flushPromises();
         expect(getPlacePredictions).toHaveBeenCalledWith(
-          { input: 'tel aviv' },
-          expect.anything(),
+          { input: "tel aviv" },
+          expect.anything()
         );
-        expect(await result).toBe('result tel aviv');
+        expect(await result).toBe("result tel aviv");
       });
 
-      it('should handle an object as the request as well', async () => {
+      it("should handle an object as the request as well", async () => {
         const client = new GoogleMapsBasicClient();
         const result = client.autocomplete(CLIENT_ID, LANG, {
-          input: 'tel aviv',
+          input: "tel aviv"
         });
         const { getPlacePredictions } = setUpGoogleMapsMock();
         (window as any).initMap();
         await flushPromises();
         expect(getPlacePredictions).toHaveBeenCalledWith(
-          { input: 'tel aviv' },
-          expect.anything(),
+          { input: "tel aviv" },
+          expect.anything()
         );
-        expect(await result).toBe('result tel aviv');
+        expect(await result).toBe("result tel aviv");
       });
 
-      it('Should throw an error if status is not OK', async () => {
-        setStatus('NOT_OK');
+      it("Should throw an error if status is not OK", async () => {
+        setStatus("NOT_OK");
         const client = new GoogleMapsBasicClient();
-        const result = client.autocomplete(CLIENT_ID, LANG, 'tel aviv');
+        const result = client.autocomplete(CLIENT_ID, LANG, "tel aviv");
         const { getPlacePredictions } = setUpGoogleMapsMock();
         (window as any).initMap();
         await flushPromises();
         expect(getPlacePredictions).toHaveBeenCalledWith(
-          { input: 'tel aviv' },
-          expect.anything(),
+          { input: "tel aviv" },
+          expect.anything()
         );
-        return expect(result).rejects.toEqual('ERROR');
+        return expect(result).rejects.toEqual("ERROR");
       });
     });
 
-    describe('geocode', () => {
-      it('should call geocode and return a promise', async () => {
+    describe("geocode", () => {
+      it("should call geocode and return a promise", async () => {
         const client = new GoogleMapsBasicClient();
-        const result = client.geocode(CLIENT_ID, LANG, 'tel aviv');
+        const result = client.geocode(CLIENT_ID, LANG, "tel aviv");
         const { geocode } = setUpGoogleMapsMock();
         (window as any).initMap();
         await flushPromises();
         expect(geocode).toHaveBeenCalledWith(
-          { address: 'tel aviv' },
-          expect.anything(),
+          { address: "tel aviv" },
+          expect.anything()
         );
         expect(await result).toEqual([
-          { geometry: { location: { lat: 1, lng: 2 } } },
+          { geometry: { location: { lat: 1, lng: 2 } } }
         ]);
       });
 
-      it('should handle an object as the request as well', async () => {
+      it("should handle an object as the request as well", async () => {
         const client = new GoogleMapsBasicClient();
-        const result = client.geocode(CLIENT_ID, LANG, { address: 'tel aviv' });
+        const result = client.geocode(CLIENT_ID, LANG, { address: "tel aviv" });
         const { geocode } = setUpGoogleMapsMock();
         (window as any).initMap();
         await flushPromises();
         expect(geocode).toHaveBeenCalledWith(
-          { address: 'tel aviv' },
-          expect.anything(),
+          { address: "tel aviv" },
+          expect.anything()
         );
         expect(await result).toEqual([
-          { geometry: { location: { lat: 1, lng: 2 } } },
+          { geometry: { location: { lat: 1, lng: 2 } } }
         ]);
       });
 
-      it('Should throw an error if status is not OK 1', async () => {
-        setStatus('NOT_OK');
+      it("Should throw an error if status is not OK 1", async () => {
+        setStatus("NOT_OK");
         const client = new GoogleMapsBasicClient();
-        const result = client.geocode(CLIENT_ID, LANG, 'tel aviv');
+        const result = client.geocode(CLIENT_ID, LANG, "tel aviv");
         const { geocode } = setUpGoogleMapsMock();
         (window as any).initMap();
         await flushPromises();
         expect(geocode).toHaveBeenCalledWith(
-          { address: 'tel aviv' },
-          expect.anything(),
+          { address: "tel aviv" },
+          expect.anything()
         );
-        return expect(result).rejects.toBe('ERROR');
+        return expect(result).rejects.toBe("ERROR");
       });
     });
   });
