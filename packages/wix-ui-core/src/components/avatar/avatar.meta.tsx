a2c580afee436cd07408e6d2f04de0084e9e8c3a
Erez Makavy <erezm@wix.com>
2018-11-04 11:57:51 +0200

    Add meta simulations

--- packages/wix-ui-core/src/components/avatar/avatar.meta.tsx
+++ packages/wix-ui-core/src/components/avatar/avatar.meta.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
-import { Avatar } from "./avatar";
 import Registry from "@ui-autotools/registry";
+
+import { Avatar } from "./avatar";
+import User from 'wix-ui-icons-common/User';
 
 const avatarMetadata = Registry.getComponentMetadata(Avatar);
 
+// TODO: use local asset. how is asset loading done in autotools?
+const IMG_SRC_STUB = 'http://lorempixel.com/30/30/';
+
 avatarMetadata.addSim({
-  title: "default",
+  title: "nameToInitials",
   props: {
     name: 'John Doe'
+  }
+});
+
+avatarMetadata.addSim({
+  title: "icon",
+  props: {
+    icon: <User/>
+  }
+});
+
+avatarMetadata.addSim({
+  title: "image",
+  props: {
+    imgProps: {src: IMG_SRC_STUB}
   }
 });
