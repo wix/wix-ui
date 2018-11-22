20f3c2dd9e01d2600fc3460ff233c23896d8c75f
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2018-11-22 17:50:19 +0200

    added entry points import-path script

--- packages/wix-ui-core/scripts/import-path.js
+++ packages/wix-ui-core/scripts/import-path.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 const importPath = require('import-path/dist/src/scan');
 
-importPath('src/components', true);
-importPath('src/components/deprecated', true);
+importPath('src/components', true, {forcePascalCaseFormat: true});
+importPath('src/components/deprecated', true, {forcePascalCaseFormat: true});
 importPath('src/utils', true);
