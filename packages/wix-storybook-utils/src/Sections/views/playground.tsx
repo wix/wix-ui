a04a5bee24bc304101524ad3236e89729febe34e
argshook <argshook@gmail.com>
2018-12-10 22:51:59 +0200

    chore(Sections/playground): refactor

--- packages/wix-storybook-utils/src/Sections/views/playground.tsx
+++ packages/wix-storybook-utils/src/Sections/views/playground.tsx
@@ -5,18 +5,25 @@ import { StoryConfig } from '../../typings/story-config';
 
 const AutoExample = require('../../AutoExample').default;
 
-export const playground: ((
-  a: PlaygroundSection,
-  b: StoryConfig,
-) => React.ReactNode) = (section, storyConfig) => (
-    <AutoExample
-      {...{
-        parsedSource: storyConfig.metadata,
-        component: storyConfig.component,
-        componentProps: storyConfig.componentProps,
-        componentWrapper: storyConfig.componentWrapper,
-        exampleProps: storyConfig.exampleProps,
-        codeExample: storyConfig.codeExample,
-      }}
-    />
+export const playground = (
+  section: PlaygroundSection,
+  {
+    metadata,
+    component,
+    componentProps,
+    componentWrapper,
+    exampleProps,
+    codeExample,
+  }: StoryConfig,
+): React.ReactNode => (
+  <AutoExample
+    {...{
+      parsedSource: metadata,
+      component,
+      componentProps,
+      componentWrapper,
+      exampleProps,
+      codeExample,
+    }}
+  />
 );
