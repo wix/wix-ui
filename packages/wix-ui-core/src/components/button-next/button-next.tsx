85b868cb7caf91ea9ef35fa41fcf755f3615414b
mykas <mykolass@wix.com>
2018-12-13 11:22:20 +0200

    Render button as anchor

--- packages/wix-ui-core/src/components/button-next/button-next.tsx
+++ packages/wix-ui-core/src/components/button-next/button-next.tsx
@@ -2,16 +2,27 @@ import * as React from 'react';
 import { withFocusable } from '../../hocs/Focusable/FocusableHOC';
 import style from './button-next.st.css';
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<any> {
+export interface BaseButtonProps {
+  as?: any;
   /** accepts prefix icon */
   prefixIcon?: React.ReactElement<any>;
   /** accepts suffix icon  */
   suffixIcon?: React.ReactElement<any>;
   /** callback need to be applied for onFocus event */
-  focusableOnFocus?: React.FocusEventHandler<HTMLButtonElement>,
+  focusableOnFocus?: React.FocusEventHandler<HTMLButtonElement> &
+    React.FocusEventHandler<HTMLAnchorElement>;
   /** callback need to be applied for onBlur event */
-  focusableOnBlur?: React.FocusEventHandler<HTMLButtonElement>,
+  focusableOnBlur?: React.FocusEventHandler<HTMLButtonElement> &
+    React.FocusEventHandler<HTMLAnchorElement>;
 }
+
+export type AnchorButtonProps = BaseButtonProps &
+  React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+export type NativeButtonProps = BaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type ButtonProps = AnchorButtonProps | NativeButtonProps;
 
 const _addAffix = (Affix, classname) =>
   Affix &&
@@ -25,20 +36,38 @@ const _addAffix = (Affix, classname) =>
 
 const ButtonNextComponent: React.SFC<ButtonProps> = props => {
   const {
+    as,
     suffixIcon,
     prefixIcon,
     children,
-    onClick,
     focusableOnFocus,
     focusableOnBlur,
     ...rest
   } = props;
+
+  // Handle rendering anchor instead of button
+  const anchorRestProps = rest as AnchorButtonProps;
+  if (anchorRestProps.href) {
+    return (
+      <a
+        {...anchorRestProps}
+        onFocus={focusableOnFocus}
+        onBlur={focusableOnBlur}
+        {...style('root', {}, props)}
+      >
+        {_addAffix(prefixIcon, 'prefix')}
+        <span className={style.content}>{children}</span>
+        {_addAffix(suffixIcon, 'suffix')}
+      </a>
+    );
+  }
+
+  // Handle regular button rendering
   return (
     <button
-      {...rest}
+      {...rest as NativeButtonProps}
       onFocus={focusableOnFocus}
       onBlur={focusableOnBlur}
-      onClick={onClick}
       {...style('root', {}, props)}
     >
       {_addAffix(prefixIcon, 'prefix')}
