09b226eddd350fddac8631b161173817501da6d5
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2018-12-16 19:38:48 +0200

    cr fixes

--- packages/wix-ui-core/stories/index.tsx
+++ packages/wix-ui-core/stories/index.tsx
@@ -34,6 +34,7 @@ import './Input/Input.story';
 import './InputWithOptions.story';
 import './Dropdown.story';
 import './IconWithOptions.story';
+import './image.story';
 import './Label.story';
 import './LabelWithOptions.story';
 import './LinearProgressBar/index.story';
