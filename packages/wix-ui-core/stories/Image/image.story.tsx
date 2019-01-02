30cd48d2c9677eb4ad9f3d4e9a6fcce283a461e3
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2019-01-02 14:23:52 +0200

    fixes

--- packages/wix-ui-core/stories/Image/image.story.tsx
+++ packages/wix-ui-core/stories/Image/image.story.tsx
@@ -13,7 +13,7 @@ export default {
     ...style('root'),
     src:'https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg',
     errorImage: 'https://cdn.pixabay.com/photo/2016/04/24/13/24/error-1349562__340.png',
-    // onError: (event) => void,
-    // onLoad: (event) => void,
+    onError: () => 'On Error!',
+    onLoad: () => 'On Load!',
   }
 };
