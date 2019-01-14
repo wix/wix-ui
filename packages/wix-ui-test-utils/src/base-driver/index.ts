e9fd1435112985bd0b5c7dc7c2ea3d6dae40f74c
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2019-01-14 10:31:55 +0200

    changed types in driver

--- packages/wix-ui-test-utils/src/base-driver/index.ts
+++ packages/wix-ui-test-utils/src/base-driver/index.ts
@@ -1,15 +1,15 @@
 import {UniDriver} from 'unidriver';
 
-export interface BaseUniDriver {
+export interface BaseUniDriver<T> {
   /** returns true if component exists */
   exists: () => Promise<boolean>;
   /** returns the component element */
-  element: () => Promise<any>;
+  element: () => Promise<T>;
   /** click on the element */
   click: () => Promise<void>;
 }
 
-export const baseUniDriverFactory = (base: UniDriver): BaseUniDriver => {
+export const baseUniDriverFactory = (base: UniDriver): BaseUniDriver<Element> => {
   return {
     exists: async () => await base.exists(),
     element: async () => await base.getNative(),
