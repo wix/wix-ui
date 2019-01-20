41ae89b7a26fa13447eaad2764d56b3319d96350
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2019-01-20 17:25:19 +0200

    added story with resize

--- packages/wix-ui-core/stories/Image/image.story.tsx
+++ packages/wix-ui-core/stories/Image/image.story.tsx
@@ -1,5 +1,5 @@
-import {Image} from '../../src/components/image';
-// import * as React from 'react';
+import {Image, ResizedMode} from '../../src/components/image';
+import * as React from 'react';
 import style from './style.st.css';
 
 
@@ -14,19 +14,19 @@ export default {
     ...style('root'),
     src:'https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg',
     errorImage: 'https://cdn.pixabay.com/photo/2016/04/24/13/24/error-1349562__340.png',
+    resizedMoade: ResizedMode.contain 
   }
 };
+export class ImageStory extends React.Component<{}, { checked: boolean }> {
 
-// export class ImageStory extends React.Component<{}, { checked: boolean }> {
-
-//   render() {
-//     return (
-//       <div style={{height: '100px', width: '100px'}}>
-//         <Image
-//           data-hook="storybook-checkbox"
-//           src= "https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg"
-//         />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div style={{height: '100px', width: '100px'}}>
+        <Image
+          data-hook="storybook-checkbox"
+          src= "https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg"
+        />
+      </div>
+    );
+  }
+}
