1c994f6b982fe4cef4de6dea40700dc8b5a0c907
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2019-01-23 15:56:00 +0200

    changed meta file

--- packages/wix-ui-core/src/components/image/image.meta.ts
+++ packages/wix-ui-core/src/components/image/image.meta.ts
@@ -9,64 +9,7 @@ imageMetadata.addStyle(style, {name: 'style', path: 'stories/Image/style.st.css'
 
 imageMetadata
   .addSim({
-    title: 'renders provided src',
-    props: {
-      src: SRC,
-      alt: 'This is an image of 2 flamingos',
-    }
-});
-
-imageMetadata
-  .addSim({
-    title: 'renders a provided srcSet ',
-    props: {
-      srcSet: SRC,
-      alt: 'This is an image of 2 flamingos',
-    }
-});
-
-imageMetadata
-  .addSim({
-    title: 'renders a provided srcSet when src is broken ',
-    props: {
-      src: BROKEN_SRC,
-      srcSet: SRC,
-      alt: 'This is an image of 2 flamingos',
-    }
-});
-
-
-imageMetadata
-  .addSim({
-    title: 'renders a provided errorImage when src/srcSet is missing',
-    props: {
-      errorImage: ERROR_IMAGE_SRC,
-      alt: 'This is an error image',
-    }
-});
-
-imageMetadata
-  .addSim({
-    title: 'renders an empty pixel when src is broken and errorImage is missing', 
-    props: {
-      src: BROKEN_SRC,
-      alt: 'This is an empty pixel',
-    }
-}); 
-
-imageMetadata
-  .addSim({
-    title: 'renders an empty pixel when src and srcSet are broken and errorImage is missing', 
-    props: {
-      src: BROKEN_SRC,
-      srcSet: BROKEN_SRC,
-      alt: 'This is  an empty pixel',
-    }
-}); 
-
-imageMetadata
-  .addSim({
-    title: 'displays a loading state style', 
+    title: 'renders an image in a loading stage at first',
     props: {
       src: SRC,
       alt: 'This is an image of 2 flamingos',
@@ -74,7 +17,44 @@ imageMetadata
     state: {
       status: ImageStatus.loading
     }
+});
+
+imageMetadata
+  .addSim({
+    title: 'succeeded with rendering an image',
+    props: {
+      src: SRC,
+      alt: 'This is an image of 2 flamingos',
+    },
+    state: {
+      status: ImageStatus.loaded
+    }
+});
+
+imageMetadata
+  .addSim({
+    title: 'renders an empty pixel', 
+    props: {
+      src: BROKEN_SRC,
+      srcSet: BROKEN_SRC,
+      alt: 'This is an empty pixel',
+    },
+    state: {
+      status: ImageStatus.loaded
+    }
 }); 
+
+imageMetadata
+  .addSim({
+    title: 'renders an errorImage with error style',
+    props: {
+      errorImage: ERROR_IMAGE_SRC,
+      alt: 'This is an error image',
+    },
+    state: {
+      status: ImageStatus.error
+    }
+});
 
 imageMetadata
   .addSim({
