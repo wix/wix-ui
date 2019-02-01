f05c76e91f94da155e9fe9911d2d210b9769d739
argshook <argshook@gmail.com>
2019-02-01 16:16:34 +0200

    chore(GoogleMaps/handlersName): remove `module.exports`
    all keys are already individually exported. `module.exports` crashes storybook

--- packages/wix-ui-core/src/clients/GoogleMaps/handlersName.ts
+++ packages/wix-ui-core/src/clients/GoogleMaps/handlersName.ts
@@ -1,7 +1,3 @@
 export const autocompleteHandlerName = 'autocomplete';
 export const geocodeHandlerName = 'geocode';
 export const placeDetailsHandlerName = 'placeDetails';
-
-module.exports = {
-  autocompleteHandlerName, geocodeHandlerName, placeDetailsHandlerName
-};
