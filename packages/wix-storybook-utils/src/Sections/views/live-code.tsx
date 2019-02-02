3811b63e7548ad131a6c81d49e4bdaca827c4586
mykas <mykolass@wix.com>
2019-02-02 21:23:27 +0200

    Sections - liveCode pass previewProps and fix story page spacing

--- packages/wix-storybook-utils/src/Sections/views/live-code.tsx
+++ packages/wix-storybook-utils/src/Sections/views/live-code.tsx
@@ -3,10 +3,16 @@ import * as React from 'react';
 const LiveCodeExample = require('../../LiveCodeExample').default;
 import { LiveCodeSection } from '../../typings/story-section';
 
-export const liveCode: ((a: LiveCodeSection) => React.ReactNode) = ({
+export const liveCode: (a: LiveCodeSection) => React.ReactNode = ({
   source,
   components,
+  previewProps,
   compact = false,
 }) => (
-  <LiveCodeExample compact={compact} scope={components} initialCode={source} />
+  <LiveCodeExample
+    previewProps={previewProps}
+    compact={compact}
+    scope={components}
+    initialCode={source}
+  />
 );
