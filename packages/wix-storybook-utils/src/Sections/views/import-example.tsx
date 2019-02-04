f53629151af644fadc057785e563c143065c4c94
mykas <mykolass@wix.com>
2019-02-04 10:48:28 +0200

    Improve sections spacing according to zeplin specs

--- packages/wix-storybook-utils/src/Sections/views/import-example.tsx
+++ packages/wix-storybook-utils/src/Sections/views/import-example.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 
 const CodeBlock = require('../../CodeBlock').default;
 import { ImportExampleSection } from '../../typings/story-section';
+const styles = require('./styles.scss');
 
-export const importExample: ((a: ImportExampleSection) => React.ReactNode) = ({
+export const importExample: (a: ImportExampleSection) => React.ReactNode = ({
   source,
-}) => <CodeBlock source={source} />;
+}) => (
+  <div className={styles.import}>
+    <CodeBlock source={source} />
+  </div>
+);
