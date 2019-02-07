5f517608701b2c7cd455f8973127c71ac7cf6811
Artem Yavorsky <info@yavorsky.org>
2019-02-07 15:44:45 +0200

    Add additional tests for case when no WSR was found. (#1132)
    * Add additional tests for no WSR installed case.
    
    * Remove only for enablenoFullWsrRule tests.
    
    Co-Authored-By: yavorsky <info@yavorsky.org>
    
    * Remove not fail test, since here we are checking to fail in current situtation
    
    * Update tests description.

--- packages/eslint-plugin-wix-style-react/tests/lib/rules/no-wsr-runner.js
+++ packages/eslint-plugin-wix-style-react/tests/lib/rules/no-wsr-runner.js
@@ -9,9 +9,10 @@ const execa = require('execa');
 try {
   const test = execa.shellSync('mocha --recursive --colors .', {
     stderr: 'inherit',
-    cwd: path.resolve(__dirname, '../../fixtures/disable-no-full-wsr-lib')
+    cwd: path.resolve(__dirname, '../../fixtures/no-wsr')
   });
-  console.log('Fixtures for `disable-no-full-wsr-lib` passing: ', test.stdout);
+  console.log('Fixtures for `no-wsr` passing: ', test.stdout);
 } catch(e) {
+  console.log(e);
   throw e.stdout;
 }
