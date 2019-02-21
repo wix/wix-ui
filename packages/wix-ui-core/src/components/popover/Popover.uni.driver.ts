26529afbc52738f1fa2080ce446c0a9db3b11696
nitayneeman <nitayneeman@users.noreply.github.com>
2019-02-21 16:29:50 +0200

    test(Popver): using `body` param instead of invoking the legacy methods

--- packages/wix-ui-core/src/components/popover/Popover.uni.driver.ts
+++ packages/wix-ui-core/src/components/popover/Popover.uni.driver.ts
@@ -1,22 +1,27 @@
 import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
 import { Simulate } from 'react-dom/test-utils';
+import { UniDriver } from 'unidriver';
+
 import { popoverPrivateDriverFactory } from './Popover.private.driver';
 
-export const testkit = (base, body) => {
+export const testkit = (base: UniDriver, body: UniDriver) => {
   const byHook = hook => base.$(`[data-hook="${hook}"]`);
   const legacyDriver = async () =>
     popoverPrivateDriverFactory({
       element: await base.getNative(),
       eventTrigger: Simulate,
     });
+  const getContentElement = async () =>
+    (await body.getNative()).querySelector('[data-hook="popover-content"]');
 
   return {
     ...baseUniDriverFactory(base),
     getTargetElement: async () => byHook('popover-element').getNative(),
 
-    getContentElement: async () => (await legacyDriver()).getContentElement(),
+    getContentElement,
 
-    getPortalElement: async () => (await legacyDriver()).getPortalElement(),
+    getPortalElement: async () =>
+      (await body.getNative()).querySelector('[data-hook="popover-portal"]'),
 
     /** Returns `true` whether the target element (`<Popover.Element/>`) exists */
     isTargetElementExists: async () => byHook('popover-element').exists(),
@@ -33,7 +38,8 @@ export const testkit = (base, body) => {
         new Event('mousedown'),
       ),
 
-    clickOnContent: async () => (await legacyDriver()).clickOnContent(),
+    clickOnContent: async () =>
+      (await getContentElement()).dispatchEvent(new Event('mousedown')),
 
     getArrowOffset: async () => {
       const arrowElement = await byHook('popover-arrow');
