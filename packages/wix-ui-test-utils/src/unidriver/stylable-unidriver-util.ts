86aa134b0df940326aed57965bcc4657352777c1
Erez Makavy <erezm@wix.com>
2019-02-24 08:30:53 +0200

    Fix lint

--- packages/wix-ui-test-utils/src/unidriver/stylable-unidriver-util.ts
+++ packages/wix-ui-test-utils/src/unidriver/stylable-unidriver-util.ts
@@ -1,16 +1,16 @@
-import { UniDriver } from 'unidriver';
-import { RuntimeStylesheet, StateValue } from '@stylable/runtime';
+import {UniDriver} from 'unidriver';
+import {RuntimeStylesheet, StateValue} from '@stylable/runtime';
 
 /**
  * This is an implementation of StylableDOMUtil for Unidriver.
- * 
+ *
  * Work-In-Progress: Not all methods are implemented yet !
  */
 export class StylableUnidriverUtil {
   constructor(private style: RuntimeStylesheet) { }
 
   public async hasStyleState(base: UniDriver, stateName: string, param: StateValue = true): Promise<boolean> {
-    const { stateKey, styleState } = this.getStateDataAttrKey(stateName, param);
+    const {stateKey, styleState} = this.getStateDataAttrKey(stateName, param);
     const actual = await base.attr(stateKey);
     return String(styleState[stateKey]) === actual;
   }
@@ -21,12 +21,12 @@ export class StylableUnidriverUtil {
    * @returns state or null if not found
    */
   public async getStyleState(base: UniDriver, stateName: string) {
-    const { stateKey } = this.getStateDataAttrKey(stateName);
+    const {stateKey} = this.getStateDataAttrKey(stateName);
     return base.attr(stateKey);
   }
-  
+
   private getStateDataAttrKey(state: string, param: StateValue = true) {
-    const styleState = this.style.$cssStates({ [state]: param });
-    return { stateKey: Object.keys(styleState)[0], styleState };
+    const styleState = this.style.$cssStates({[state]: param});
+    return {stateKey: Object.keys(styleState)[0], styleState};
   }
 }
