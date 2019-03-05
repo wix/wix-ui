4c90ec81fba3658fc77dd7489c143966c89f578b
Nitay Rabinovich <nitayr@wix.com>
2019-03-05 16:15:49 +0200

    react-ui-test-utils - Add body to createUniDriverTestkitFactory (#1183)
    - Add body to protractor factory.
    - Add body to puppeteer factory.

--- packages/wix-ui-test-utils/src/protractor/protractor.ts
+++ packages/wix-ui-test-utils/src/protractor/protractor.ts
@@ -6,19 +6,20 @@ import {BaseUniDriver} from '../base-driver';
 export function protractorTestkitFactoryCreator<T>(
   driverFactory: (e: ElementFinder) => T
 ) {
-  return (obj: { dataHook: string; wrapper?: ElementFinder }) =>
+  return (obj: {dataHook: string; wrapper?: ElementFinder}) =>
     obj.wrapper
       ? driverFactory(obj.wrapper.$(`[data-hook='${obj.dataHook}']`))
       : driverFactory($(`[data-hook='${obj.dataHook}']`));
 }
 
 export function protractorUniTestkitFactoryCreator<T extends BaseUniDriver>(
-  driverFactory: (base: UniDriver) => T
+  driverFactory: (base: UniDriver, body: UniDriver) => T
 ) {
-  return (obj: { dataHook: string }) => {
+  return (obj: {dataHook: string}) => {
     const base = protractorUniDriver(
       async () => await $(`[data-hook='${obj.dataHook}']`)
     );
-    return driverFactory(base);
+    const body = protractorUniDriver(async () => await $('body'));
+    return driverFactory(base, body);
   };
 }
