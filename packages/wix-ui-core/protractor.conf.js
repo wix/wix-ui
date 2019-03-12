b99ad57aed438f798f4650d091459bd12da4470a
Artem Yavorsky <artem@yavorsky.org>
2019-03-12 17:09:26 +0200

    Remove tm

--- packages/wix-ui-core/protractor.conf.js
+++ packages/wix-ui-core/protractor.conf.js
@@ -1,5 +1,3 @@
 const {baseProtractorConfig} = require('wix-ui-test-utils/protractor');
 
-baseProtractorConfig.jasmineNodeOpts = baseProtractorConfig.jasmineNodeOpts || {};
-baseProtractorConfig.jasmineNodeOpts.defaultTimeoutInterval = 25000;
 exports.config = baseProtractorConfig;
