ac917e1ba8b1eeff2adcdcbda254eb17605fa64c
Erez Makavy <erezm@wix.com>
2019-04-02 15:12:34 +0300

    wix-ui-core: import `UniDriver` from `wix-ui-test-utils` (#1218)

--- packages/wix-ui-core/src/components/button-next/button-next.driver.ts
+++ packages/wix-ui-core/src/components/button-next/button-next.driver.ts
@@ -2,7 +2,7 @@ import {
   BaseUniDriver,
   baseUniDriverFactory
 } from 'wix-ui-test-utils/base-driver';
-import { UniDriver } from 'unidriver';
+import { UniDriver } from 'wix-ui-test-utils/unidriver';
 import {StylableUnidriverUtil} from '../../../test/StylableUnidriverUtil';
 import styles from './button-next.st.css';
 
