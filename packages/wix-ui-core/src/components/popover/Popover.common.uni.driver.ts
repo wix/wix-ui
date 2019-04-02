43731ce662a4691b02d31ad5c19ebc5019e6c35f
Erez Makavy <erezm@wix.com>
2019-04-02 17:15:12 +0300

    Revert Popover.common unidriver upgrade

--- packages/wix-ui-core/src/components/popover/Popover.common.uni.driver.ts
+++ packages/wix-ui-core/src/components/popover/Popover.common.uni.driver.ts
@@ -3,7 +3,9 @@ import { UniDriver } from 'wix-ui-test-utils/unidriver';
 export const CommonDriver = (base: UniDriver, body: UniDriver ) => {
   const queryDocumentOrElement= async (query: string) => {
      const elm = base.$$(query).get(0);
-     if (await elm.exists()) {
+     // Workaround a UniDriver bug: in this case elm.exists() returns true
+     const elmExists = !!(await elm.getNative());
+     if (elmExists) {
        return elm;
      } else {
        return body.$(query);
