f48c915d2d1d6a87d815756b14444b1831dc40db
Erez Makavy <erezm@wix.com>
2019-04-02 15:33:12 +0300

    wix-ui-test-utils: upgrade unidriver (#1223)

--- packages/wix-ui-test-utils/src/base-driver/index.ts
+++ packages/wix-ui-test-utils/src/base-driver/index.ts
@@ -3,7 +3,7 @@
  * Importing directly from this file is deprecated. Please import from `wix-ui-test-utils/unidriver`.
  */
 
-import {UniDriver} from 'unidriver';
+import {UniDriver} from '@unidriver/core';
 
 export interface BaseUniDriver {
   /** returns true if component exists */
