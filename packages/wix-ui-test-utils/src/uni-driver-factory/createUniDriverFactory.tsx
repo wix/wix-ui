b7d06f0ee1aec0e9a04aea314d287ab68a74c048
Erez Makavy <erezm@wix.com>
2019-04-02 17:12:34 +0300

    wix-ui-test-utils: revert unidriver upgrade

--- packages/wix-ui-test-utils/src/uni-driver-factory/createUniDriverFactory.tsx
+++ packages/wix-ui-test-utils/src/uni-driver-factory/createUniDriverFactory.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 import {BaseUniDriver} from '../base-driver';
-import {UniDriver} from '@unidriver/core';
-import {jsdomReactUniDriver} from '@unidriver/jsdom-react';
+import {UniDriver} from 'unidriver';
+import {reactUniDriver} from 'unidriver/react';
 
 export type UniDriverFactory<TDriver extends BaseUniDriver> = (base: UniDriver) => TDriver;
 
 function componentFactory(Component: React.ReactElement<any>): UniDriver {
   const wrapperDiv = document.createElement('div');
   render(Component, wrapperDiv);
-  const base = jsdomReactUniDriver(wrapperDiv.childNodes[0] as Element);
+  const base = reactUniDriver(wrapperDiv.childNodes[0] as Element);
   return base;
 }
 
--- packages/wix-ui-test-utils/src/unidriver/index.ts
+++ packages/wix-ui-test-utils/src/unidriver/index.ts
@@ -1,3 +1,3 @@
-export {UniDriver} from '@unidriver/core';
+export { UniDriver } from 'unidriver';
 export * from './stylable-unidriver-util';
 export * from '../base-driver';
