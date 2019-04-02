accdb0f015c69b4fcb83f5fdc022724723e824d4
Artem Yavorsky <artem@yavorsky.org>
2019-04-02 14:33:36 +0300

    Add index.ts for convinient named imports
    # Conflicts:
    #	packages/wix-ui-core/src/components/loadable/index.ts

---
+++ packages/wix-ui-core/src/index.ts
@@ -0,0 +1,90 @@
+export { AddressInput, AddressInputProps } from './components/address-input';
+export { Autocomplete, AutocompleteProps } from './components/autocomplete';
+export { Avatar, AvatarProps } from './components/avatar';
+export { ButtonNext } from './components/button-next';
+export { GoogleMapsIframeClient } from './clients/GoogleMaps';
+export {
+  Captcha,
+  CaptchaProps,
+  Size,
+  CaptchaType,
+  Theme,
+  CaptchaLang,
+} from './components/captcha';
+export { Checkbox, CheckboxProps } from './components/checkbox';
+export {
+  CircularProgressBar,
+  CircularProgressBarProps,
+} from './components/circular-progress-bar';
+export { Dropdown, DropdownProps } from './components/dropdown';
+export {
+  DropdownContent,
+  DropdownContentProps,
+} from './components/dropdown-content';
+export {
+  DropdownOption,
+  DropdownOptionType,
+  DropdownOptionProps,
+  OptionFactory,
+  Option,
+  DividerArgs,
+} from './components/dropdown-option';
+export { IconWithOptions } from './components/icon-with-options';
+export { Image } from './components/image';
+export { Input, InputProps, AriaAutoCompleteType } from './components/input';
+export {
+  InputWithOptions,
+  InputWithOptionsProps,
+} from './components/input-with-options';
+export {
+  LabelWithOptions,
+  LabelWithOptionsProps,
+} from './components/label-with-options';
+export {
+  LinearProgressBar,
+  LinearProgressBarProps,
+} from './components/linear-progress-bar';
+export { MenuItem, MenuItemProps } from './components/menu-item';
+export { NavStepper, NavStepperProps } from './components/nav-stepper';
+export { Pagination, PaginationProps } from './components/pagination';
+export {
+  Placement,
+  AppendTo,
+  PopoverProps,
+  PopoverState,
+  PopoverType,
+  Popover,
+} from './components/popover';
+export {
+  RadioButton,
+  RadioButtonChangeEvent,
+  RadioButtonHoverEvent,
+} from './components/radio-button';
+export { Slider, SliderProps } from './components/slider';
+export {
+  StepProps,
+  GetStepProps,
+  ChildrenArgs,
+  StepperProps,
+  Stepper,
+} from './components/stepper';
+export { Thumbnail, ThumbnailProps } from './components/thumbnail';
+export {
+  TimePickerConstants,
+  TimePickerUtils,
+  TimePicker,
+  TimePickerProps,
+} from './components/time-picker';
+export { ToggleSwitch, ToggleSwitchProps } from './components/toggle-switch';
+export {
+  Point,
+  TooltipProps,
+  TooltipState,
+  TooltipComponent,
+  Tooltip,
+} from './components/tooltip';
+export { Video, IVideoProps } from './components/video';
+export { createHOC } from './createHOC';
+export { withEllipsedTooltip } from './hocs/EllipsedTooltip';
+export { withFocusable } from './hocs/Focusable';
+export { withStylable } from './utils/withStylable';
