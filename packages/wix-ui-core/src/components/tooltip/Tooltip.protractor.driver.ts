ac00681fc32388d581d6ae82787a46b848620039
Mykolas Sindeikis <mykolass@wix.com>
2019-04-03 09:51:21 +0300

    <Tooltip/> - enhance functionalities (#1216)
    * Add react-testing-library and render container to test with
    
    * <Tooltip/> - add a11y support and some lint changes
    
    * <Tooltip/> - add unit tests for onHide & onShow
    
    * <Tooltip/> - add correct typings for elements
    
    * <Tooltip/> - add tooltip content tests
    
    * <Tooltip/> - add additional unit tests and fix sanity tests
    
    * <Tooltip/> - add children prop unit tests
    
    * <Tooltip/> - handle focusablehoc methdos
    
    * <Tooltip/> - typos
    
    * <Tooltip/> - remove only on tests
    
    * <Tooltip/> - change internal methods usage to be with underscore

--- packages/wix-ui-core/src/components/tooltip/Tooltip.protractor.driver.ts
+++ packages/wix-ui-core/src/components/tooltip/Tooltip.protractor.driver.ts
@@ -1,17 +1,23 @@
-import {ILocation} from 'wix-ui-test-utils/protractor';
-import {BaseDriver, DriverFactory} from '../../common/BaseDriver.protractor';
-import {popoverDriverFactory, PopoverDriver} from '../popover/Popover.protractor.driver';
+import { ILocation } from 'wix-ui-test-utils/protractor';
+import { BaseDriver, DriverFactory } from '../../common/BaseDriver.protractor';
+import {
+  popoverDriverFactory,
+  PopoverDriver,
+} from '../popover/Popover.protractor.driver';
 
 export interface TooltipDriver extends PopoverDriver {
-  getTooltipLocation: () => Promise<ILocation>;
+  getTooltipLocation(): Promise<ILocation>;
 }
 
 export const tooltipDriverFactory: DriverFactory<TooltipDriver> = component => {
   const popoverDriver = popoverDriverFactory(component);
 
-  return Object.assign(
-    {},
-    popoverDriver, {
-    getTooltipLocation: async () =>  popoverDriver.getContentElement().getWebElement().getLocation()
-  });
+  return {
+    ...popoverDriver,
+    getTooltipLocation: async () =>
+      popoverDriver
+        .getContentElement()
+        .getWebElement()
+        .getLocation(),
+  };
 };
--- packages/wix-ui-core/src/components/tooltip/Tooltip.spec.tsx
+++ packages/wix-ui-core/src/components/tooltip/Tooltip.spec.tsx
@@ -1,50 +1,129 @@
 import * as React from 'react';
-import * as eventually from 'wix-eventually';
-import {tooltipDriverFactory} from './Tooltip.driver';
-import {isEnzymeTestkitExists} from 'wix-ui-test-utils/enzyme';
-import {ReactDOMTestContainer} from '../../../test/dom-test-container';
-import {isTestkitExists} from 'wix-ui-test-utils/vanilla';
-import {Tooltip} from './';
-import {tooltipTestkitFactory} from '../../testkit';
-import {tooltipTestkitFactory as enzymeTooltipTestkitFactory} from '../../testkit/enzyme';
-import {mount} from 'enzyme';
+import {
+  createRendererWithUniDriver,
+  cleanup,
+} from '../../../test/utils/react';
+
+import { tooltipPrivateDriverFactory } from './Tooltip.private.uni.driver';
+
+import { ButtonNext } from '../button-next';
+import { Tooltip } from './';
 
 describe('Tooltip', () => {
-  const createDriver =
-    new ReactDOMTestContainer()
-    .unmountAfterEachTest()
-    .createLegacyRenderer(tooltipDriverFactory);
+  const render = createRendererWithUniDriver(tooltipPrivateDriverFactory);
 
-  const createTooltip = (props = {}) => (
-    <Tooltip placement="top" {...props} content={<span>Hovered Content</span>}>
-      <div>
-        Element
-      </div>
-    </Tooltip>
+  const tooltip = (props = {}) => (
+    <Tooltip
+      placement="top"
+      timeout={0}
+      content="Hovered Content"
+      children={<div>Element</div>}
+      {...props}
+    />
   );
+  afterEach(() => cleanup());
 
-  it('should not display content by default', () => {
-    const driver = createDriver(createTooltip());
-    expect(driver.isContentElementExists()).toBeFalsy();
+  it('should be hidden by default', async () => {
+    const { driver } = render(tooltip());
+    expect(await driver.tooltipExists()).toBe(false);
   });
 
-  it('should display content on hover and hide it on leave', async () => {
-    const driver = createDriver(createTooltip());
-    driver.mouseEnter();
-    expect(driver.isContentElementExists()).toBeTruthy();
-    driver.mouseLeave();
-    await eventually(() => expect(driver.isContentElementExists()).toBeFalsy());
-  });
+  describe('Mouse events', () => {
+    it('tooltip should be visible on mouse over', async () => {
+      const { driver } = render(tooltip());
+      await driver.mouseEnter();
+      expect(await driver.tooltipExists()).toBe(true);
+    });
 
-  describe('testkit', () => {
-    it('should exist', () => {
-      expect(isTestkitExists(createTooltip(), tooltipTestkitFactory)).toBe(true);
+    it('tooltip should hidden on mouse leave', async () => {
+      const { driver } = render(tooltip());
+      await driver.mouseEnter();
+      expect(await driver.tooltipExists()).toBe(true);
+      await driver.mouseLeave();
+      expect(await driver.tooltipExists()).toBe(false);
     });
   });
 
-  describe('enzyme testkit', () => {
-    it('should exist', () => {
-      expect(isEnzymeTestkitExists(createTooltip(), enzymeTooltipTestkitFactory, mount)).toBe(true);
+  describe('Keyboard events', () => {
+    it('tooltip should be visible on keyboard focus', async () => {
+      const { driver } = render(
+        tooltip({ children: <ButtonNext>Button</ButtonNext> }),
+      );
+      await driver.tabIn();
+      expect(await driver.tooltipExists()).toBe(true);
+    });
+
+    it('tooltip should be hidden on keyboard blur event', async () => {
+      const { driver } = render(tooltip());
+      expect(await driver.tooltipExists()).toBe(false);
+      await driver.tabIn();
+      expect(await driver.tooltipExists()).toBe(true);
+      await driver.tabOut();
+      expect(await driver.tooltipExists()).toBe(false);
+    });
+  });
+
+  describe('`onShow` & `onHide` props', () => {
+    it('should call onShow on mouse enter', async () => {
+      const onShow = jest.fn();
+      const { driver } = render(tooltip({ onShow }));
+      await driver.mouseEnter();
+      expect(onShow).toHaveBeenCalled();
+    });
+
+    it('should call onHide on mouse leave', async () => {
+      const onHide = jest.fn();
+      const { driver } = render(tooltip({ onHide }));
+      await driver.mouseEnter();
+      await driver.mouseLeave();
+      expect(onHide).toHaveBeenCalled();
+    });
+  });
+
+  describe('`content` prop', () => {
+    it('should render when given string', async () => {
+      const content = 'string';
+      const { driver } = render(tooltip({ content }));
+      await driver.mouseEnter();
+      expect(await driver.getTooltipText()).toBe(content);
+    });
+    it('should render when given react element', async () => {
+      const content = <div>kido</div>;
+      const { driver } = render(tooltip({ content }));
+      await driver.mouseEnter();
+      expect(await driver.getTooltipText()).toBe('kido');
+    });
+  });
+
+  describe('`children` prop', () => {
+    it('should render when given string', async () => {
+      const children = 'kido';
+      const { driver } = render(tooltip({ children }));
+      expect(await driver.getTargetText()).toBe(children);
+    });
+    it('should render when given react element', async () => {
+      const children = <div>kido</div>;
+      const { driver } = render(tooltip({ children }));
+      expect(await driver.getTargetText()).toBe('kido');
+    });
+  });
+
+  describe('`onClickOutside` & `shouldCloseOnClickOutside` props', () => {
+    it('should call onClickOutside when clicked outside', async () => {
+      const onClickOutside = jest.fn();
+      const { driver } = render(tooltip({ onClickOutside }));
+      await driver.clickOutside();
+      expect(onClickOutside).toHaveBeenCalled();
+    });
+
+    it('should close tooltip when given shouldCloseOnClickOutside', async () => {
+      const onClickOutside = jest.fn();
+      const { driver } = render(
+        tooltip({ onClickOutside, shouldCloseOnClickOutside: true }),
+      );
+      await driver.mouseEnter();
+      await driver.clickOutside();
+      expect(await driver.tooltipExists()).toBe(false);
     });
   });
 });
