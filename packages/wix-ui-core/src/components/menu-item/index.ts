16e5ee3d9881a76b1c510c627d6820711fbea50c
Artem Yavorsky <info@yavorsky.org>
2019-04-15 08:24:38 -0400

    Add named-imports (#1241)

---
+++ packages/wix-ui-core/src/components/menu-item/index.ts
@@ -0,0 +1,1 @@
+export { MenuItem, MenuItemProps } from './menu-item';
--- packages/wix-ui-core/src/components/time-picker/index.ts
+++ packages/wix-ui-core/src/components/time-picker/index.ts
@@ -1,4 +1,4 @@
 export { TimePicker, TimePickerProps } from './TimePicker';
 import * as TimePickerConstants from './constants';
 import * as TimePickerUtils from './utils';
-export { TimePickerConstants, TimePickerUtils };
+export {TimePickerConstants, TimePickerUtils};
