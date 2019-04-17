db93c90f30f6d7895e3cbaed7541f49926e841ff
Nitay Rabinovich <nitayr@wix.com>
2019-04-17 18:49:38 +0300

    Export puppeteer drivers (#1245)
    - Export UniDrivers as Puppeteer drivers

---
+++ packages/wix-ui-core/src/testkit/puppeteer.ts
@@ -0,0 +1,46 @@
+import {puppeteerUniTestkitFactoryCreator} from 'wix-ui-test-utils/puppeteer';
+
+import {
+  avatarDriverFactory,
+  AvatarDriver,
+} from '../components/avatar/avatar.driver';
+export const avatarTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  avatarDriverFactory
+);
+export {AvatarDriver};
+
+import {
+  buttonNextDriverFactory,
+  ButtonNextDriver,
+} from '../components/button-next/button-next.driver';
+export const buttonNextTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  buttonNextDriverFactory
+);
+export {ButtonNextDriver};
+
+import {
+  captchaDriverFactory,
+  CaptchaDriver,
+} from '../components/captcha/Captcha.driver';
+export const captchaTestkitFactory = puppeteerUniTestkitFactoryCreator<
+  CaptchaDriver
+>(captchaDriverFactory);
+export {CaptchaDriver};
+
+import {
+  filePickerButtonUniDriverFactory,
+  FilePickerButtonUniDriver,
+} from '../components/file-picker-button/test/FilePickerButton.uni.driver';
+export const filePickerButtonTestkitFactory = puppeteerUniTestkitFactoryCreator(
+  filePickerButtonUniDriverFactory
+);
+export {FilePickerButtonUniDriver};
+
+import {
+  LinearProgressBarUniDriver,
+  linearProgressBarUniDriverFactory,
+} from '../components/linear-progress-bar/LinearProgressBar.uni.driver';
+export const linearProgressBarTestkitFactory = puppeteerUniTestkitFactoryCreator<
+  LinearProgressBarUniDriver
+>(linearProgressBarUniDriverFactory);
+export {LinearProgressBarUniDriver};
