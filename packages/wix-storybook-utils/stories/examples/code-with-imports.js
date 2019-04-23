07173b901e3b7730b33155fe215f919ad342112c
argshook <argshook@gmail.com>
2019-04-23 16:35:08 +0300

    feat(LiveCodeExample): do nothing with import and require statements and exclude lint comments

---
+++ packages/wix-storybook-utils/stories/examples/code-with-imports.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+/* prettier-ignore */
+/*eslint-disable*/
+/* eslint-enable */
+/* eslint-disable hello-kitty */
+/** eslint-disable */
+/* eslint-disable hello-kitty */
+// eslint-disable-next-line
+/* prettier-ignore */
+
+import Something from 'some-library/Something';
+
+const SomethingElse = require('some-library/SomethingElse');
+
+/* tslint:enable:rule1 rule2 rule3... */
+// tslint:disable-next-line
+// prettier-ignore
+<div>Lorem nihil ea adipisci possimus?</div>;
