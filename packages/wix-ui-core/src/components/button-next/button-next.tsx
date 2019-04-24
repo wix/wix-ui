0306f793f66044820dca6c2c3418045983c3162d
nitayneeman <nitayneeman@users.noreply.github.com>
2019-04-24 07:17:24 +0300

    <ButtonNext/> - using classNames

--- packages/wix-ui-core/src/components/button-next/button-next.tsx
+++ packages/wix-ui-core/src/components/button-next/button-next.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as classNames from 'classnames';
 
 import style from './button-next.st.css';
 
@@ -22,7 +23,7 @@ export interface ButtonProps
 const _addAffix = (Affix, styleClass) =>
   Affix &&
   React.cloneElement(Affix, {
-    className: `${style[styleClass]} ${Affix.props.className}`,
+    className: classNames(style[styleClass], Affix.props.className),
   });
 
 /**
