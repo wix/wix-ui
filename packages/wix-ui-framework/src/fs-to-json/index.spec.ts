0121d712117a4550c1d0f47636fa94c104841da8
argshook <argshook@gmail.com>
2019-05-08 00:18:07 +0300

    feat(fsToJson): add filesystem to json function

---
+++ packages/wix-ui-framework/src/fs-to-json/index.spec.ts
@@ -0,0 +1,71 @@
+import * as cista from 'cista';
+import { fsToJson } from './index';
+
+describe('fsToJson', () => {
+  describe('given non existing path', () => {
+    it('should reject with error', () => {
+      const fsTree = {
+        'src/hello': '',
+      };
+      const fakeFs = cista(fsTree);
+      return expect(
+        fsToJson({ cwd: fakeFs.dir, path: 'src/goodbye' }),
+      ).rejects.toThrowError();
+    });
+  });
+  describe('given existing path', () => {
+    it('should resolve with json representation of file system', async () => {
+      const fsTree = {
+        'templates/component/hello.js': 'a',
+        'src/matching-comonent/hello.js': '',
+        'src/non-matching-component/goodbye.js': '',
+      };
+      const fakeFs = cista(fsTree);
+      return expect(fsToJson({ cwd: fakeFs.dir, path: '.' })).resolves.toEqual({
+        templates: {
+          component: {
+            'hello.js': '',
+          },
+        },
+        src: {
+          'matching-comonent': {
+            'hello.js': '',
+          },
+          'non-matching-component': {
+            'goodbye.js': '',
+          },
+        },
+      });
+    });
+
+    it('should resolve with json with content when using `withContent`', async () => {
+      const fsTree = {
+        'templates/component/hello.js': 'a',
+        'src/matching-comonent/hello.js': 'b',
+        'src/non-matching-component/goodbye.js': 'c',
+      };
+      const fakeFs = cista(fsTree);
+      return expect(
+        fsToJson({
+          cwd: fakeFs.dir,
+          path: '.',
+          options: { withContent: true },
+        }),
+      ).resolves.toEqual({
+        templates: {
+          component: {
+            'hello.js': 'a',
+          },
+        },
+        src: {
+          'matching-comonent': {
+            'hello.js': 'b',
+          },
+          'non-matching-component': {
+            'goodbye.js': 'c',
+          },
+        },
+      });
+    });
+  });
+});
---
+++ packages/wix-ui-framework/src/fs-to-json/index.ts
@@ -0,0 +1,67 @@
+import * as fs from 'fs';
+import { promisify } from 'util';
+import { resolve as pathResolve } from 'path';
+
+interface Params {
+  cwd: string;
+  path: string;
+  options?: {
+    withContent?: boolean;
+  };
+}
+
+const fsAccess = promisify(fs.access);
+const fsReaddir = promisify(fs.readdir);
+const fsReadFile = promisify(fs.readFile);
+const fsStat = promisify(fs.stat);
+
+const rejectNonExisting = fn => async params => {
+  const realPath = pathResolve(params.cwd, params.path);
+
+  try {
+    await fsAccess(realPath, fs.constants.F_OK);
+    return fn(params);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+const fsToJsonUnguarded: (a: Params) => Promise<object> = async ({
+  cwd,
+  path,
+  options = {},
+}) => {
+  const recursion = ({ entries, entryCwd }) =>
+    entries.reduce(
+      (accPromise, entry) =>
+        accPromise.then(async acc => {
+          const entryPath = pathResolve(entryCwd, entry);
+          const isFolder = (await fsStat(entryPath)).isDirectory();
+
+          return isFolder
+            ? {
+                ...acc,
+                [entry]: await recursion({
+                  entries: await fsReaddir(entryPath),
+                  entryCwd: entryPath,
+                }),
+              }
+            : {
+                ...acc,
+                [entry]: options.withContent
+                  ? await fsReadFile(entryPath, 'utf8')
+                  : '',
+              };
+        }),
+      Promise.resolve({}),
+    );
+
+  const json = await recursion({
+    entries: fs.readdirSync(pathResolve(cwd, path)),
+    entryCwd: cwd,
+  });
+
+  return Promise.resolve(json);
+};
+
+export const fsToJson = rejectNonExisting(fsToJsonUnguarded);
