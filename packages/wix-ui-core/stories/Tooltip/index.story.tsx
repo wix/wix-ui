64ec24bb366366edec9aadbc13e2c3a69ba2fd4b
Mykolas Sindeikis <mykolass@wix.com>
2019-05-07 11:18:49 +0300

    <Tooltip/> - improve Tooltip documentation (#1288)
    * <Tooltip/> - improve Tooltip documentation
    
    * <Tooltip/> - remove wix-style-react references
    
    * <Tooltip/> - fix build error
    
    * <Tooltip/> - again build errors
    
    * Remove index.ts from ignore list
    
    * Remove index.ts
    
    * Move generating index.ts command to the begining of the yoshi build

--- packages/wix-ui-core/stories/Tooltip/index.story.tsx
+++ packages/wix-ui-core/stories/Tooltip/index.story.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
-import {Tooltip} from '../../src/components/tooltip';
+import {
+  header,
+  tabs,
+  tab,
+  description,
+  columns,
+  playground,
+  api,
+  testkit,
+  importExample,
+  divider,
+  code,
+  title,
+} from 'wix-storybook-utils/Sections';
 
-const childrenExamples = [
-  {label: 'Simple text',
-    value: <span>Hover me for a tooltip!</span>
-  }
-];
+import { Tooltip } from '../../src/components/tooltip';
+import { ButtonNext } from '../../src/components/button-next';
+import { baseScope } from '../utils';
 
-const contentExamples = [
-  {label: 'Simple text',
-    value: <span>This is my tooltip</span>
-  }
-];
+import * as examples from './examples';
+
+const liveCode = config =>
+  code({
+    previewProps: {
+      style: { backgroundColor: '#f0f4f7' },
+    },
+    compact: true,
+    components: baseScope,
+    ...config,
+  });
+
+const example = ({ source, ...rest }) =>
+  columns([description({ ...rest }), liveCode({ source })]);
 
 export default {
   category: 'Components',
@@ -21,14 +41,12 @@ export default {
 
   componentProps: {
     'data-hook': 'story-tooltip-right',
-    content: contentExamples[0].value,
-    children: childrenExamples[0].value,
-    placement: 'right'
+    content: 'This is my tooltip',
+    children: 'Hover me for a tooltip',
+    placement: 'top',
   },
 
   exampleProps: {
-    children: childrenExamples,
-    content: contentExamples,
     placement: [
       'auto-start',
       'auto',
@@ -44,7 +62,123 @@ export default {
       'bottom-start',
       'left-end',
       'left',
-      'left-start'
-    ]
-  }
+      'left-start',
+    ],
+  },
+
+  sections: [
+    header({
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/tree/master/src/TooltipNext/',
+      component: (
+        <Tooltip appendTo="window" content="HERE I AM! THIS IS ME!">
+          <ButtonNext skin="dark">Hover me</ButtonNext>
+        </Tooltip>
+      ),
+    }),
+
+    tabs([
+      tab({
+        title: 'Description',
+        sections: [
+          columns([
+            description(
+              'A tooltip is a popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+            ),
+          ]),
+
+          importExample("import Tooltip from 'wix-ui-core/Tooltip';"),
+
+          ...[
+            {
+              title: 'Plain Example',
+              text: 'Plain example of how to use tooltip.',
+              source: examples.basic,
+            },
+            {
+              title: 'Placement',
+              text: 'Tooltips have 14 different content placement states.',
+              source: examples.placements,
+            },
+            {
+              title: 'Delay',
+              text:
+                'Time in milliseconds to wait before showing or hiding the tooltip. Controlled by props `showDelay` or `hideDelay`.',
+              source: examples.delay,
+            },
+            {
+              title: 'Accessibility',
+              text:
+                'The tooltip content appears on keyboard focus for native focusable html elements like: `<button>` or `<input>` or components that uses `focusableHOC` from wix-ui-core',
+              source: examples.a11y,
+            },
+            {
+              title: 'Arrow',
+              text: 'The tooltip content can appear with or without the arrow.',
+              source: examples.arrow,
+            },
+          ].map(example),
+
+          title('Floating Behaviour'),
+
+          columns([
+            liveCode({
+              title: 'Flip: Enabled (default) & Fixed: Disabled (default)',
+              subtitle:
+                'Focus target element (TAB) and scroll viewport to see behaviour',
+              source: examples.flip,
+            }),
+            liveCode({
+              title: 'Flip: Disabled & Fixed: Disabled (default)',
+              subtitle:
+                'Focus target element (TAB) and scroll viewport to see behaviour',
+              source: examples.flipnot,
+            }),
+          ]),
+          columns([
+            liveCode({
+              title: 'Flip: Enabled (default) & Fixed: Enabled',
+              subtitle:
+                'Focus target element (TAB) and scroll viewport to see behaviour',
+              source: examples.fixed,
+            }),
+          ]),
+
+          title('Attachement to DOM'),
+
+          columns([
+            liveCode({
+              title: 'Append to: parent',
+              subtitle: `If you inspect the content, you'll see it is attached to a new div next to the target element.`,
+              source: examples.parent,
+            }),
+            liveCode({
+              title: 'Append to: window',
+              subtitle: `If you inspect the content, you'll see it is attached to a new <div/> under the body.`,
+              source: examples.window,
+            }),
+          ]),
+          columns([
+            liveCode({
+              title: 'Append to: viewport',
+              subtitle: `This is similar to window as it also appends the content to a new <div/> under the body, but also set its boundry to the viewport.`,
+              source: examples.viewport,
+            }),
+            liveCode({
+              title: 'Append to: scrollparent',
+              subtitle: `If you inspect the content, you'll see it is attached to a new div under the list container.`,
+              source: examples.scrollParent,
+            }),
+          ]),
+        ],
+      }),
+
+      ...[
+        { title: 'API', sections: [api()] },
+        { title: 'Testkit', sections: [testkit()] },
+        { title: 'Playground', sections: [playground()] },
+      ].map(tab),
+    ]),
+  ],
 };
