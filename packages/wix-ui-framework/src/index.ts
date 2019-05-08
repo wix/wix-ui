8ece25829f7e0dc381f36ab8696994fbdcc583e3
argshook <argshook@gmail.com>
2019-05-08 16:43:28 +0300

    chore(package.json): jest ignore cli-commands templates & codemods

--- packages/wix-ui-framework/src/index.ts
+++ packages/wix-ui-framework/src/index.ts
@@ -1,7 +1,5 @@
 import * as program from 'commander';
 import { generate } from './cli-commands/generate';
-import { fsToJson } from './fs-to-json';
-import * as fs from 'fs';
 
 // the following must be `require`
 // otherwise `dist` would contain extraneous `src` folder
@@ -24,19 +22,6 @@ program
     'Path to codemods. Default is "/generator/codemods/"',
   )
   .action(options => generate(options).catch(e => console.error(e)));
-
-program
-  .command('jsonify')
-  .option('--path <path>', 'shit')
-  .action(async options => {
-    const json = await fsToJson({
-      cwd: options.path,
-      path: '.',
-      options: { withContent: true },
-    });
-
-    fs.writeFileSync('./output.json', JSON.stringify(json, null, 2), 'utf8');
-  });
 
 if (!process.argv.slice(2).length) {
   program.help();
