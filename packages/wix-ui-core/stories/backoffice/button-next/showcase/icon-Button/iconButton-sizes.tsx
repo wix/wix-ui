c6169a87fc8c1086813b1098ae796ebbf41db904
TalZa <talza@wix.com>
2019-06-04 16:35:38 +0300

    `<IconButton/>`- Adding tiny and large styling & updating documentation (#1326)
    * adding tiny and large styling
    
    * fixing large icon width
    
    * dummy commit

--- packages/wix-ui-core/stories/backoffice/button-next/showcase/icon-Button/iconButton-sizes.tsx
+++ packages/wix-ui-core/stories/backoffice/button-next/showcase/icon-Button/iconButton-sizes.tsx
@@ -2,30 +2,45 @@ import * as React from 'react';
 
 import CodeShowcase from 'wix-storybook-utils/CodeShowcase';
 import { ButtonNext } from '../../../../../src/components/button-next';
-import { backofficeTheme } from '../../../../../src/themes/backoffice';
 import { iconButton } from '../../../../../src/themes/backoffice';
 import More from 'wix-ui-icons-common/More';
+import MoreSmall from 'wix-ui-icons-common/MoreSmall';
+
+
+const iconButtonSizes = {
+    tiny: { size: 'tiny' , px: '18' },
+    small: { size: 'small' , px: '18' },
+    medium: { size: 'medium' , px: '24' },
+    large: { size: 'large' , px: '24'}
+};
 
 const example = `import * as React from "react";
 import { ButtonNext } from "wix-ui-core/button-next";
 import { iconButton } from "wix-ui-core/themes/backoffice";
 import More from "wix-ui-icons-common/More";
+import MoreSmall from 'wix-ui-icons-common/MoreSmall';
 
 export default () => (
   <React.Fragment>
-    <ButtonNext className={iconButton("small")}>
-      <More width="24" height="24" />
+     <ButtonNext className={iconButton('${iconButtonSizes.tiny.size}')}>
+      <MoreSmall width="${iconButtonSizes.tiny.px}" height="${iconButtonSizes.tiny.px}" />
+    </ButtonNext>
+    <ButtonNext className={iconButton('${iconButtonSizes.small.size}')}>
+      <MoreSmall width="${iconButtonSizes.small.px}" height="${iconButtonSizes.small.px}" />
     </ButtonNext>
     <ButtonNext className={iconButton()}>
-      <More width="24" height="24" />
+      <More width="${iconButtonSizes.medium.px}" height="${iconButtonSizes.medium.px}" />
+    </ButtonNext>
+    <ButtonNext className={iconButton('${iconButtonSizes.large.size}')}>
+      <More width="${iconButtonSizes.large.px}" height="${iconButtonSizes.large.px}" />
     </ButtonNext>
   </React.Fragment>
 );`;
 
 const description = (
   <div>
-    IconButton supports only <code>small</code> and <code>medium</code> sizes.
-    The default value is <code>medium</code>
+    IconButton supports four main sizes: <code>{iconButtonSizes.tiny.size}</code>, <code>{iconButtonSizes.small.size}</code>, <code>{iconButtonSizes.medium.size}</code>, and <code>{iconButtonSizes.large.size}</code> sizes.
+    Default size is <code>{iconButtonSizes.medium.size}</code>
   </div>
 );
 
@@ -40,11 +55,17 @@ export const IconButtonSizes = ({ style }: IconButtonSizesProps) => (
     code={example}
     description={description}
   >
-    <ButtonNext className={iconButton('small')}>
-      <More width="24" height="24" />
+    <ButtonNext className={iconButton(iconButtonSizes.tiny.size)}>
+      <MoreSmall  width={iconButtonSizes.tiny.px} height={iconButtonSizes.tiny.px} />
+    </ButtonNext>
+    <ButtonNext className={iconButton(iconButtonSizes.small.size)}>
+      <MoreSmall width={iconButtonSizes.small.px} height={iconButtonSizes.small.px} />
     </ButtonNext>
     <ButtonNext className={iconButton()}>
-      <More width="24" height="24" />
+      <More width={iconButtonSizes.medium.px} height={iconButtonSizes.medium.px} />
+    </ButtonNext>
+    <ButtonNext className={iconButton(iconButtonSizes.large.size)}>
+       <More width={iconButtonSizes.large.px} height={iconButtonSizes.large.px} />
     </ButtonNext>
   </CodeShowcase>
 );
