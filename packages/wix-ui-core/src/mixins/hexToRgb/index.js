c5057a49df02f36beeb256e01ca04d0de77f2c92
TalZa <talza@wix.com>
2019-06-17 15:48:52 +0300

    hexToRgb- move util function to "mixin" folder (#1349)

--- packages/wix-ui-core/src/mixins/hexToRgb/index.js
+++ packages/wix-ui-core/src/mixins/hexToRgb/index.js
@@ -1,6 +1,4 @@
-/* stylable formatters can consume js files easier without compiling the formatter from ts */
-
-function hexToRgb(hex, opacity = 1) {
+module.exports = function(hex, opacity = 1) {
     let c;
     let hexRegExp = /^#([A-Fa-f0-9]{3}){1,2}$/;
 
@@ -18,8 +16,4 @@ function hexToRgb(hex, opacity = 1) {
     }
 
     throw new Error('Bad Hex Color');
-}
-
-module.exports = {
-    hexToRgb,
 };
