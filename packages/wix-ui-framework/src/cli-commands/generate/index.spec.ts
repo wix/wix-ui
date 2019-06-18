2da483fad53ecc6dbfdba58ffb9642b4c2e13080
argshook <argshook@gmail.com>
2019-06-18 13:28:08 +0300

    feat(generate): use correct relative path to `codemods` options
    also typescript definitions

--- packages/wix-ui-framework/src/cli-commands/generate/index.spec.ts
+++ packages/wix-ui-framework/src/cli-commands/generate/index.spec.ts
@@ -49,4 +49,37 @@ describe('generate', () => {
       expect(output).toEqual('Test');
     });
   });
+
+  describe('when codemods option is defined', () => {
+    it('should run codemods of specified path', async () => {
+      const fakeFs = cista({
+        'templates/src/index.js': '{%ComponentName%}',
+        'assert-file': 'hello world',
+        'custom/codemods/codemods.js':
+          'module.exports = [ { codemod: "../../codemods/deep/path/codemod.js", dist: "", description: "do the test codemod" } ]',
+        'codemods/deep/path/codemod.js': 'fake codemod',
+        'package.json': '{}',
+        'node_modules/.bin/jscodeshift':
+          '#!/usr/bin/env node\nconst fs = require("fs");\nfs.writeFileSync(__dirname + "/../../assert-file", "hello mars");',
+      });
+
+      // set permissions for fake jscodeshift so that we're able to execute and write to file
+      fs.chmodSync(`${fakeFs.dir}/node_modules/.bin/jscodeshift`, 0o500); // read and execute
+      fs.chmodSync(`${fakeFs.dir}/assert-file`, 0o600); // read & write
+
+      await generate({
+        templates: `${fakeFs.dir}/templates`,
+        ComponentName: 'Test',
+        componentName: 'test',
+        codemods: 'custom/codemods/codemods.js',
+        description: '',
+        force: true,
+        _process: { cwd: fakeFs.dir },
+      });
+
+      const output = fs.readFileSync(`${fakeFs.dir}/assert-file`, 'utf8');
+
+      expect(output).toEqual('hello mars');
+    });
+  });
 });
