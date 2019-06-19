41b89c0e03cbd221d015e71cb61aab5a9dbfc91c
Barak Igal <baraki@wix.com>
2019-06-19 11:54:19 +0300

    fix type

--- packages/wix-ui-test-utils/src/unidriver/legacy-stylable-types.d.ts
+++ packages/wix-ui-test-utils/src/unidriver/legacy-stylable-types.d.ts
@@ -5,17 +5,23 @@ export interface StateMap {
 }
 
 export interface CompatStylesheet {
-  $cssStates(states: StateMap): { className: string };
+  $cssStates(states?: StateMap | null): { className: string };
 }
 export interface LegacyStylesheet {
-  $cssStates(states: StateMap): { [key: string]: string };
+  $cssStates(states?: StateMap | null): StateMap;
 }
 
 export interface StylesheetV2 {
-  cssStates(states: StateMap): string;
+  cssStates(states?: StateMap | null): string;
 }
 
 export type CommonStylesheet =
-  | CompatStylesheet
   | LegacyStylesheet
+  | CompatStylesheet
   | StylesheetV2;
+
+// export type CommonStylesheet<T extends any = any> = T extends LegacyStylesheet
+//   ? LegacyStylesheet
+//   : T extends CompatStylesheet
+//   ? CompatStylesheet
+//   : StylesheetV2;
