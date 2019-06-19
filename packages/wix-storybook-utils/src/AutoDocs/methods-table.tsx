de5e7ac271e0f44263688357630d7816efaf9191
Arijus Šukys <arijuss@wix.com>
2019-06-19 17:50:29 +0300

    fix(Autodocs): hide lifecycle methods from methods table (#1362)

--- packages/wix-storybook-utils/src/AutoDocs/methods-table.tsx
+++ packages/wix-storybook-utils/src/AutoDocs/methods-table.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
+export const hiddenMethods = [
+  'UNSAFE_componentWillMount',
+  'UNSAFE_componentWillReceiveProps',
+  'UNSAFE_componentWillUpdate',
+  'componentDidCatch',
+  'componentDidMount',
+  'componentDidUpdate',
+  'componentWillUnmount',
+  'constructor',
+  'forceUpdate',
+  'getDerivedStateFromError',
+  'getDerivedStateFromProps',
+  'getSnapshotBeforeUpdate',
+  'render',
+  'setState',
+  'shouldComponentUpdate',
+];
+
 export const MethodsTable = ({ methods = [] }) => (
   <table data-hook="autodocs-methods-table">
     <thead>
@@ -11,13 +29,15 @@ export const MethodsTable = ({ methods = [] }) => (
     </thead>
 
     <tbody>
-      {methods.map(({ name = '', params = [], description = '' }) => (
-        <tr key={name}>
-          <td>{name}</td>
-          <td>{params.map(({ name: paramName }) => paramName).join(', ')}</td>
-          <td>{description}</td>
-        </tr>
-      ))}
+      {methods
+        .filter(({ name }) => !hiddenMethods.includes(name))
+        .map(({ name = '', params = [], description = '' }) => (
+          <tr data-hook="autodocs-methods-table-row" key={name}>
+            <td>{name}</td>
+            <td>{params.map(({ name: paramName }) => paramName).join(', ')}</td>
+            <td>{description}</td>
+          </tr>
+        ))}
     </tbody>
   </table>
 );
