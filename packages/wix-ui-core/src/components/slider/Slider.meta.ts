7ba7cb7af5a9e83cf774a08a50070b1e403e660d
mosheKerbel <moshek@wix.com>
2019-07-09 13:52:55 +0300

    disable a11y tests for Slider that start breaking

--- packages/wix-ui-core/src/components/slider/Slider.meta.ts
+++ packages/wix-ui-core/src/components/slider/Slider.meta.ts
@@ -2,6 +2,7 @@ import { Slider } from './Slider';
 import Registry from '@ui-autotools/registry';
 
 const sliderMetadata = Registry.getComponentMetadata(Slider);
+sliderMetadata.nonA11yCompliant = true;
 sliderMetadata.addSim({
   title: 'sliderSim',
   props: {},
