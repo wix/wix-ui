14f87f1537e1242fe8546f57fd1d33ec39a5acbd
mosheKerbel <moshek@wix.com>
2019-07-10 11:10:37 +0300

    Update packages/wix-ui-core/src/components/slider/Slider.meta.ts
    enable a11y test
    
    Co-Authored-By: Arijus Šukys <arijuss@wix.com>

--- packages/wix-ui-core/src/components/slider/Slider.meta.ts
+++ packages/wix-ui-core/src/components/slider/Slider.meta.ts
@@ -2,7 +2,6 @@ import { Slider } from './Slider';
 import Registry from '@ui-autotools/registry';
 
 const sliderMetadata = Registry.getComponentMetadata(Slider);
-sliderMetadata.nonA11yCompliant = true;
 sliderMetadata.addSim({
   title: 'sliderSim',
   props: {
