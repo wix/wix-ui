f7429c89a8048ba8b5e03ff641be23e965dea61d
Ran Yitzhaki <ranyitz@gmail.com>
2019-07-16 10:18:42 +0300

    add es version

--- packages/wix-ui-core/scripts/build-standalone.js
+++ packages/wix-ui-core/scripts/build-standalone.js
@@ -1,20 +1,22 @@
 const path = require('path');
 const execa = require('execa');
 
-(async () => {
-  const outDir = path.join('dist', 'standalone');
+const outDir = path.join('dist', 'standalone');
+const esOutDir = path.join(outDir, 'es');
 
+function buildStandalone() {
+  console.log('Creating "standalone" version');
   console.log('running tsc...');
 
   // https://www.typescriptlang.org/docs/handbook/compiler-options.html
-  await execa('tsc', ['--outDir', outDir], { stdio: 'inherit' });
+  execa.sync('tsc', ['--outDir', outDir], { stdio: 'inherit' });
 
   console.log('✔︎');
 
   console.log('running stc...');
 
   // https://github.com/wix/stylable/tree/master/packages/cli#usage
-  await execa(
+  execa.sync(
     'stc',
     [
       `--outDir=${outDir}`,
@@ -27,7 +29,39 @@ const execa = require('execa');
   );
 
   console.log('✔︎');
+}
+
+function buildStandaloneEs() {
+  console.log('Creating "standalone/es" version');
+  console.log('running tsc...');
+
+  // https://www.typescriptlang.org/docs/handbook/compiler-options.html
+  execa.sync('tsc', ['--outDir', esOutDir, '--module', 'ESNext'], {
+    stdio: 'inherit',
+  });
+
+  console.log('✔︎');
+
+  console.log('running stc...');
+
+  // https://github.com/wix/stylable/tree/master/packages/cli#usage
+  execa.sync(
+    'stc',
+    [
+      `--outDir=${esOutDir}`,
+      '--cssFilename=[filename].global.css',
+      '--cjs',
+      '--css',
+      '--icr',
+    ],
+    { stdio: 'inherit' },
+  );
+
+  console.log('✔︎');
   console.log();
 
-  console.log(`Finish build standalone version under ${outDir}`);
-})();
+  console.log(`Finish build standalone version under ${esOutDir}`);
+}
+
+buildStandalone();
+buildStandaloneEs();
