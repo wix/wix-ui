6814b720baa4581f33a17f313f7b8685f229db00
argshook <argshook@gmail.com>
2019-07-29 15:27:53 +0300

    feat(testkits): automate vanilla testkits exports file

---
+++ packages/wix-ui-core/.wuf/testkits/definitions.js
@@ -0,0 +1,40 @@
+// Contents in this file are used in .wuf/testkits/*.template.ejs
+
+module.exports = {
+  button: { deprecated: true, pascalCase: true },
+  divider: { deprecated: true, pascalCase: true },
+  label: { deprecated: true, pascalCase: true },
+  badge: {
+    deprecated: true,
+    driverPath:
+      "./../dist/src/components/deprecated/stylable-badge/Badge.driver"
+  },
+
+  "address-input": { pascalCase: true },
+  autocomplete: { pascalCase: true },
+  captcha: { pascalCase: true },
+  checkbox: { pascalCase: true },
+  "circular-progress-bar": { pascalCase: true },
+  "dropdown-content": { pascalCase: true },
+  "dropdown-option": { pascalCase: true },
+  dropdown: { pascalCase: true },
+  "icon-with-options": { pascalCase: true },
+  "input-with-options": { pascalCase: true },
+  input: { pascalCase: true },
+  "label-with-options": { pascalCase: true },
+  "linear-progress-bar": { pascalCase: true },
+  loadable: { pascalCase: true },
+  "nav-stepper": { pascalCase: true },
+  pagination: { pascalCase: true },
+  popover: { pascalCase: true },
+  "radio-button": { pascalCase: true },
+  slider: { pascalCase: true },
+  thumbnail: { pascalCase: true },
+  "time-picker": { pascalCase: true },
+  "toggle-switch": { pascalCase: true },
+  tooltip: { pascalCase: true },
+  video: { pascalCase: true },
+  stepper: { noDriver: true },
+  "ellipsis-tooltip": { noDriver: true },
+  "file-picker-button": { noDriver: true }
+};
---
+++ packages/wix-ui-core/.wuf/update-components.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# create components list
+wuf update \
+  --shape .wuf/required-component-files.json \
+  --components src/components \
+  --max-mismatch 1 \
+  --exclude "deprecated" \
+  --output .wuf/components.json
+
+# vanilla testkits
+wuf export-testkits \
+  --components .wuf/components.json \
+  --definitions .wuf/testkits/definitions.js \
+  --template .wuf/testkits/vanilla.template.ejs \
+  --output drivers/vanilla.js
+
