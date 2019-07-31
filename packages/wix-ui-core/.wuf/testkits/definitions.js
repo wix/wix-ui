ca35e8dde41ae7682796888473705057e90cdfaf
Arijus Šukys <arijuss@wix.com>
2019-07-31 10:52:25 +0300

    wix-ui-core: integrate `wuf export-testkits` the right way (#1419)
    * Revert "Revert "wix-ui-core: integrate `wuf export-testkits` (properly) (#1417)""
    
    This reverts commit b9e007bf2bc384d9dba9849525330f05302d16a8.
    
    * Revert "Revert "wix-ui-core(fix): add signature-input to definitions.js""
    
    This reverts commit 15c3a67a3b862814531b133da327e0977018fc9a.
    
    * fix(wuf export-testkits): use whitelist approach for testkits generation
    
    * fix(wuf/testkits/definitions.js): set signature-input as pascalCase
    
    due to mixed convention in the lib, this component uses pascalCase. The
    proper approach should likely be kebab-case

---
+++ packages/wix-ui-core/.wuf/testkits/definitions.js
@@ -0,0 +1,170 @@
+// Contents in this file are used in .wuf/testkits/*.template.ejs
+
+module.exports = {
+  "address-input": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "button-next": { hasUnidriver: true, hasVanillaDriver: true },
+  "circular-progress-bar": {
+    hasUnidriver: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "dropdown-content": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "dropdown-option": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "file-picker-button": {
+    hasUnidriver: true,
+    uniDriverPath:
+      "./../dist/src/components/file-picker-button/test/FilePickerButton.uni.driver"
+  },
+  "icon-with-options": {
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "input-with-options": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "label-with-options": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "linear-progress-bar": {
+    hasUnidriver: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "media-image": { hasUnidriver: true, hasVanillaDriver: true },
+  "menu-item": { hasUnidriver: true, hasVanillaDriver: true },
+  "nav-stepper": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "radio-button": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "time-picker": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  "toggle-switch": {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  autocomplete: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  avatar: {
+    hasUnidriver: true,
+    hasVanillaDriver: true
+  },
+  badge: {
+    deprecated: true,
+
+    hasVanillaDriver: true,
+    vanillaDriverPath:
+      "./../dist/src/components/deprecated/stylable-badge/Badge.driver",
+
+    hasProtractorDriver: true,
+    protractorDriverPath:
+      "./../dist/src/components/deprecated/stylable-badge/Badge.protractor.driver"
+  },
+  button: {
+    deprecated: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  captcha: { hasUnidriver: true, hasVanillaDriver: true, pascalCase: true },
+  checkbox: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  divider: {
+    deprecated: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  dropdown: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  image: { hasUnidriver: true, hasVanillaDriver: true },
+  input: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  label: {
+    deprecated: true,
+    hasUnidriver: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  loadable: {
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  pagination: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  popover: {
+    hasUnidriver: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  slider: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  thumbnail: {
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  tooltip: {
+    hasUnidriver: true,
+    hasProtractorDriver: true,
+    hasVanillaDriver: true,
+    pascalCase: true
+  },
+  video: { hasUnidriver: true, hasVanillaDriver: true, pascalCase: true },
+  stepper: {
+    noDriver: true
+  },
+  "ellipsis-tooltip": {
+    noDriver: true
+  },
+  "signature-input": {
+    pascalCase: true
+  }
+};
