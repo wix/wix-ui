33ced183bfcc35ae37c60577c4be00f63d7c771e
argshook <argshook@gmail.com>
2019-09-10 18:11:09 +0300

    fix(AutoTestkit/flatten): ensure falsy objects dont throw error
    TS prevents stuff like that, however, autodocs users are js, so
    defensive programing ;)

--- packages/wix-storybook-utils/src/AutoTestkit/flatten.ts
+++ packages/wix-storybook-utils/src/AutoTestkit/flatten.ts
@@ -3,17 +3,19 @@ import { Descriptor } from './typings';
 const isNested = (item: Descriptor) => item.type === 'object';
 
 export const flatten = (methodsList: Descriptor[], name = '') =>
-  methodsList.reduce((list, item: Descriptor) => {
-    if (isNested(item)) {
-      list = list.concat(
-        flatten(item.props, name ? `${name}.${item.name}` : item.name),
-      );
-    } else {
-      list.push({
-        ...item,
-        ...(name ? { name: `${name}.${item.name}` } : {}),
-      });
-    }
+  methodsList
+    .filter(i => i && i.type)
+    .reduce((list, item: Descriptor) => {
+      if (isNested(item)) {
+        list = list.concat(
+          flatten(item.props, name ? `${name}.${item.name}` : item.name),
+        );
+      } else {
+        list.push({
+          ...item,
+          ...(name ? { name: `${name}.${item.name}` } : {}),
+        });
+      }
 
-    return list;
-  }, []);
+      return list;
+    }, []);
