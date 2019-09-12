aae8904cf3a0044d12da4775e38c215adaa344d9
Nitay Rabinovich <nitayr@wix.com>
2019-09-12 19:05:08 +0300

    fix ui-autotools

--- packages/wix-ui-core/.autotools/webpack.config.js
+++ packages/wix-ui-core/.autotools/webpack.config.js
@@ -1,5 +1,6 @@
 const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
 const path = require('path');
+const { StylableWebpackPlugin } = require("@stylable/webpack-plugin");
 const packagePath = path.resolve(__dirname, '..');
 
 const config = {
@@ -12,5 +13,26 @@ const config = {
 };
 
 const yoshiConfig = wixStorybookConfig(config);
+
+// Filter out yoshi's stylable plugin
+yoshiConfig.plugins = yoshiConfig.plugins.filter(plugin => {
+  return plugin.constructor.name !== 'StylableWebpackPlugin';
+});
+
+const stylableWebpackPlugin = new StylableWebpackPlugin({
+  filename: '[name].stylable.bundle.css',
+  optimize: { 
+    classNameOptimizations: false,
+    shortNamespaces: false,
+    removeUnusedComponents: false
+  },
+  generate: {
+    runtimeStylesheetId: 'namespace',
+  },
+  legacyRuntime: true,
+  unsafeBuildNamespace: true 
+});
+
+yoshiConfig.plugins.push(stylableWebpackPlugin);
 
 module.exports = yoshiConfig;
