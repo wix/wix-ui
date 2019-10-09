2f49e06d96efdfee99e79c05e96c6487febe6b3b
mykas <mykolass@wix.com>
2019-10-09 11:01:40 +0300

    Wrong puppeteer export for dataHook

--- packages/wix-ui-test-utils/src/puppeteer/puppeteer.ts
+++ packages/wix-ui-test-utils/src/puppeteer/puppeteer.ts
@@ -4,14 +4,16 @@ import {UniDriver} from '@unidriver/core';
 import {pupUniDriver} from '@unidriver/puppeteer';
 
 export function puppeteerTestkitFactoryCreator<T>(
-  driverFactory: (e: ElementHandle | null, page: Page) => T
+  driverFactory: (
+    e: ElementHandle | null,
+    page: Page,
+    options: { dataHook: string }
+  ) => T
 ) {
-  return async (obj: {
-    dataHook: string;
-    page: Page;
-    options: { dataHook: string };
-  }) =>
-    driverFactory(await obj.page.$(`[data-hook='${obj.dataHook}']`), obj.page);
+  return async (obj: { dataHook: string; page: Page }) =>
+    driverFactory(await obj.page.$(`[data-hook='${obj.dataHook}']`), obj.page, {
+      dataHook: obj.dataHook,
+    });
 }
 
 export function puppeteerUniTestkitFactoryCreator<T extends BaseUniDriver>(
