2188a8ce1c6db1351231a9b96c539177664de213
mosheKerbel <moshek@wix.com>
2019-10-24 15:11:44 +0300

    Wsr types project (#1525)
    * WSR-types: initial commmit
    
    * concat
    
    * remove redundant stuff
    
    * esnext

---
+++ packages/wsr-types/src/BaseComponents.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react" />
+
+declare namespace __WSR {
+  namespace BaseComponents {
+    export interface WixComponentProps {
+      dataHook?: string;
+      styles?: string;
+    }
+
+    export class WixComponent<
+      T extends WixComponentProps = {}
+    > extends React.PureComponent<T> {}
+  }
+}
---
+++ packages/wsr-types/src/Loader.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="react" />
+
+declare namespace __WSR {
+  namespace Loader {
+    export interface LoaderProps
+      extends __WSR.BaseComponents.WixComponentProps {
+      size?: LoaderSize;
+      color?: LoaderColor;
+      text?: React.ReactNode;
+      status?: LoaderStatus;
+      statusMessage?: string;
+      shouldLoadAsync?: boolean;
+    }
+
+    export class Loader extends __WSR.BaseComponents.WixComponent<
+      LoaderProps
+    > {}
+
+    export type LoaderSize = 'tiny' | 'small' | 'medium' | 'large';
+
+    export type LoaderColor = 'blue' | 'white';
+
+    export type LoaderStatus = 'loading' | 'success' | 'error';
+  }
+}
+
+declare module 'wix-style-react' {
+  export import Loader = __WSR.Loader.Loader;
+  export import LoaderProps = __WSR.Loader.LoaderProps;
+}
+
+declare module 'wix-style-react/Loader' {
+  export interface LoaderProps extends __WSR.Loader.LoaderProps {}
+  export default __WSR.Loader.Loader;
+}
