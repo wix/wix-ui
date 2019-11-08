5fe303e57c751782ba8069be1e0773f11bda130c
Simas Toleikis <simast@wix.com>
2019-11-08 10:06:35 +0200

    Switch wix-ui-jss package to Node v12.

--- packages/wix-ui-jss/src/withClasses.tsx
+++ packages/wix-ui-jss/src/withClasses.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import {generateClasses, detachStyleSheetFromDom} from './domStyleRenderer';
 import hoistNonReactMethods from 'hoist-non-react-methods';
+import { generateClasses, detachStyleSheetFromDom } from './domStyleRenderer';
+
 const uniqueId = require('lodash/uniqueId');
 
-const isStatelessComponent = Component => !(Component.prototype && Component.prototype.render);
+const isStatelessComponent = Component =>
+  !(Component.prototype && Component.prototype.render);
 
 export interface ThemedComponentProps {
   theme?: object;
@@ -13,21 +15,29 @@ export interface HasClasses {
   classes?: object;
 }
 
-export function withClasses<P extends {ref?: string}>(CoreComponent: React.ComponentType<P & HasClasses>, styles: Function): React.ComponentClass<P & ThemedComponentProps> {
-  class ThemedComponent extends React.PureComponent<ThemedComponentProps & P, HasClasses> {
-    private id;
+export function withClasses<P extends { ref?: string }>(
+  CoreComponent: React.ComponentType<P & HasClasses>,
+  styles: Function,
+): React.ComponentClass<P & ThemedComponentProps> {
+  class ThemedComponent extends React.PureComponent<
+    ThemedComponentProps & P,
+    HasClasses
+  > {
+    private readonly id;
     private wrappedComponentRef: React.Component = null;
     static displayName = CoreComponent.displayName || 'ThemedComponent';
 
     constructor(props) {
       super(props);
       this.id = uniqueId();
-      this.state = {classes: generateClasses(styles(props.theme), this.id)};
+      this.state = { classes: generateClasses(styles(props.theme), this.id) };
     }
 
     componentWillReceiveProps(nextProps) {
       if (this.props.theme !== nextProps.theme) {
-        this.setState({classes: generateClasses(styles(nextProps.theme), this.id)});
+        this.setState({
+          classes: generateClasses(styles(nextProps.theme), this.id),
+        });
       }
     }
 
@@ -36,15 +46,24 @@ export function withClasses<P extends {ref?: string}>(CoreComponent: React.Compo
     }
 
     render() {
-      let coreProps = Object.assign({}, this.props, {theme: undefined});
+      const coreProps = Object.assign({}, this.props, { theme: undefined });
 
-      return isStatelessComponent(CoreComponent)
-        ? (<CoreComponent {...coreProps} classes={this.state.classes}/>)
-        : (<CoreComponent ref={ref => this.wrappedComponentRef = ref} {...coreProps} classes={this.state.classes} />);
+      return isStatelessComponent(CoreComponent) ? (
+        <CoreComponent {...coreProps} classes={this.state.classes} />
+      ) : (
+        <CoreComponent
+          ref={ref => (this.wrappedComponentRef = ref)}
+          {...coreProps}
+          classes={this.state.classes}
+        />
+      );
     }
   }
 
   return isStatelessComponent(CoreComponent)
     ? ThemedComponent
-    : hoistNonReactMethods(ThemedComponent, CoreComponent, {delegateTo: c => c.wrappedComponentRef, hoistStatics: true});
+    : hoistNonReactMethods(ThemedComponent, CoreComponent, {
+        delegateTo: c => c.wrappedComponentRef,
+        hoistStatics: true,
+      });
 }
