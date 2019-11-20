6e72b44d12fc78709a37a573af1735eb9194b98e
Vytenis Urbonavičius <vytenisu@wix.com>
2019-11-20 09:39:21 +0200

    Storybook utils loader plugin support

--- packages/wix-storybook-utils/src/loader/index.js
+++ packages/wix-storybook-utils/src/loader/index.js
@@ -6,9 +6,22 @@ const gatherAll = require('react-autodocs-utils/src/gather-all');
 const metadataMerger = require('react-autodocs-utils/src/metadata-merger');
 const prepareStory = require('react-autodocs-utils/src/prepare-story'); // TODO: should be part of wix-storybook-utils
 
+const applyMetadataPlugins = (source, metadataPlugins) => metadata =>
+  Promise.all(
+    Object.values(metadataPlugins).map(plugin => plugin(source)),
+  ).then(metaResults => {
+    metadata = { ...(metadata || {}), plugins: {} };
+
+    Object.keys(metadataPlugins).forEach((pluginName, index) => {
+      metadata.plugins[pluginName] = metaResults[index];
+    });
+
+    return metadata;
+  });
+
 module.exports = function(source) {
   const callback = this.async();
-  const { storyConfig } = loaderUtils.getOptions(this);
+  const { storyConfig, metadataPlugins = {} } = loaderUtils.getOptions(this);
 
   // 1. find component path
   pathFinder(source)
@@ -19,8 +32,9 @@ module.exports = function(source) {
         : Promise.resolve({});
 
       return metadata
-        .then(metadataMerger(source)) // 3. merge component metadata with storybook config
-        .then(prepareStory(storyConfig)); // 4. import and wrap with `story` function
+        .then(applyMetadataPlugins(source, metadataPlugins)) // 3. apply plugged in analyzers
+        .then(metadataMerger(source)) // 4. merge component metadata with storybook config
+        .then(prepareStory(storyConfig)); // 5. import and wrap with `story` function
     })
     // 5. succeed with augmented source
     .then(finalSource => callback(null, finalSource))
