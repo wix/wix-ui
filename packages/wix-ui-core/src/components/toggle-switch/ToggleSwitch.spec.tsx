93a427d3438df34f86ac7e71a4456f71b9e9a1b4
TalZa <24567980+TalZa1810@users.noreply.github.com>
2019-12-02 10:58:47 +0200

    refactoring unit tests

--- packages/wix-ui-core/src/components/toggle-switch/ToggleSwitch.spec.tsx
+++ packages/wix-ui-core/src/components/toggle-switch/ToggleSwitch.spec.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { toggleSwitchDriverFactory } from './ToggleSwitch.driver';
 import { isEnzymeTestkitExists } from 'wix-ui-test-utils/enzyme';
 import { ReactDOMTestContainer } from '../../../test/dom-test-container';
 import { isTestkitExists } from 'wix-ui-test-utils/vanilla';
@@ -7,148 +6,141 @@ import { ToggleSwitch } from './';
 import { toggleSwitchTestkitFactory } from '../../testkit';
 import { toggleSwitchTestkitFactory as enzymeToggleSwitchTestkitFactory } from '../../testkit/enzyme';
 import { mount } from 'enzyme';
-
+import { toggleSwitchDriverFactory } from './ToggleSwitch.driver';
 import { toggleSwitchUniDriverFactory } from "./ToggleSwitch.uni.driver";
 
-describe('ToggleSwitch sync', () => {
-  const createDriver = new ReactDOMTestContainer()
-    .unmountAfterEachTest()
-    .createLegacyRenderer(toggleSwitchDriverFactory);
+describe('ToggleSwitch', () => {
+  const testContainer = new ReactDOMTestContainer().unmountAfterEachTest();
 
-  describe('checked prop', () => {
-    it('should be controlled', () => {
-      const driver = createDriver(<ToggleSwitch />);
-      expect(driver.isChecked()).toBe(false);
-      driver.click();
-
-      expect(driver.isChecked()).toBe(false);
-    });
-
-    it('should pass down to input when checked', () => {
-      const driver = createDriver(<ToggleSwitch checked />);
-      expect(driver.isChecked()).toBe(true);
-    });
-
-    it('should pass down to input when not checked', () => {
-      const driver = createDriver(<ToggleSwitch checked={false} />);
-      expect(driver.isChecked()).toBe(false);
-    });
-  });
-
-  describe('onChange prop', () => {
-    it('should be called when the input is clicked', () => {
-      const onChange = jest.fn();
-      const driver = createDriver(
-        <ToggleSwitch checked={false} onChange={onChange} />,
+  describe('[sync]', () => {
+      runTests(
+          testContainer.createLegacyRenderer(toggleSwitchDriverFactory)
       );
-
-      driver.click();
-      expect(onChange).toBeCalled();
-    });
   });
 
-  describe('disabled prop', () => {
-    it('should not be disabled by default', () => {
-      const driver = createDriver(<ToggleSwitch />);
-      expect(driver.isDisabled()).toBe(false);
-    });
+  function runTests(createDriver){
+      describe('checked prop', () => {
+          it('should be controlled', async () => {
+              const driver = await createDriver(<ToggleSwitch />);
+              expect(await driver.isChecked()).toBe(false);
+              await driver.click();
+              expect(await driver.isChecked()).toBe(false);
+          });
 
-    it('should not be clickable when disabled and unchecked', () => {
-      const onChange = jest.fn();
-      const driver = createDriver(
-        <ToggleSwitch checked={false} onChange={onChange} disabled />,
-      );
-      driver.click();
-      expect(onChange).toHaveBeenCalledTimes(0);
-      expect(driver.isChecked()).toBe(false);
-    });
+          it('should pass down to input when checked', async () => {
+              const driver = await createDriver(<ToggleSwitch checked />);
+              expect(await driver.isChecked()).toBe(true);
+          });
 
-    it('should not be clickable when disabled and checked', () => {
-      const onChange = jest.fn();
-      const driver = createDriver(
-        <ToggleSwitch checked onChange={onChange} disabled />,
-      );
-      driver.click();
-      expect(onChange).toHaveBeenCalledTimes(0);
-      expect(driver.isChecked()).toBe(true);
-    });
-  });
+          it('should pass down to input when not checked', async () => {
+              const driver = await createDriver(<ToggleSwitch checked={false} />);
+              expect(await driver.isChecked()).toBe(false);
+          });
+      });
 
-  describe('attributes', () => {
-    it('should apply user specified id', () => {
-      const testId = 'testId';
-      const driver = createDriver(<ToggleSwitch id={testId} />);
-      expect(driver.getId()).toBe(testId);
-    });
+      describe('onChange prop', () => {
+          it('should be called when the input is clicked', async () => {
+              const onChange = jest.fn();
+              const driver = await createDriver(
+                  <ToggleSwitch checked={false} onChange={onChange} />,
+              );
 
-    it('should have tabIndex=0 by default', () => {
-      const driver = createDriver(<ToggleSwitch />);
-      expect(driver.getTabIndex()).toBe(0);
-    });
+              await driver.click();
+              expect(onChange).toBeCalled();
+          });
+      });
 
-    it('should apply user specified tabIndex', () => {
-      const driver = createDriver(<ToggleSwitch tabIndex={7} />);
-      expect(driver.getTabIndex()).toBe(7);
-    });
-  });
+      describe('disabled prop', () => {
+          it('should not be disabled by default', async () => {
+              const driver = createDriver(<ToggleSwitch />);
+              expect(driver.isDisabled()).toBe(false);
+          });
 
-  describe('icons', () => {
-    it('should not have unchecked icon by default', () => {
-      const driver = createDriver(<ToggleSwitch />);
-      expect(driver.getKnobIcon().innerHTML).toBe('');
-    });
+          it('should not be clickable when disabled and unchecked', async () => {
+              const onChange = jest.fn();
+              const driver = await createDriver(
+                  <ToggleSwitch checked={false} onChange={onChange} disabled />,
+              );
+              await driver.click();
+              expect(onChange).toHaveBeenCalledTimes(0);
+              expect(await driver.isChecked()).toBe(false);
+          });
 
-    it('should not have checked icon by default', () => {
-      const driver = createDriver(<ToggleSwitch checked />);
-      expect(driver.getKnobIcon().innerHTML).toBe('');
-    });
+          it('should not be clickable when disabled and checked', async () => {
+              const onChange = jest.fn();
+              const driver = await createDriver(
+                  <ToggleSwitch checked onChange={onChange} disabled />,
+              );
+              await driver.click();
+              expect(onChange).toHaveBeenCalledTimes(0);
+              expect(await driver.isChecked()).toBe(true);
+          });
+      });
 
-    it('should show uncheckedIcon when unchecked', () => {
-      const driver = createDriver(
-        <ToggleSwitch checkedIcon="✅" uncheckedIcon="❎" />,
-      );
-      expect(driver.getKnobIcon().innerHTML).toBe('❎');
-    });
+      describe('attributes', () => {
+          it('should apply user specified id', async () => {
+              const testId = 'testId';
+              const driver = await createDriver(<ToggleSwitch id={testId} />);
+              expect(await driver.getId()).toBe(testId);
+          });
 
-    it('should show checkedIcon when checked', () => {
-      const driver = createDriver(
-        <ToggleSwitch checked checkedIcon="✅" uncheckedIcon="❎" />,
-      );
-      expect(driver.getKnobIcon().innerHTML).toBe('✅');
-    });
-  });
+          it('should have tabIndex=0 by default', async () => {
+              const driver = await createDriver(<ToggleSwitch />);
+              expect(await driver.getTabIndex()).toBe(0);
+          });
 
-  describe('testkit', () => {
-    it('should exist', () => {
-      expect(
-        isTestkitExists(<ToggleSwitch />, toggleSwitchTestkitFactory),
-      ).toBe(true);
-    });
-  });
+          it('should apply user specified tabIndex', async () => {
+              const driver = await createDriver(<ToggleSwitch tabIndex={7} />);
+              expect(await driver.getTabIndex()).toBe(7);
+          });
+      });
 
-  describe('enzyme testkit', () => {
-    it('should exist', () => {
-      expect(
-        isEnzymeTestkitExists(
-          <ToggleSwitch />,
-          enzymeToggleSwitchTestkitFactory,
-          mount,
-        ),
-      ).toBe(true);
-    });
-  });
+      describe('icons', () => {
+          it('should not have unchecked icon by default', async () => {
+              const driver = await createDriver(<ToggleSwitch />);
+              expect(await driver.getKnobIcon().innerHTML).toBe('');
+          });
+
+          it('should not have checked icon by default', async () => {
+              const driver = await createDriver(<ToggleSwitch checked />);
+              expect(await driver.getKnobIcon().innerHTML).toBe('');
+          });
+
+          it('should show uncheckedIcon when unchecked', async () => {
+              const driver = await createDriver(
+                  <ToggleSwitch checkedIcon="✅" uncheckedIcon="❎" />,
+              );
+              expect(await driver.getKnobIcon().innerHTML).toBe('❎');
+          });
+
+          it('should show checkedIcon when checked', async () => {
+              const driver = await createDriver(
+                  <ToggleSwitch checked checkedIcon="✅" uncheckedIcon="❎" />,
+              );
+              expect(await driver.getKnobIcon().innerHTML).toBe('✅');
+          });
+      });
+
+      // describe('testkit', () => {
+      //     it('should exist', async () => {
+      //         expect(
+      //             isTestkitExists(<ToggleSwitch />, toggleSwitchTestkitFactory),
+      //         ).toBe(true);
+      //     });
+      // });
+      //
+      // describe('enzyme testkit', () => {
+      //     it('should exist', async () => {
+      //         expect(
+      //             isEnzymeTestkitExists(
+      //                 <ToggleSwitch />,
+      //                 enzymeToggleSwitchTestkitFactory,
+      //                 mount,
+      //             ),
+      //         ).toBe(true);
+      //     });
+      // });
+  }
 });
 
-fdescribe('ToggleSwitch async', () => {
 
-    const testContainer = new ReactDOMTestContainer().unmountAfterEachTest();
-
-    const driver = testContainer.createUniRendererAsync(toggleSwitchUniDriverFactory);
-
-
-    it('should exist', async() => {
-      console.log(driver);
-
-    })
-
-});
