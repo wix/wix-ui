09d8fc44feda2ae91057332074fca36d92b1c213
Simas Toleikis <simast@wix.com>
2019-12-03 10:51:37 +0200

    Add basic CLI wrapper for jscodeshift.

---
+++ packages/wix-ui-codemod/src/index.ts
@@ -0,0 +1,37 @@
+import * as program from 'commander';
+import path from 'path';
+import { execFileSync } from 'child_process';
+
+program
+  .arguments('<transform> [path]')
+  .option('-p, --print', 'print modified files to stdout')
+  .option('-d, --dry', 'run in dry mode (will not modify any files on disk)');
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
+
+const transform = path.join(__dirname, `${process.argv[0]}.ts`);
+const args: string[] = [];
+
+if (program.dry) {
+  args.push('--dry');
+}
+
+if (program.print) {
+  args.push('--print');
+}
+
+args.push('--ignore-pattern=**/node_modules/**');
+args.push('--parser', 'tsx');
+args.push('--extensions', 'tsx,ts,jsx,js');
+args.push('--transform', transform);
+args.push('--verbose', '2');
+
+if (program.args[1]) {
+  args.push(program.args[1]);
+}
+
+execFileSync(require.resolve('.bin/jscodeshift'), args);
