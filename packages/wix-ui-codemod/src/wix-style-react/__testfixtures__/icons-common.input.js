8ead8fed65b620e9501d3aa5afa6e105e7e69b00
Simas Toleikis <simast@wix.com>
2019-12-03 13:04:10 +0200

    Add tests.

---
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/icons-common.input.js
@@ -0,0 +1,4 @@
+import Add from 'wix-style-react/new-icons/Add';
+import { Check, More } from 'wix-style-react/new-icons';
+import * as Icons from 'wix-style-react/new-icons';
+import Button from 'wix-style-react/Button';
---
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/icons-common.output.js
@@ -0,0 +1,4 @@
+import Add from 'wix-ui-icons-common/Add';
+import { Check, More } from 'wix-ui-icons-common';
+import * as Icons from 'wix-ui-icons-common';
+import Button from 'wix-style-react/Button';
