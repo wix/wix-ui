0e41465aea88220d7d466baabe18202e3696624e
Simas Toleikis <simast@wix.com>
2019-12-03 16:47:46 +0200

    Add more test cases.

--- packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/icons-common.input.js
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/icons-common.input.js
@@ -1,4 +1,9 @@
 import Add from 'wix-style-react/new-icons/Add';
 import { Check, More } from 'wix-style-react/new-icons';
+import Move from 'wix-style-react/new-icons/system/Move';
 import * as Icons from 'wix-style-react/new-icons';
+import {
+  Delete,
+  Help,
+} from 'wix-style-react/new-icons';
 import Button from 'wix-style-react/Button';
--- packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/icons-common.output.js
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/icons-common.output.js
@@ -1,4 +1,9 @@
 import Add from 'wix-ui-icons-common/Add';
 import { Check, More } from 'wix-ui-icons-common';
+import Move from 'wix-ui-icons-common/system/Move';
 import * as Icons from 'wix-ui-icons-common';
+import {
+  Delete,
+  Help,
+} from 'wix-ui-icons-common';
 import Button from 'wix-style-react/Button';
