7f5371766648e16d5118239f4ad70452607c546b
Simas Toleikis <simast@wix.com>
2020-01-07 10:29:46 +0200

    Update Stepper testkit return types.

--- packages/wsr-types/src/drivers/Stepper.uni.driver.d.ts
+++ packages/wsr-types/src/drivers/Stepper.uni.driver.d.ts
@@ -1,12 +1,12 @@
 declare namespace __WSRTests {
   interface StepperUniDriver extends BaseUniDriver {
-    getType: () => Promise<string>;
-    getFit: () => Promise<string>;
+    getType: () => Promise<__WSR.Stepper.StepperType>;
+    getFit: () => Promise<__WSR.Stepper.StepperFit>;
     clickStep: (index: number) => Promise<void>;
     hoverStep: (index: number) => Promise<void>;
     getNumberOfSteps: () => Promise<number>;
     isStepActive: (index: number) => Promise<boolean>;
-    getStepType: (index: number) => Promise<string>;
+    getStepType: (index: number) => Promise<__WSR.Stepper.StepperStepType>;
     getStepText: (index: number) => Promise<string>;
   }
 }
