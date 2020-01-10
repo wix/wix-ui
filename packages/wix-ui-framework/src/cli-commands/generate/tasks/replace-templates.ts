7a77a2b7b94cbdbe808f1e7d938abc79bfad6196
argshook <argshook@gmail.com>
2020-01-10 15:56:59 +0200

    feat(generator): support ejs in generator template

--- packages/wix-ui-framework/src/cli-commands/generate/tasks/replace-templates.ts
+++ packages/wix-ui-framework/src/cli-commands/generate/tasks/replace-templates.ts
@@ -1,18 +1,45 @@
-// Extracted from
-// https://github.com/wix/yoshi/blob/master/packages/create-yoshi-app/src/replaceTemplates.js
+import * as ejs from 'ejs';
 
-const templateRegex = /{%[\w-]+%}/g;
+import * as camelCase from 'lodash/camelCase';
+import * as kebabCase from 'lodash/kebabCase';
+import * as snakeCase from 'lodash/snakeCase';
 
-export const replaceTemplates = (content, map) => {
-  function replacer(match) {
+const utils = {
+  toCamel: camelCase,
+  toKebab: kebabCase,
+  toSnake: snakeCase,
+  toPascal: (s: string) => {
+    const camel: string = camelCase(s);
+    return camel[0].toUpperCase() + camel.substring(1);
+  },
+};
+
+export const replaceTemplates = (source: string, scope: Object) => {
+  /*
+   * TODO: usage of `{%` & `%}` tags is deprecated!
+   * regular EJS should be used in user templates
+   *
+   * keeping it to not break existing users
+   * can be removed either:
+   * 1. with major version release
+   * 2. when ensured no consumers use it
+   */
+  const replaceDeprecatedSyntax = source.replace(/{%[\w-]+%}/g, match => {
     const key = match.slice(2, -2);
 
-    if (!map.hasOwnProperty(key)) {
-      throw new Error(`key '${key}' must be one of: [${Object.keys(map)}]`);
+    if (!scope.hasOwnProperty(key)) {
+      throw new Error(`key '${key}' must be one of: [${Object.keys(scope)}]`);
     }
 
-    return map[key];
-  }
+    return scope[key];
+  });
 
-  return content.replace(templateRegex, replacer);
+  try {
+    return ejs.render(replaceDeprecatedSyntax, {
+      utils,
+      ...scope,
+    });
+  } catch (e) {
+    throw new Error(`Erroneous template syntax: ${e}`);
+  }
 };
