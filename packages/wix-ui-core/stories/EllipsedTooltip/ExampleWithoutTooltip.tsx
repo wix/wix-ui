bd87c40b799bf68a91d1c1c027d02892da7fbdf5
Mykolas Sindeikis <mykolass@wix.com>
2020-01-14 10:59:10 +0200

    <WithEllipsisTooltipNext/> - with dynamic loading coming from popover (#1717)

--- packages/wix-ui-core/stories/EllipsedTooltip/ExampleWithoutTooltip.tsx
+++ packages/wix-ui-core/stories/EllipsedTooltip/ExampleWithoutTooltip.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import {withEllipsedTooltip} from '../../src/hocs/EllipsedTooltip';
+import { withEllipsedTooltipNext } from '../../src/hocs/EllipsedTooltip';
 
-const Text = ({children, ...rest}) => <span {...rest}>{children}</span>;
+const Text = ({ children, ...rest }) => <span {...rest}>{children}</span>;
 
-const EllipsedText = withEllipsedTooltip({showTooltip: false})(Text);
+const EllipsedText = withEllipsedTooltipNext({ showTooltip: false })(Text);
 
 export default () => (
-  <div style={{width: '170px', background: 'azure'}}><EllipsedText data-hook="ellipsedTooltip-without-tooltip">This text is going to get ellipsed</EllipsedText></div>
+  <div style={{ width: '170px', background: 'azure' }}>
+    <EllipsedText data-hook="ellipsedTooltip-without-tooltip">
+      This text is going to get ellipsed
+    </EllipsedText>
+  </div>
 );
