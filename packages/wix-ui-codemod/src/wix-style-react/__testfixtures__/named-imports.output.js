e9fc8de43d9030e146dda61898d7f2b2f910045e
mykas <mykolass@wix.com>
2020-01-28 13:28:37 +0200

    wix-ui-codemod(named-imports) - pass all named imports and add tslib to depedency

--- packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/named-imports.output.js
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/named-imports.output.js
@@ -1,4 +1,4 @@
-import { Avatar, Button, TextButton as WSRTextButton, Media, MediaProps, Text } from 'wix-style-react';
+import { Avatar, Button, TextButton as WSRTextButton, Media, MediaProps, Text, SIZE } from 'wix-style-react';
 import * as SomeIconButton from 'wix-style-react/IconButton';
 import { buttonTestkitFactory } from 'wix-style-react/testkit/enzyme';
 import something from 'around-here';
