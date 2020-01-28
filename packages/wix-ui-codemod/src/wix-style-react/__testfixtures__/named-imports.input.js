020b931ec91b68584a3a74c491594d8a52815277
Mykolas Sindeikis <mykolass@wix.com>
2020-01-28 11:50:07 +0200

    wix-ui-codemode - introduce named-imports codemode (#1735)
    * wix-ui-codemode - introduce named-imports codemode
    
    * wix-ui-codemod - make named-importing working solution
    
    * wix-ui-codemod - fix a bug,refactor and add documentation
    
    * wix-ui-codemod - refactor moduleName
    
    * Small improvement
    
    * EsmoduleInterlop bug
    
    * Fix some things after testing
    
    * Typo

---
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/named-imports.input.js
@@ -0,0 +1,9 @@
+import Button from 'wix-style-react/Button';
+import { Avatar } from 'wix-style-react';
+import WSRTextButton from 'wix-style-react/TextButton';
+import * as SomeIconButton from 'wix-style-react/IconButton';
+import Media, { MediaProps } from 'wix-style-react/Media';
+import Text, { SIZE } from 'wix-style-react/Text';
+import { buttonTestkitFactory } from 'wix-style-react/testkit/enzyme';
+import something from 'around-here';
+import icon from 'wix-style-react/new-icons';
---
+++ packages/wix-ui-codemod/src/wix-style-react/named-imports.ts
@@ -0,0 +1,95 @@
+import { Transform } from 'jscodeshift';
+import addImports from 'jscodeshift-add-imports';
+
+const componentNameRegex = /^wix-style-react\/([A-Z][\w-]*?)$/;
+
+const transform: Transform = (fileInfo, api, options) => {
+  const j = api.jscodeshift;
+  const root = j(fileInfo.source);
+
+  const resultSpecifiers = new Map();
+
+  const addSpecifier = (source, specifier) => {
+    if (!resultSpecifiers.has(source)) {
+      resultSpecifiers.set(source, []);
+    }
+    resultSpecifiers.get(source).push(specifier);
+  };
+
+  root
+    .find(j.ImportDeclaration)
+    .filter(path =>
+      [
+        path.node.specifiers.length,
+        componentNameRegex.test(path.node.source.value as string),
+        path.node.importKind === 'value',
+      ].every(Boolean),
+    )
+    .forEach(path => {
+      const sourceNode = path.node.source.value;
+      const moduleName = (sourceNode as string).match(componentNameRegex)[1];
+
+      const cleanImport = name => {
+        path.node.specifiers = path.node.specifiers.filter(
+          spec => spec.local.name !== name,
+        );
+      };
+
+      path.node.specifiers.forEach(specifier => {
+        const localName = specifier.local.name;
+
+        if (j.ImportDefaultSpecifier.check(specifier)) {
+          addSpecifier(
+            'wix-style-react',
+            j.importSpecifier(
+              j.identifier(moduleName),
+              j.identifier(localName),
+            ),
+          );
+          cleanImport(localName);
+        }
+
+        if (j.ImportSpecifier.check(specifier)) {
+          if (localName === moduleName || localName === `${moduleName}Props`) {
+            addSpecifier('wix-style-react', specifier);
+          }
+          cleanImport(localName);
+        }
+
+        if (j.ImportNamespaceSpecifier.check(specifier)) {
+          console.warn(
+            `[wix-ui-codemod][${fileInfo.path}]: unable to handle this import: "import as * ${specifier.local.name} from ${sourceNode}". Make sure to fix it to named import otherwise tree-shaking will not work for you.`,
+          );
+        }
+      });
+
+      if (!path.node.specifiers.length) {
+        path.prune();
+      }
+    });
+
+  if (!resultSpecifiers.size) {
+    return;
+  }
+
+  addImports(
+    root,
+    Array.from(resultSpecifiers.keys())
+      .sort()
+      .map(source =>
+        j.importDeclaration(
+          resultSpecifiers.get(source).sort(),
+          j.stringLiteral(source),
+        ),
+      ),
+  );
+
+  return root.toSource(
+    options.printOptions || {
+      quote: 'single',
+      trailingComma: true,
+    },
+  );
+};
+
+export default transform;
