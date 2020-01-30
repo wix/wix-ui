c4f43f6fca97a2c202acb75de74730ea732fa60b
nitayneeman <nitayneeman@users.noreply.github.com>
2020-01-30 11:20:40 +0200

    test(Carousel): fix incorrect API

--- packages/wsr-types/__tests__/Carousel-tests.tsx
+++ packages/wsr-types/__tests__/Carousel-tests.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import Carousel from "wix-style-react/Carousel";
-import { carouselTestkitFactory } from "wix-style-react/dist/testkit";
-import { carouselTestkitFactory as carouselEnzymeTestkitFactory } from "wix-style-react/dist/testkit/enzyme";
-import { carouselTestkitFactory as carouselPuppeteerTestkitFactory } from "wix-style-react/dist/testkit/puppeteer";
-import * as enzyme from "enzyme";
-import * as puppeteer from "puppeteer";
+import * as React from 'react';
+import Carousel from 'wix-style-react/Carousel';
+import { carouselTestkitFactory } from 'wix-style-react/dist/testkit';
+import { carouselTestkitFactory as carouselEnzymeTestkitFactory } from 'wix-style-react/dist/testkit/enzyme';
+import { carouselTestkitFactory as carouselPuppeteerTestkitFactory } from 'wix-style-react/dist/testkit/puppeteer';
+import * as enzyme from 'enzyme';
+import * as puppeteer from 'puppeteer';
 
 function CarouselWithMandatoryProps() {
   return <Carousel />;
@@ -20,29 +20,29 @@ function CarouselWithAllProps() {
       className="cls"
       dataHook="hook"
       dots
-      images={["a"]}
+      images={[{ src: 'a' }]}
       infinite
       initialSlideIndex={1}
       variableWidth
     />
   );
 }
-document.querySelectorAll
+document.querySelectorAll;
 async function testkits() {
   const testkit = carouselTestkitFactory({
-    dataHook: "hook",
-    wrapper: document.createElement("div")
+    dataHook: 'hook',
+    wrapper: document.createElement('div')
   });
 
   const enzymeTestkit = carouselEnzymeTestkitFactory({
-    dataHook: "hook",
+    dataHook: 'hook',
     wrapper: enzyme.mount(<div />)
   });
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const puppeteerTestkit = await carouselPuppeteerTestkitFactory({
-    dataHook: "hook",
+    dataHook: 'hook',
     page
   });
 }
