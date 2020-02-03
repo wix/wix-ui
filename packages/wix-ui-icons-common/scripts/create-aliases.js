2cabe8900f7c77ef3069bfb396ed2bb9a12639cf
moshek <moshek@wix.com>
2020-02-03 17:28:56 +0200

    create d-ts for the icons

--- packages/wix-ui-icons-common/scripts/create-aliases.js
+++ packages/wix-ui-icons-common/scripts/create-aliases.js
@@ -1,21 +1,35 @@
-const path = require('path');
-const fse = require('fs-extra');
+const path = require("path");
+const fse = require("fs-extra");
 const [from, to, indexPath] = process.argv.slice(2);
 
-if (to && to !== '.' ) {
+if (to && to !== ".") {
   fse.removeSync(to);
   fse.ensureDirSync(to);
 }
-fse.readdirSync(from)
-  .filter(file => file.endsWith('.js'))
-  .forEach(file => fse.writeFileSync(
-    `${to}/${file}`,
-    `module.exports = require('./${path.relative(to, from)}/${file}');\n`
-  ));
+fse
+  .readdirSync(from)
+  .filter(file => file.endsWith(".js"))
+  .forEach(file => {
+    const fileName = file.split(".").shift();
+    const tsFile = `${fileName}.d.ts`;
+    fse.writeFileSync(
+      `${to}/${tsFile}`,
+      [
+        `export * from './${path.relative(to, from)}/${fileName}';`,
+        `import defaultExport from './${path.relative(to, from)}/${fileName}';`,
+        `export default defaultExport`
+      ].join("\n")
+    );
+
+    return fse.writeFileSync(
+      `${to}/${file}`,
+      `module.exports = require('./${path.relative(to, from)}/${file}');\n`
+    );
+  });
 
 if (indexPath) {
   fse.writeFileSync(
     `${to}/index.js`,
     `module.exports = require('./${path.relative(to, indexPath)}');\n`
-  )
+  );
 }
