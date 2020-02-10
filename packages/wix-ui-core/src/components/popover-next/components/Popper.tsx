a6ffd2f5771bd396aa260cc7c79ea49647ef55e7
mykas <mykolass@wix.com>
2020-02-10 14:31:37 +0200

    <PopoverNext/> - move css transition to popper file

--- packages/wix-ui-core/src/components/popover-next/components/Popper.tsx
+++ packages/wix-ui-core/src/components/popover-next/components/Popper.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition as Transition } from 'react-transition-group';
 import { Popper as ReactPopper } from 'react-popper';
 
 import Portal from './Portal';
-import CSSTransitionWrapper from './CSSTransitionWrapper';
 
 import { getModifiers } from '../../popover/utils/getModifiers';
 import { getArrowShift } from '../../popover/utils/getArrowShift';
@@ -31,6 +30,28 @@ const Arrow = props => {
   return <div {...commonProps} className={styles.arrow} />;
 };
 
+const CSSTransition = props => {
+  const { shown, timeout, children, shouldAnimate, detachSyles } = props;
+  return shouldAnimate ? (
+    <Transition
+      in={shown}
+      timeout={timeout}
+      unmountOnExit
+      classNames={{
+        enter: styles['popoverAnimation-enter'],
+        enterActive: styles['popoverAnimation-enter-active'],
+        exit: styles['popoverAnimation-exit'],
+        exitActive: styles['popoverAnimation-exit-active'],
+      }}
+      onExited={detachSyles}
+    >
+      {children}
+    </Transition>
+  ) : (
+    children
+  );
+};
+
 const Popper = (props: any) => {
   const {
     placement,
@@ -48,7 +69,7 @@ const Popper = (props: any) => {
   } = props;
   return (
     <Portal node={portalNode}>
-      <CSSTransitionWrapper Component={CSSTransition} {...props}>
+      <CSSTransition {...props}>
         <ReactPopper modifiers={getModifiers(props)} placement={placement}>
           {({
             ref,
@@ -96,7 +117,7 @@ const Popper = (props: any) => {
             );
           }}
         </ReactPopper>
-      </CSSTransitionWrapper>
+      </CSSTransition>
     </Portal>
   );
 };
