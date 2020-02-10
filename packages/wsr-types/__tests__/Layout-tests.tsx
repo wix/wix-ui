c618c0bd915d1ce300ac193f91af97e424f01c8b
nitayneeman <nitayneeman@users.noreply.github.com>
2020-02-10 16:58:06 +0200

    test: types

---
+++ packages/wsr-types/__tests__/Layout-tests.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Layout, Cell } from 'wix-style-react/Layout';
+
+function LayoutWithMandatoryProps() {
+  return (
+    <Layout>
+      <Cell />
+    </Layout>
+  );
+}
+
+function LayoutWithAllProps() {
+  return (
+    <Layout dataHook="hook" gap={30} cols={12}>
+      <Cell span={12} vertical>
+        Text
+      </Cell>
+    </Layout>
+  );
+}
