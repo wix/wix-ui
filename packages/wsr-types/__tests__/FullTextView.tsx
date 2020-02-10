ad995999de75fdba7e1ff61836bd7367fe26f88f
Gilad Segal <gilads@wix.com>
2020-02-10 17:53:20 +0200

    Add <FullTextView> types

---
+++ packages/wsr-types/__tests__/FullTextView.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import FullTextView from 'wix-style-react/FullTextView';
+
+function FullTextViewWithMandatoryProps() {
+  return <FullTextView />;
+}
+
+function FullTextViewWithAllProps() {
+  return <FullTextView maxWidth="10px" />;
+}
