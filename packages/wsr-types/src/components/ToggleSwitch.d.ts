ebd3577f03257ba6d662c2d0c93a6b68d20bd414
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-02-10 17:22:29 +0200

    adding d.ts file

---
+++ packages/wsr-types/src/components/ToggleSwitch.d.ts
@@ -0,0 +1,31 @@
+declare namespace __WSR {
+  namespace ToggleSwitch {
+    export type ToggleSwitchProps = Button.ButtonWithAsProp<{
+      dataHook?: string;
+      skin?: ToggleSwitchSkin;
+      size?: ToggleSwitchSize;
+      checked?: boolean;
+      disabled?: boolean;
+      id: string;
+      onChange?(): void;
+      tabIndex: number;
+    }>;
+
+    export class ToggleSwitch extends React.Component<ToggleSwitchProps> {}
+
+    export type ToggleSwitchSkin =  'standard' | 'success' | 'error';
+    export type ToggleSwitchSize =  'small' | 'medium' | 'large';
+
+  }
+}
+
+declare module 'wix-style-react' {
+  export import ToggleSwitch = __WSR.ToggleSwitch.ToggleSwitch;
+  export import ToggleSwitchProps = __WSR.ToggleSwitch.ToggleSwitchProps;
+}
+
+declare module 'wix-style-react/ToggleSwitch' {
+  export type ToggleSwitchProps = __WSR.ToggleSwitch.ToggleSwitchProps;
+  export default __WSR.ToggleSwitch.ToggleSwitch;
+}
+
