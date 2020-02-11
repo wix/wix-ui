2d61901e7c070a1d7e58cce1476cc4b5b6c70953
moshek <moshek@wix.com>
2020-02-11 12:14:06 +0200

    driver - getContent should return string

--- packages/wsr-types/src/drivers/RichTextInputArea.uni.driver.d.ts
+++ packages/wsr-types/src/drivers/RichTextInputArea.uni.driver.d.ts
@@ -2,7 +2,7 @@ declare namespace __WSRTests {
   interface RichTextInputAreaUniDriver extends BaseUniDriver {
     isDisabled: () => Promise<boolean>;
     hasError: () => Promise<boolean>;
-    getContent: () => Promise<HTMLElement>;
+    getContent: () => Promise<string>;
     getPlaceholder: () => Promise<string | null>;
     getErrorMessage: () => Promise<string>;
     enterText: (value: string) => Promise<void>;
