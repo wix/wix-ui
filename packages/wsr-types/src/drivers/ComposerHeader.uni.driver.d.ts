2601e9579479e9222ba3126035bc0bfc5ebe7e51
moshek <moshek@wix.com>
2020-02-11 13:07:50 +0200

    remove comments from driver

--- packages/wsr-types/src/drivers/ComposerHeader.uni.driver.d.ts
+++ packages/wsr-types/src/drivers/ComposerHeader.uni.driver.d.ts
@@ -2,15 +2,10 @@ declare namespace __WSRTests {
   interface ComposerHeaderUniDriver extends BaseUniDriver {
     saveStatusExists: (dataHook: string) => Promise<boolean>;
     getSaveStatusValue: (dataHook: string) => Promise<string>;
-    /** returns true if action exists. Requires dataHook. */
     actionsExists: (dataHook: string) => Promise<boolean>;
-    /** return true if main action exists */
     mainActionsExists: (dataHook: string) => Promise<boolean>;
-    /** return true if backButton exists. */
     backButtonExists: () => Promise<boolean>;
-    /** returns back button value in string */
     getBackButtonText: () => Promise<string>;
-    /** clicks on back button */
     clickBack: () => Promise<void>;
   }
 }
