d1142e7b458864d451aede5394485e309edd16da
mykas <mykolass@wix.com>
2020-03-20 14:42:26 +0200

    Change more

--- packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/named-imports.input.js
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/named-imports.input.js
@@ -1,8 +1,6 @@
 import Button from 'wix-style-react/Button';
 import { Avatar } from 'wix-style-react';
 import PopoverMenu, {PopoverMenuProps} from 'wix-style-react/beta/PopoverMenu';
-import Modal from 'wix-style-react/dist/src/Modal/Modal';
-import RadioGroup, {RadioButton} from 'wix-style-react/dist/src/RadioGroup';
 import WSRTextButton from 'wix-style-react/TextButton';
 import * as SomeIconButton from 'wix-style-react/IconButton';
 import Media, { MediaProps } from 'wix-style-react/Media';
@@ -11,3 +9,5 @@ import { buttonTestkitFactory } from 'wix-style-react/testkit/enzyme';
 import something from 'around-here';
 import icon from 'wix-style-react/new-icons';
 import meh from 'wix-style-react/dist/src/styles.scss'
+import Modal from 'wix-style-react/dist/src/Modal/Modal';
+import RadioGroup, {RadioButton} from 'wix-style-react/dist/src/RadioGroup';
