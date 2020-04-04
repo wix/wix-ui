66a12e7e077defab8dcdd140f546eb3992cefac8
mykas <mykolass@wix.com>
2020-04-04 16:10:10 +0300

    <Popover/> - refactory story

--- packages/wix-ui-core/stories/Popover/Popover.story.tsx
+++ packages/wix-ui-core/stories/Popover/Popover.story.tsx
@@ -4,6 +4,7 @@ import {
   code,
   columns,
   description,
+  testkit,
   divider,
   header,
   importExample,
@@ -20,53 +21,6 @@ const example = config => baseExample({ components: baseScope, ...config });
 
 import * as examples from './examples';
 
-// // Click target to open close
-// class PopoverWithState extends React.Component<
-//   Partial<PopoverProps>,
-//   { shown: boolean }
-// > {
-//   state = { shown: true };
-
-//   render() {
-//     const props: PopoverProps & { children?: any } = {
-//       placement: 'right',
-//       showArrow: true,
-//       shown: this.state.shown,
-//       onClick: () => this.setState({ shown: !this.state.shown }),
-//       ...this.props,
-//     };
-//     return (
-//       <Popover {...props}>
-//         <Popover.Element>The Element</Popover.Element>
-//         <Popover.Content>The content</Popover.Content>
-//       </Popover>
-//     );
-//   }
-// }
-
-// const ScrollableContainer = props => (
-//   <div
-//     style={{
-//       textAlign: 'center',
-//       overflow: 'hidden',
-//       position: 'relative',
-//       border: '1px solid black',
-//       width: 250,
-//       margin: 10,
-//     }}
-//   >
-//     <div
-//       data-hook={props.dataHook}
-//       style={{
-//         overflow: 'auto',
-//         height: 100,
-//       }}
-//     >
-//       <div style={{ padding: '25px 25px 80px' }}>{props.children}</div>
-//     </div>
-//   </div>
-// );
-
 export default {
   category: Category.COMPONENTS,
   storyName: 'Popover',
@@ -74,6 +28,19 @@ export default {
   componentPath: '../../src/components/popover-next/popover-next.tsx',
   componentProps: {
     'data-hook': 'storybook-popover',
+    children: [
+      <Popover.Element key="1">This is the Popover.Element</Popover.Element>,
+      <Popover.Content key="2">Content</Popover.Content>,
+    ],
+    appendTo: 'window',
+    showArrow: true,
+    timeout: 150,
+    shown: false,
+    fluid: false,
+    placement: 'top',
+  },
+
+  exampleProps: {
     children: [
       {
         label: 'Default example',
@@ -85,15 +52,6 @@ export default {
         ],
       },
     ],
-    appendTo: 'window',
-    showArrow: true,
-    timeout: 150,
-    shown: false,
-    fluid: false,
-    placement: 'top',
-  },
-
-  exampleProps: {
     appendTo: [
       { label: 'window', value: window },
       { label: 'scrollParent', value: 'scrollParent' },
@@ -151,12 +109,74 @@ export default {
             text: 'A simple example of Popover usage',
             source: examples.simple,
           }),
+
+          example({
+            title: 'Placement',
+            text: 'There is a variety of options for placement selection.',
+            source: examples.placement,
+          }),
+
+          example({
+            title: 'MoveBy',
+            text:
+              '<em>x</em> and <em>y</em> axis orientation is relative to the placement of the popover.',
+            source: examples.moveBy,
+          }),
+
+          example({
+            title: 'zIndex',
+            text:
+              'If some container got higher zIndex then Popover content - the zIndex of Popover content can be controlled',
+            source: examples.zIndex,
+          }),
+
+          title('Floating Behaviour'),
+
+          example({
+            title: 'Flip:Enabled & Fixed: Disabled',
+            text:
+              'Focus target element (TAB) and scroll viewport to see behaviour',
+            source: examples.flip,
+          }),
+
+          example({
+            title: 'Flip: Disabled & Fixed: Enabled',
+            text:
+              'Focus target element (TAB) and scroll viewport to see behaviour',
+            source: examples.fixed,
+          }),
+
+          title('Handling Overflow'),
+
+          example({
+            title: 'appendTo="window"',
+            text: `If you inspect the content, you'll see it is attached to a new div under the body.`,
+            source: examples.window,
+          }),
+
+          example({
+            title: 'appendTo="scrollParent"',
+            text: `If you inspect the content, you'll see it is attached to a new div under the list container.`,
+            source: examples.scrollParent,
+          }),
+
+          example({
+            title: 'appendTo="viewport"',
+            text: `If you inspect the content, you'll see it is attached to a document.body but the popvoer content element reacts to screen viewport.`,
+            source: examples.viewPort,
+          }),
+
+          example({
+            title: `appendTo={elm => elm.getAttribute('attribute') === 'value'}`,
+            text: `Sometimes we want a to target certain overflow element. A function can be passed that will be run against all parent elements until found.If you inspect the content, you'll see it is attached to a document.body but the popvoer content element reacts to screen viewport.`,
+            source: examples.predicate,
+          }),
         ],
       }),
 
       ...[
         { title: 'API', sections: [api()] },
-        { title: 'Testkit', sections: [description()] },
+        { title: 'Testkit', sections: [testkit()] },
         { title: 'Playground', sections: [playground()] },
       ].map(tab),
     ]),
@@ -186,33 +206,33 @@ export default {
   //         If you inspect the content, you'll see it is attached to a new div
   //         under the list container.
   //       </p>
-  //       <div
-  //         style={{
-  //           overflow: 'auto',
-  //           height: '100px',
-  //           width: '400px',
-  //           border: '1px solid black',
-  //         }}
-  //       >
-  //         <ul>
-  //           <li>item</li>
-  //           <PopoverWithState
-  //             appendTo="scrollParent"
-  //             data-hook="popover-appendto-scroll-parent"
-  //           />
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //           <li>item</li>
-  //         </ul>
-  //       </div>
-  //     </div>
+  //   <div
+  //     style={{
+  //       overflow: 'auto',
+  //       height: '100px',
+  //       width: '400px',
+  //       border: '1px solid black',
+  //     }}
+  //   >
+  //     <ul>
+  //       <li>item</li>
+  //       <PopoverWithState
+  //         appendTo="scrollParent"
+  //         data-hook="popover-appendto-scroll-parent"
+  //       />
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //       <li>item</li>
+  //     </ul>
+  //   </div>
+  // </div>
   //     <div>
   //       <h2>moveBy={'{x:50, y:100}'}</h2>
   //       <p>
@@ -377,3 +397,67 @@ export default {
   //   </div>
   // ),
 };
+
+// export const hello = () => {
+//   const [shown, setShown] = React.useState(true);
+//   return (
+//     <div data-hook="story-popover-z-index" style={{ height: '50px' }}>
+//       <div styles={{ display: 'flex' }}>
+//         <div style={{ position: 'relative' }}>
+//           <div style={{ position: 'absolute', top: '10px', left: '0px' }}>
+//             <Popover
+//               shown={shown}
+//               fixed={true}
+//               onClick={() => setShown(!shown)}
+//               placement="top"
+//               showArrow
+//             >
+//               <Popover.Element>The Element</Popover.Element>
+//               <Popover.Content>The content</Popover.Content>
+//             </Popover>
+//           </div>
+//           <div
+//             style={{
+//               zIndex: 2000,
+//               position: 'absolute',
+//               top: '0px',
+//               left: '0px',
+//               backgroundColor: 'lightgray',
+//               width: '200px',
+//               height: '50px',
+//             }}
+//           >
+//             cover
+//           </div>
+//         </div>
+//         <div style={{ position: 'relative' }}>
+//           <div style={{ position: 'absolute', top: '10px', left: '0px' }}>
+//             <Popover
+//               shown={shown}
+//               fixed={true}
+//               onClick={() => setShown(!shown)}
+//               placement="top"
+//               showArrow
+//             >
+//               <Popover.Element>The Element</Popover.Element>
+//               <Popover.Content>The content</Popover.Content>
+//             </Popover>
+//           </div>
+//           <div
+//             style={{
+//               zIndex: 2000,
+//               position: 'absolute',
+//               top: '0px',
+//               left: '0px',
+//               backgroundColor: 'lightgray',
+//               width: '200px',
+//               height: '50px',
+//             }}
+//           >
+//             cover
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
---
+++ packages/wix-ui-core/stories/Popover/examples.ts
@@ -0,0 +1,319 @@
+export const simple = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <Popover shown={shown} onClick={() => setShown(!shown)} placement="top" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>The content</Popover.Content>
+    </Popover>
+  )
+}
+`;
+
+export const placement = `
+() => {
+  const [placement, setPlacement] = React.useState(0);
+  const VALID_PLACEMENTS =  ['auto-start'
+  , 'auto'
+  , 'auto-end'
+  , 'top-start'
+  , 'top'
+  , 'top-end'
+  , 'right-start'
+  , 'right'
+  , 'right-end'
+  , 'bottom-end'
+  , 'bottom'
+  , 'bottom-start'
+  , 'left-end'
+  , 'left'
+  , 'left-start' ]
+
+  React.useEffect(() => {
+    setTimeout(() => setPlacement((placement + 1) % VALID_PLACEMENTS.length), 1000)
+  }, [placement])
+
+  return (
+    <Popover shown flip={false} placement={VALID_PLACEMENTS[placement]} showArrow>
+      <Popover.Element><div style={{ width: '100px', height: '50px',  backgroundColor: 'lightgray'}}/></Popover.Element>
+      <Popover.Content>{VALID_PLACEMENTS[placement]}</Popover.Content>
+    </Popover>
+  )
+}
+`;
+
+export const moveBy = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <Popover shown={shown} onClick={() => setShown(!shown)} moveBy={{ x: 50, y: 100 }} placement="right" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>The content</Popover.Content>
+    </Popover>
+  )
+}
+`;
+
+export const zIndex = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <div data-hook="story-popover-z-index">
+    <div style={{ display: 'flex' }}>
+      <div style={{ display: 'block', position: 'relative', width: '50%', marginRight: '30px', height: '50px' }}>
+        <div style={{ position: 'absolute', top: '10px', left: '0px' }}>
+          <Popover
+            shown={shown}
+            flip={false}
+            fixed={true}
+            onClick={() => setShown(!shown)}
+            placement="top"
+          >
+            <Popover.Element>The Element</Popover.Element>
+            <Popover.Content>zIndex 1000</Popover.Content>
+          </Popover>
+        </div>
+        <div
+          style={{
+            zIndex: 2000,
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            backgroundColor: 'lightgray',
+            width: '150px',
+            height: '50px',
+          }}
+        >
+        zIndex 2000
+        </div>
+      </div>
+      <div style={{ display: 'block', position: 'relative', width: '50%', height: '50px' }}>
+        <div style={{ position: 'absolute', top: '10px', left: '0px' }}>
+          <Popover
+            shown={shown}
+            zIndex={3000}
+            flip={false}
+            fixed={true}
+            onClick={() => setShown(!shown)}
+            placement="top"
+          >
+            <Popover.Element>The Element</Popover.Element>
+            <Popover.Content>zIndex 3000</Popover.Content>
+          </Popover>
+        </div>
+        <div
+          style={{
+            zIndex: 2000,
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            backgroundColor: 'lightgray',
+            width: '150px',
+            height: '50px',
+          }}
+        >
+          zIndex 2000
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+
+}
+`;
+
+export const flip = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <div
+    style={{
+      overflow: 'hidden',
+      position: 'relative',
+      border: '1px solid black',
+    }}
+  >
+    <div
+    style={{
+      overflow: 'auto',
+      height: '100px',
+      zIndex: '15',
+    }}
+  >
+    <ul>
+      <li>item</li>
+      <Popover flip={true} fixed={false} zIndex={1} shown={shown} onClick={() => setShown(!shown)} placement="top" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>The content</Popover.Content>
+    </Popover>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+    </ul>
+  </div>
+  </div>
+  )
+}
+`;
+
+export const fixed = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <div
+    style={{
+      overflow: 'hidden',
+      position: 'relative',
+      border: '1px solid black',
+    }}
+  >
+    <div
+    style={{
+      overflow: 'auto',
+      height: '100px',
+      zIndex: '15',
+    }}
+  >
+    <ul>
+      <li>item</li>
+      <Popover flip={false} fixed={true} zIndex={1} shown={shown} onClick={() => setShown(!shown)} placement="top" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>The content</Popover.Content>
+    </Popover>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+    </ul>
+  </div>
+  </div>
+  )
+}
+`;
+
+export const window = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <Popover data-hook="popover-appendto-window" appendTo="window" shown={shown} onClick={() => setShown(!shown)} placement="right" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>The content</Popover.Content>
+    </Popover>
+  )
+}
+`;
+
+export const scrollParent = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <div
+    style={{
+      overflow: 'hidden',
+      position: 'relative',
+      border: '1px solid black',
+    }}
+  >
+    <div
+    style={{
+      overflow: 'auto',
+      height: '100px',
+      zIndex: '15',
+    }}
+  >
+    <ul>
+      <li>item</li>
+      <Popover data-hook="popover-appendto-scroll-scrollParent" zIndex={1} appendTo="scrollParent" shown={shown} onClick={() => setShown(!shown)} placement="right" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>The content</Popover.Content>
+    </Popover>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+    </ul>
+  </div>
+  </div>
+  )
+}
+`;
+
+export const viewPort = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <Popover data-hook="popover-appendto-scroll-viewport" appendTo="viewport" shown={shown} onClick={() => setShown(!shown)} placement="top" showArrow>
+      <Popover.Element>The Element</Popover.Element>
+      <Popover.Content>I am attached to viewport</Popover.Content>
+    </Popover> 
+  )
+}
+`;
+
+export const predicate = `
+() => {
+  const [shown, setShown] = React.useState(true)
+  return (
+    <div data-hook="hello" style={{ padding: '15px', overflow: 'hidden', position: 'relative', border: '1px solid black' }}>
+      <div
+      style={{
+        overflow: 'hidden',
+        position: 'relative',
+        border: '1px solid black',
+      }}
+    >
+      <div
+      style={{
+        overflow: 'auto',
+        height: '100px',
+        zIndex: '15',
+      }}
+    >
+      <ul>
+        <li>item</li>
+        <Popover 
+            data-hook="popover-appendto-scroll-custom" 
+            appendTo={elm => elm.getAttribute('data-hook') === 'hello'} 
+            shown={shown} onClick={() => setShown(!shown)} 
+            placement="right" 
+            showArrow
+        >
+        <Popover.Element>The Element</Popover.Element>
+        <Popover.Content>The content</Popover.Content>
+      </Popover>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+    </div>
+    </div>
+  </div>
+  )
+}
+`;
