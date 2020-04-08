9de621518f878221b2c54fdee8dc5e267e62a6a6
mykas <mykolass@wix.com>
2020-04-08 15:15:08 +0300

    Upgrade storybook

--- packages/wix-ui-core/.storybook/config.js
+++ packages/wix-ui-core/.storybook/config.js
@@ -1,18 +1,23 @@
-import {addParameters, configure} from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import { create } from '@storybook/theming';
 
 function loadStories() {
-    require('../stories');
-    require('./stories.scss');
+  require('../stories');
+  require('./stories.scss');
 }
 
+const theme = create({
+  base: 'light',
+  brandTitle: `Wix Ui Core 2`,
+  brandUrl: 'https://github.com/wix/wix-ui-core',
+});
 
 addParameters({
-    options: {
-        name: 'wix-ui-core',
-        url: 'https://github.com/wix/wix-ui/tree/master/packages/wix-ui-core',
-        showPanel: false,
-        sidebarAnimations: true
-    }
+  options: {
+    theme,
+    showPanel: false,
+    isToolshown: true,
+  },
 });
 
 configure(loadStories, module);
