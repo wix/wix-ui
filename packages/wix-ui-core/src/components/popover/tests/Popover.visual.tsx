881bfad4bc75b4dd066c2da1395a1951b6635ef4
mykas <mykolass@wix.com>
2020-04-08 09:31:25 +0300

    Implement eyes visual

--- packages/wix-ui-core/src/components/popover/tests/Popover.visual.tsx
+++ packages/wix-ui-core/src/components/popover/tests/Popover.visual.tsx
@@ -23,4 +23,37 @@ visualize('Popover', () => {
       </div>
     ));
   });
+
+  story('zIndex', () => {
+    snap('should set zindex', () => (
+      <div
+        style={{
+          display: 'block',
+          position: 'relative',
+          width: '50%',
+          height: '50px',
+        }}
+      >
+        <div style={{ position: 'absolute', top: '10px', left: '0px' }}>
+          <Popover shown zIndex={3000} flip={false} fixed placement="top">
+            <Popover.Element>The Element</Popover.Element>
+            <Popover.Content>zIndex 3000</Popover.Content>
+          </Popover>
+        </div>
+        <div
+          style={{
+            zIndex: 2000,
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            backgroundColor: 'lightgray',
+            width: '150px',
+            height: '50px',
+          }}
+        >
+          zIndex 2000
+        </div>
+      </div>
+    ));
+  });
 });
