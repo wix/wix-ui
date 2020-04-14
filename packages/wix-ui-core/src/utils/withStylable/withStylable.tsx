8910448601cbe3eb9641503b225c6727433253d2
argshook <argshook@gmail.com>
2020-04-14 18:11:01 +0300

    chore(utils/withStylable): bring back required types
    for now, leaving required types for withStylable.
    
    this util should not be used at all, but leaving it for now until
    further investigation

--- packages/wix-ui-core/src/utils/withStylable/withStylable.tsx
+++ packages/wix-ui-core/src/utils/withStylable/withStylable.tsx
@@ -8,6 +8,38 @@ function isComponentInstance(value: any): value is React.Component {
   return value && value instanceof React.Component;
 }
 
+type StateValue = boolean | number | string;
+
+interface StateMap {
+  [stateName: string]: StateValue;
+}
+
+interface AttributeMap {
+  className?: string;
+  [attributeName: string]: StateValue | undefined;
+}
+
+interface InheritedAttributes {
+  className?: string;
+  [props: string]: any;
+}
+
+type RuntimeStylesheet = {
+  (
+    className: string,
+    states?: StateMap,
+    inheritedAttributes?: InheritedAttributes,
+  ): AttributeMap;
+  $root: string;
+  $namespace: string;
+  $depth: number;
+  $id: string | number;
+  $css?: string;
+
+  $get(localName: string): string | undefined;
+  $cssStates(stateMapping?: StateMap | null): StateMap;
+} & { [localName: string]: string };
+
 function withStylableStateful<CoreProps, ExtendedProps = {}>(
   Component: React.ComponentClass<CoreProps>,
   stylesheet: RuntimeStylesheet,
