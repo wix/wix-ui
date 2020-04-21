5b27aa0651a9d0afea2060d24a309ae93edbd1a3
argshook <argshook@gmail.com>
2020-04-21 09:48:30 +0300

    fix(migrate-wsr8): add Card.Divider only if Card.Header lacks withoutDivider

--- packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/migrate-wsr8/components.output.js
+++ packages/wix-ui-codemod/src/wix-style-react/__testfixtures__/migrate-wsr8/components.output.js
@@ -18,15 +18,15 @@ import {
 
 export default class extends React.Component {
   render() {
-    const otherProps = { one: 1, two: 2, three: 3 };
+    const otherProps = { one: 1, two: 2, three: 3, withoutDivider: false };
     return (
       <Page dataHook="shipping-label-page">
         <Page.Header title="header title" showBackButton={true} />
         <Page.Content>
           <Card>
             <Card.Header />
-            <Card.Divider />
             <InputWithOptions />
+
             <Card.Content>
               <Tooltip
                 appendTo="window"
@@ -47,21 +47,31 @@ export default class extends React.Component {
 
           <Card>
             <Card.Header />
-            <Card.Divider />
             <Text>Awesome text #2</Text>
             <BarChart />
+
             <Card.Content>
               <Tooltip appendTo="window" content={<div>content</div>}>
                 <DropdownLayout />
               </Tooltip>
 
               <Card>
-                <Card.Header title="Dont touch me" />
+                <Card.Header title="Add divider" />
+                <Card.Divider />
               </Card>
 
               <Card>
-                <Card.Header title="Insert divider here" />
+                <Card.Header title="No divider" />
+              </Card>
+
+              <Card>
+                <Card.Header title="Add divider" />
                 <Card.Divider />
+                <div>something</div>
+                <Card.Header title="Add divider" />
+                <Card.Divider />
+                <Card.Header title="no divider" />
+                <Card.Header title="no divider" />
               </Card>
             </Card.Content>
           </Card>
