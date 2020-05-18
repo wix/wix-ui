928803b78d31bf31d67d0d5b49667b5b32da216c
argshook <argshook@gmail.com>
2020-05-18 13:47:52 +0300

    chore(filterDataProps): dont check for inherited props

--- packages/wix-ui-core/src/utils/filter-data-props.ts
+++ packages/wix-ui-core/src/utils/filter-data-props.ts
@@ -4,6 +4,7 @@ export const filterDataProps = (props: object) => {
   const output = {};
   for (const key in props) {
     if (
+      props.hasOwnProperty(key) &&
       key.length > acceptedPropsStart.length &&
       key.startsWith(acceptedPropsStart)
     ) {
