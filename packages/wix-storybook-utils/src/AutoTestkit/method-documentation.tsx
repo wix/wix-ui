d807482f04ffffb050868954b3b9247fd50b36d2
Egozi <egozieran@gmail.com>
2020-08-23 10:20:37 +0300

    Unified Testkit Documentation (#1905)
    * Unified Testkit Documentation
    
    * Unified Testkit Documentation
    
    * Unified Testkit Documentation
    
    * Filter overridden functions
    
    * remove log

--- packages/wix-storybook-utils/src/AutoTestkit/method-documentation.tsx
+++ packages/wix-storybook-utils/src/AutoTestkit/method-documentation.tsx
@@ -22,15 +22,77 @@ const FunctionArguments = ({ args }) => {
   );
 };
 
+const paramDocumentation = ({ title, name, description }, index) => {
+  return (
+    <li key={index}>
+      <b>{title}</b> {name}
+      {description && ` - ${description}`}
+    </li>
+  );
+};
+
+const returnsDocumentation = ({ title, type, description }, index) => {
+  if (!type) {
+    return;
+  }
+
+  const returnType = type.applications
+    .map(app => {
+      if (app.name) {
+        return app.name;
+      }
+
+      if (app.type === 'UnionType') {
+        return app.elements
+          .map(element => {
+            if (element.value) {
+              return `'${element.value}'`;
+            }
+            return element.name;
+          })
+          .join(' | ');
+      }
+    })
+    .join(', ');
+
+  return (
+    <li key={index}>
+      <b>{title}</b> {`Promise<${returnType}>`}
+      {description && ` - ${description}`}
+    </li>
+  );
+};
+
 export const MethodDocumentation = ({ unit }) => {
-  const { args, name } = unit;
+  const { args, name, tags } = unit;
   return (
     <tr className="auto-testkit-field">
       <td>
         <span data-hook="auto-testkit-function-name">{name}</span>(
         <FunctionArguments args={args} />)
       </td>
-      <td data-hook="auto-testkit-function-description">{unit.description}</td>
+      <td data-hook="auto-testkit-function-description">
+        <div>{unit.description}</div>
+        {tags && tags.length > 0 && (
+          <ul>
+            {tags.map((tag, index) => {
+              if (tag.title === 'param') {
+                return paramDocumentation(tag, index);
+              }
+
+              if (tag.title === 'returns' || tag.title === 'return') {
+                return returnsDocumentation(tag, index);
+              }
+
+              return (
+                <li key={index}>
+                  <b>{tag.title}</b> {tag.description}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </td>
     </tr>
   );
 };
