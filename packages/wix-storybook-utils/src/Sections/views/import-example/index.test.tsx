c2f6fb6632b80217702345aa791f819ec0c0fd23
Arijus Šukys <arijuss@wix.com>
2020-08-24 18:00:42 +0300

    wix-storybook-utils/import example (#1908)
    * wix-storybook-utils(importExample): fix section to show default
    
    when no arguments passed,
    it should display a configured string from `.storybook/webpack.config.js`
    
    * test(importExample): assert case with no arguments

--- packages/wix-storybook-utils/src/Sections/views/import-example/index.test.tsx
+++ packages/wix-storybook-utils/src/Sections/views/import-example/index.test.tsx
@@ -3,6 +3,11 @@ import { mount } from 'enzyme';
 import { importExample as view } from './';
 import { importExample as builder } from '../../index';
 
+const storyConfigFixture = {
+  metadata: { displayName: 'Component', props: {} },
+  config: {},
+};
+
 describe('importExample view', () => {
   it('should pass trimmed and ticked source to markdown', () => {
     const source = `
@@ -13,7 +18,27 @@ describe('importExample view', () => {
 \"hello\"
 \`\`\``;
 
-    const rendered = mount(view(builder({ source })));
+    const rendered = mount(view(builder({ source }), storyConfigFixture));
     expect(rendered.find('Markdown').prop('source')).toEqual(assertion);
+  });
+
+  describe('given no arguments', () => {
+    it('should construct importExample from StoryConfig', () => {
+      const assertion = `\`\`\`js
+import Component from 'library/Component';
+\`\`\``;
+
+      const rendered = mount(
+        view(builder(), {
+          ...storyConfigFixture,
+          config: {
+            importFormat:
+              "import %componentName from '%moduleName/%componentName';",
+            moduleName: 'library',
+          },
+        }),
+      );
+      expect(rendered.find('Markdown').prop('source')).toEqual(assertion);
+    });
   });
 });
--- packages/wix-storybook-utils/src/Sections/views/import-example/index.tsx
+++ packages/wix-storybook-utils/src/Sections/views/import-example/index.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
 
+import { StoryConfig } from '../../../typings/story-config';
 import { ImportExampleSection } from '../../../typings/story-section';
+import { importString } from '../../../StoryPage/import-string';
 
 import { Code } from './Code';
 
-export const importExample: (a: ImportExampleSection) => React.ReactNode = ({
-  dataHook = '',
-  source,
-}) =>
+export const importExample: (
+  a: ImportExampleSection,
+  b: StoryConfig,
+) => React.ReactNode = ({ dataHook = '', source }, storyConfig) =>
   typeof source === 'string' ? (
     <Code data-hook={dataHook}>{source.trim()}</Code>
   ) : (
-    source
+    source || (
+      <Code data-hook={dataHook}>
+        {importString({
+          metadata: storyConfig.metadata,
+          config: storyConfig.config,
+          exampleImport: storyConfig.exampleImport,
+        })}
+      </Code>
+    )
   );
