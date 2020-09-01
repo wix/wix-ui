710ee2dbf89026a82fe54f6ab1a740f8258dd001
gendeld <danielge@wix.com>
2020-09-01 15:53:30 +0300

    Don't wait for angular

--- packages/wix-ui-icons-common/protractor.conf.js
+++ packages/wix-ui-icons-common/protractor.conf.js
@@ -3,6 +3,7 @@ const { baseProtractorConfig } = require("wix-ui-test-utils/protractor");
 module.exports.config = {
   ...baseProtractorConfig,
   onPrepare() {
+    browser.ignoreSynchronization = true;
     require("@babel/register")({
       presets: [[require.resolve("babel-preset-yoshi")]],
       ignore: [],
