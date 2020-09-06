48636b1be69d7f6e1c761ae16f61f9cc3e43f334
gendeld <danielge@wix.com>
2020-09-06 11:35:11 +0300

    Change types

--- packages/wix-ui-icons-common/src/external-types.d.ts
+++ packages/wix-ui-icons-common/src/external-types.d.ts
@@ -2,5 +2,5 @@ declare module "*.st.css" {
   const stylesheet: import("@stylable/runtime").RuntimeStylesheet;
   export = stylesheet;
 }
-declare module "*.scss";
-declare module "*.json";
+
+/// <reference types="yoshi/types" />
