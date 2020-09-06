11e75f557c68f25a2f2f58488e8d13574a975831
gendeld <danielge@wix.com>
2020-09-06 11:23:32 +0300

    Add declaration to external-types

--- packages/wix-ui-icons-common/src/external-types.d.ts
+++ packages/wix-ui-icons-common/src/external-types.d.ts
@@ -1,2 +1,6 @@
-declare module '*.scss';
-declare module '*.json';
+declare module "*.st.css" {
+  const stylesheet: import("@stylable/runtime").RuntimeStylesheet;
+  export = stylesheet;
+}
+declare module "*.scss";
+declare module "*.json";
