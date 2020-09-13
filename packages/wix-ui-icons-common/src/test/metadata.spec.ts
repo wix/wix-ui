2158cd3661e2067043cba52f7a2df39b38b8e436
gendeld <danielge@wix.com>
2020-09-13 16:27:48 +0300

    Make tests a bit clearer

--- packages/wix-ui-icons-common/src/test/metadata.spec.ts
+++ packages/wix-ui-icons-common/src/test/metadata.spec.ts
@@ -12,39 +12,45 @@ const getFileNames = (iconsMetadata: Array<IconMetadata>) =>
     return fileNames;
   });
 
-const generalIconFileNames = getFileNames(generalIconsMetadata);
-const systemIconFileNames = getFileNames(systemIconsMetadata);
+const generalIconsWithMetadata = getFileNames(generalIconsMetadata);
+const systemIconsWithMetadata = getFileNames(systemIconsMetadata);
 
 describe("General icons metadata", () => {
   const generalIconComponentNames = Object.keys(generalIconComponents);
+  // File names not included in the deprecated icons array
   const activeGeneralIcons = generalIconComponentNames.filter(
     (iconName) => !deprecatedGeneralIcons.includes(iconName)
   );
+  // Checks that every non-depreacted icon file has metadata
   activeGeneralIcons.forEach((iconName) => {
     it(`${iconName} has metadata`, () => {
-      expect(generalIconFileNames.includes(iconName)).toBe(true);
+      expect(generalIconsWithMetadata.includes(iconName)).toBe(true);
     });
   });
+  // Checks that deprecated files are not described with metadata
   deprecatedGeneralIcons.forEach((iconName) => {
     it(`${iconName} is deprecated`, () => {
-      expect(generalIconFileNames.includes(iconName)).toBe(false);
+      expect(generalIconsWithMetadata.includes(iconName)).toBe(false);
     });
   });
 });
 
 describe("System icons metadata", () => {
   const systemIconComponentNames = Object.keys(systemIconComponents);
+  // File names not included in the deprecated icons array
   const activeSystemIcons = systemIconComponentNames.filter(
     (iconName) => !deprecatedSystemIcons.includes(iconName)
   );
+  // Checks that every non-depreacted icon file has metadata
   activeSystemIcons.forEach((iconName) => {
     it(`${iconName} has metadata`, () => {
-      expect(systemIconFileNames.includes(iconName)).toBe(true);
+      expect(systemIconsWithMetadata.includes(iconName)).toBe(true);
     });
   });
+  // Checks that deprecated files are not described with metadata
   deprecatedSystemIcons.forEach((iconName) => {
     it(`${iconName} is deprecated`, () => {
-      expect(systemIconFileNames.includes(iconName)).toBe(false);
+      expect(systemIconsWithMetadata.includes(iconName)).toBe(false);
     });
   });
 });
