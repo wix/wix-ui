ab8f01a5f93220021d2d7f81dfb16973b70ea979
gendeld <danielge@wix.com>
2020-09-14 17:40:01 +0300

    Reset less important unclear changes

--- packages/wix-storybook-utils/src/Sections/views/code.tsx
+++ packages/wix-storybook-utils/src/Sections/views/code.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import CodeBlock from '../../CodeBlock';
 import { CodeSection } from '../../typings/story-section';
-import styles from './styles.scss';
 
 export const code: (a: CodeSection) => React.ReactNode = ({
   source,
@@ -18,9 +17,7 @@ export const code: (a: CodeSection) => React.ReactNode = ({
   if (interactive) {
     const LiveCodeExample = React.lazy(() => import('../../LiveCodeExample'));
     return (
-      <React.Suspense
-        fallback={<div className={styles.codeLoader}>Loading...</div>}
-      >
+      <React.Suspense fallback={<div>Loading...</div>}>
         <LiveCodeExample
           {...{
             previewProps,
