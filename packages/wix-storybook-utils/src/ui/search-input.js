88a8db08c5493534053d9bba8b6068b948c46bcc
gendeld <danielge@wix.com>
2020-09-21 12:26:49 +0300

    Add search input proptypes

--- packages/wix-storybook-utils/src/ui/search-input.js
+++ packages/wix-storybook-utils/src/ui/search-input.js
@@ -1,20 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Input from './input';
 import Search from 'wix-ui-icons-common/Search';
 
 import styles from './styles.scss';
 
-const SearchInput = ({
-  onChange,
-  dataHook,
-  className,
-  inputClassName,
-  ...props
-}) => (
+HTMLInputElement;
+const SearchInput = ({ dataHook, className, inputClassName, ...props }) => (
   <div className={classnames(styles.searchInputContainer, className)}>
     <Input
-      onChange={onChange}
       placeholder="Search Icons"
       className={classnames(styles.searchInput, inputClassName)}
       data-hook={dataHook}
@@ -23,5 +18,11 @@ const SearchInput = ({
     <Search className={styles.searchIcon} />
   </div>
 );
+
+SearchInput.propTypes = {
+  dataHook: PropTypes.string,
+  className: PropTypes.string,
+  inputClassName: PropTypes.string,
+};
 
 export default SearchInput;
