1aa87cbe33d614f0955c8703802cbd59ab935717
gendeld <danielge@wix.com>
2020-09-21 18:29:32 +0300

    Fix attempt for fuse

--- packages/wix-ui-icons-common/stories/components/category-list/useSearchIcons.ts
+++ packages/wix-ui-icons-common/stories/components/category-list/useSearchIcons.ts
@@ -1,17 +1,12 @@
-import _Fuse = require("fuse.js");
-import type Fuse from "fuse.js";
+import * as Fuse from "fuse.js";
 import { useState, useMemo } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { IconMetadata } from "../../../src/types";
 
-interface FuseConstructor<T>  {
-  new (
-    list: Array<T>,
-    options?: Fuse.IFuseOptions<T>,): Fuse<T>;
-}
+// Fuse type expects ES modules, but is imported as commonjs
+const FuseIndex = Fuse.default || Fuse;
 
-// Fuse type expects ES modules, this retypes it for the commonjs module
-const FuseIndex =_Fuse as unknown as FuseConstructor<IconMetadata>;
+console.log(FuseIndex);
 
 /** Searches icons with `searchCategoryIcons`
  * and sets the resulting categories to state
