bc3868dd1ef8c9b23719c0341e8ee15d78ee4d7e
Jonathan Adler <jonathana@wix.com>
2020-09-21 09:57:49 +0300

    fix(SigningPad): adjust type change
    https://github.com/szimek/signature_pad/commit/3a5d0bb060efd89ca1b25cc87cc2e5e57f9ba0a3#diff-df4090cbd1297e7b2adf8b3e32ab146cR23

--- packages/wix-ui-core/src/components/signature-input/signing-pad/SigningPad.tsx
+++ packages/wix-ui-core/src/components/signature-input/signing-pad/SigningPad.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import SignaturePad, { IOptions } from 'signature_pad';
+import SignaturePad, { Options as IOptions } from 'signature_pad';
 import {
   withSignatureInputContext,
   WithSignaturePadProps,
