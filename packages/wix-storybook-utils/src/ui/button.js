6dbb044d59789a079e8e88cf7b8ead05e473cb30
gendeld <danielge@wix.com>
2020-10-07 16:32:06 +0300

    Revert "[storybook-utils] Add ui button component"
    This reverts commit 3ac4699777a25fa643bbadb7dd023d038604d2a1.

--- packages/wix-storybook-utils/src/ui/button.js
+++ packages/wix-storybook-utils/src/ui/button.js
@@ -1,58 +1,10 @@
-import React, { PureComponent } from 'react';
-import classNames from 'classnames';
-import styles from './button.scss';
-import PropTypes from 'prop-types';
+import { createElement } from 'react';
 
-class Button extends PureComponent {
-  static displayName = 'Button';
+import styles from './styles.scss';
 
-  static propTypes = {
-    /** Additional classes */
-    className: PropTypes.string,
-    /** Size of Button content */
-    size: PropTypes.oneOf(['tiny', 'small', 'medium', 'large']),
-    /** Click event handler  */
-    onClick: PropTypes.func,
-    /** Sets button width to 100% */
-    fullWidth: PropTypes.bool,
-    /** Applies disabled styles */
-    disabled: PropTypes.bool,
-    /** String based node */
-    children: PropTypes.node,
-    /** String based data hook */
-    dataHook: PropTypes.string,
-  };
-
-  static defaultProps = {
-    size: 'medium',
-  };
-
-  render() {
-    const {
-      skin,
-      priority,
-      size,
-      className,
-      fullWidth,
-      children,
-      dataHook,
-      disabled,
-      onClick,
-    } = this.props;
-
-    return (
-      <button
-        className={classNames(className, styles.root, styles[`size-${size}`], {
-          [styles.fluid]: fullWidth,
-        })}
-        disabled={disabled}
-        data-hook={dataHook}
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  }
-}
-
-export default Button;
+export default props =>
+  createElement('button', {
+    type: 'button',
+    className: styles.button,
+    ...props,
+  });
