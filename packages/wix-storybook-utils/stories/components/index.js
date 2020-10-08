334949d1dd1dac0220490b7e5a6533fcb5af4fed
Daniel Gendelman <gendeld@gmail.com>
2020-10-08 11:42:49 +0300

    [wix-storybook-utils] Add ui button component (#1943)
    * [storybook-utils] Add ui button component
    
    * Add button example
    
    * Fix story example
    
    * Add more button examples
    
    * Fix disabled cursor
    
    * Unformat styles
    
    * Unformat ui styles
    
    * Remove unused props

--- packages/wix-storybook-utils/stories/components/index.js
+++ packages/wix-storybook-utils/stories/components/index.js
@@ -1,35 +1,36 @@
 /* eslint-disable no-alert */
-import React from "react";
-import { storiesOf } from "@storybook/react";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
 
-import Markdown from "../../src/Markdown";
-import InteractiveCodeExample from "../../src/InteractiveCodeExample";
-import CodeExample from "../../src/CodeExample";
-import Input from "../../src/ui/input";
-import SearchInput from "../../src/ui/search-input";
+import Markdown from '../../src/Markdown';
+import InteractiveCodeExample from '../../src/InteractiveCodeExample';
+import CodeExample from '../../src/CodeExample';
+import Input from '../../src/ui/input';
+import SearchInput from '../../src/ui/search-input';
+import Button from '../../src/ui/button';
 
-import markdown from "./examples/markdown.md";
-import SomeComponentExample from "./examples/Example";
-import SomeComponentExampleRaw from "!raw-loader!./examples/Example";
+import markdown from './examples/markdown.md';
+import SomeComponentExample from './examples/Example';
+import SomeComponentExampleRaw from '!raw-loader!./examples/Example';
 
-storiesOf("Components", module)
-  .add("<CodeExample/>", () => (
+storiesOf('Components', module)
+  .add('<CodeExample/>', () => (
     <div>
       Cool way to render components and see their code
-      <CodeExample title={"some code example"} code={SomeComponentExampleRaw}>
+      <CodeExample title={'some code example'} code={SomeComponentExampleRaw}>
         <SomeComponentExample />
       </CodeExample>
     </div>
   ))
 
-  .add("<Markdown/>", () => (
+  .add('<Markdown/>', () => (
     <div>
       A great way to display Markdown files
       <Markdown source={markdown} />
     </div>
   ))
 
-  .add("<InteractiveCodeExample/> ", () => (
+  .add('<InteractiveCodeExample/> ', () => (
     <div>
       This component should display a component with some code above it
       <br />
@@ -39,17 +40,40 @@ storiesOf("Components", module)
       </InteractiveCodeExample>
     </div>
   ))
-  
-  .add("<Input/>", () => (
+
+  .add('<Input/>', () => (
     <div>
       This component allows to add text input to a story
       <Input />
     </div>
   ))
-  
-  .add("<SearchInput/>", () => (
+
+  .add('<SearchInput/>', () => (
     <div>
       This component allows to add search to a story
       <SearchInput />
+    </div>
+  ))
+
+  .add('<Button/>', () => (
+    <div style={{ display: 'grid', gridAutoRows: 'auto', gap: '10px' }}>
+      <div>
+        <Button>Regular</Button>
+      </div>
+      <div>
+        <Button disabled>Disabled</Button>
+      </div>
+      <div>
+        <Button size="tiny">Tiny</Button>
+      </div>
+      <div>
+        <Button size="small">Small</Button>
+      </div>
+      <div>
+        <Button size="large">Large</Button>
+      </div>
+      <div>
+        <Button fullWidth>Full Width</Button>
+      </div>
     </div>
   ));
