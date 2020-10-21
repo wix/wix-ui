9c1e8d95a4e7c00a7704db6611ce9e047842475a
Ziv Pizanti <zivp@wix.com>
2020-10-21 13:25:05 +0300

    Fix typing bug when component is in rtl direction

--- packages/wix-ui-core/src/components/signature-input/signing-pad/SigningPad.tsx
+++ packages/wix-ui-core/src/components/signature-input/signing-pad/SigningPad.tsx
@@ -27,6 +27,7 @@ export type SigningPadOwnProps = Omit<
   penWidth?: string;
   disabled?: boolean;
   required?: boolean;
+  direction?: 'rtl' | 'ltr';
   canvasRef?(instance: HTMLCanvasElement): void;
   onInit?(padAPI: SignaturePadApi): void;
   onDrawStart?(e: MouseEvent | React.Touch | React.ChangeEvent): void;
@@ -83,6 +84,8 @@ class SigningPadComp extends React.Component<SigningPadProps, SigningPadState> {
     onInit: PropTypes.func,
     /* Is the signature pad disabled */
     disabled: PropTypes.bool,
+    /* Is the signature pad direction rtl or ltr */
+    direction: PropTypes.oneOf(['rtl', 'ltr']),
     /* Is signature mandatory in form context */
     required: PropTypes.bool,
     /* Callback to get an instance of the canvas HTML element instance */
@@ -213,7 +216,7 @@ class SigningPadComp extends React.Component<SigningPadProps, SigningPadState> {
   };
 
   handleInputChange: React.ChangeEventHandler<HTMLInputElement> = e => {
-    const { onDraw } = this.props;
+    const { onDraw, direction } = this.props;
 
     // clears drawns curves and typed characters and restore drawn curves
     if (!this.signaturePad.isEmpty() || !this.isA11yInputEmpty()) {
@@ -225,7 +228,9 @@ class SigningPadComp extends React.Component<SigningPadProps, SigningPadState> {
     this.setState(updateA11yValue(e.target.value));
     const ctx = this.canvasEl.getContext('2d');
     ctx.font = '25px Sacramento';
-    ctx.fillText(e.target.value, 20, this.canvasEl.offsetHeight / 2);
+    ctx.fillText(e.target.value,
+      direction === 'rtl' ? this.canvasEl.offsetHeight - 20 : 20,
+      this.canvasEl.offsetHeight / 2);
     this.invokeIfDefined(onDraw)(e);
   };
 
