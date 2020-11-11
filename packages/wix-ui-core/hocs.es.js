fab450df6fd52e5b49204c1a0c5cffcfa02fb105
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-11-12 01:38:58 +0200

    add missing es hocs entries

---
+++ packages/wix-ui-core/hocs.es.js
@@ -0,0 +1,5 @@
+var withEllipsedTooltip = require('./dist/es/src/hocs/EllipsedTooltip').withEllipsedTooltip;
+var withFocusable = require('./dist/es/src/hocs/Focusable').withFocusable;
+
+module.exports.withEllipsedTooltip = withEllipsedTooltip;
+module.exports.withFocusable = withFocusable;
