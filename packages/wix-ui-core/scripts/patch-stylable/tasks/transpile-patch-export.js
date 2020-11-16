d4205d6a9e5832f1ea7af80d55f188e778ff0907
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-11-16 15:13:09 +0200

    add automatically the named parts of popover to the index file

--- packages/wix-ui-core/scripts/patch-stylable/tasks/transpile-patch-export.js
+++ packages/wix-ui-core/scripts/patch-stylable/tasks/transpile-patch-export.js
@@ -2,18 +2,20 @@ const execa = require('execa');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-
-
 const appendFile = util.promisify(fs.appendFileSync);
 
+/**
+ * This is a temporary addition of internal named parts until the Stylable CLI tool will support it
+ * For this to be part of the stylesheet exports, we need to wait for this - https://github.com/wix/stylable/issues/1489 and this https://github.com/wix/stylable/issues/1484
+ */
 const addPopoverInternalParts = ({entryFile}) => {
   const popoverInternalParts = (isEs) => `
-:import {-st-from: "./dist/${isEs ? 'es/' : ''}src/components/popover/Popover.st.css";-st-named: arrow as Popover_arrow, withArrow as  as Popover_withArrow, popoverContent as Popover_popoverContent;}
-.root .Popover_arrow {}
-.root .Popover_withArrow {}
-.root .Popover_popoverContent {}
-`;
-
+  :import {-st-from: "./dist/${isEs ? 'es/' : ''}src/components/popover/Popover.st.css";-st-named: arrow as Popover_arrow, withArrow as  as Popover_withArrow, popoverContent as Popover_popoverContent;}
+  .root .Popover_arrow {}
+  .root .Popover_withArrow {}
+  .root .Popover_popoverContent {}
+  `;
+  
   return Promise.all([
     appendFile(path.resolve(`${entryFile}.st.css`), popoverInternalParts()),
     appendFile(path.resolve(`${entryFile}.es.st.css`), popoverInternalParts(true))
