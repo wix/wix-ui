ee08d81e15dbe8d98c3a659ad3c631d43f7d7afd
Arijus Šukys <arijuss@wix.com>
2020-12-08 13:10:00 +0200

    wix-ui-core: fix stylable patch script (#1978)
    * fix(patch-stylable): ensure all tasks are running
    
    * chore(): add generated updates
    
    * fix(patch-stylable): add popover internals only for index.st.css

--- packages/wix-ui-core/scripts/patch-stylable/tasks/transpile-patch-export.js
+++ packages/wix-ui-core/scripts/patch-stylable/tasks/transpile-patch-export.js
@@ -2,7 +2,7 @@ const execa = require('execa');
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-const appendFile = util.promisify(fs.appendFileSync);
+const appendFile = util.promisify(fs.appendFile);
 
 /**
  * This is a temporary addition of internal named parts until the Stylable CLI tool will support it
@@ -10,12 +10,12 @@ const appendFile = util.promisify(fs.appendFileSync);
  */
 const addPopoverInternalParts = ({entryFile}) => {
   const popoverInternalParts = (isEs) => `
-  :import {-st-from: "./dist/${isEs ? 'es/' : ''}src/components/popover/Popover.st.css";-st-named: arrow as Popover_arrow, withArrow as Popover_withArrow, popoverContent as Popover_popoverContent;}
-  .root .Popover_arrow {}
-  .root .Popover_withArrow {}
-  .root .Popover_popoverContent {}
-  `;
-  
+:import {-st-from: "./dist/${isEs ? 'es/' : ''}src/components/popover/Popover.st.css";-st-named: arrow as Popover_arrow, withArrow as Popover_withArrow, popoverContent as Popover_popoverContent;}
+.root .Popover_arrow {}
+.root .Popover_withArrow {}
+.root .Popover_popoverContent {}
+`.trim();
+
   return Promise.all([
     appendFile(path.resolve(`${entryFile}.st.css`), popoverInternalParts()),
     appendFile(path.resolve(`${entryFile}.es.st.css`), popoverInternalParts(true))
@@ -23,9 +23,8 @@ const addPopoverInternalParts = ({entryFile}) => {
 }
 
 
-module.exports = function({folder, entryFile}) {  
+module.exports = function({folder, entryFile}) {
   let entryFileCJS, entryFileES;
-
 
   const patchCJS = execa.command(
     `stc --srcDir="./src/${folder}/" --stcss=true --cjs=false --outDir="./dist/src/${folder}" --useNamespaceReference=true`,
@@ -39,13 +38,15 @@ module.exports = function({folder, entryFile}) {
     entryFileCJS = execa.command(
       `stc --srcDir=./dist/src/${folder} --indexFile=${entryFile}.st.css`,
     );
-  
+
    entryFileES = execa.command(
       `stc --srcDir=./dist/es/src/${folder} --indexFile=${entryFile}.es.st.css`,
     );
   }
 
- 
-
-  return Promise.all([patchCJS, patchES, entryFileCJS, entryFileES]).then(() => addPopoverInternalParts({entryFile}))
+  return Promise.all([patchCJS, patchES, entryFileCJS, entryFileES]).then(() => {
+    if(entryFile === 'index') {
+      return addPopoverInternalParts({entryFile})
+    }
+  })
 };
