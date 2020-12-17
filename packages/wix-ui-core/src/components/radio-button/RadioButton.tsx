8ed9ed5ae669ae1914ae1c2506037a11ab6fd572
urig-wix <73787639+urig-wix@users.noreply.github.com>
2020-12-17 11:43:08 +0200

    Update packages/wix-ui-core/src/components/radio-button/RadioButton.tsx
    Co-authored-by: Jonathan Adler <jonathana@wix.com>

--- packages/wix-ui-core/src/components/radio-button/RadioButton.tsx
+++ packages/wix-ui-core/src/components/radio-button/RadioButton.tsx
@@ -169,7 +169,7 @@ export class RadioButton extends React.Component<
 
   onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
     this.setState({ focused: true, focusVisible: !this.focusedByMouse });
-    this.props.onFocus && this.props.onFocus({ ...event });
+    this.props.onFocus && this.props.onFocus(event);
   };
 
   onInputBlur = (event: React.FocusEvent<HTMLInputElement>) => {
