71fa56ea3a9850cb0e91f978b2581e2b20e36326
Ziv Pizanti <zivp@wix.com>
2021-02-18 11:05:35 +0200

    #IE-184|AddressInput - Support externalBaseUrl for Atlas (#2039)

--- packages/wix-ui-core/src/clients/Atlas/AtlasBasicClient.spec.ts
+++ packages/wix-ui-core/src/clients/Atlas/AtlasBasicClient.spec.ts
@@ -1,4 +1,4 @@
-import { AtlasBasicClient, mockWixAtlasService } from './AtlasBasicClient'
+import { ATLAS_WEB_BASE_URL, AtlasBasicClient, mockWixAtlasService } from './AtlasBasicClient'
 import {
   getPlaceRequestMock,
   metaSiteInstaceMock,
@@ -12,8 +12,58 @@ const lang = 'en'
 const clientId = 'some-id'
 
 describe('AtlasBasicClient', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     mockWixAtlasService(WixAtlasServiceMock)
+  })
+
+  describe('atlas base url', () => {
+    const getBaseUrlAtlasMock = (baseUrl: string) => {
+      return {
+        AutocompleteServiceV2: jest.fn().mockReturnValue(
+          jest.fn().mockReturnValue({
+            predict: jest.fn().mockResolvedValue({
+              predictions: [{
+                description: baseUrl
+              }]
+            }),
+          })),
+        PlacesServiceV2: jest.fn().mockReturnValue(
+          jest.fn().mockReturnValue({
+            getPlace: jest.fn().mockResolvedValue(baseUrl),
+          })),
+      }
+    }
+
+    beforeEach(() => {
+      mockWixAtlasService(getBaseUrlAtlasMock)
+    })
+
+    it('should use externalBaseUrl if provided', async () => {
+      let result
+      const externalBaseUrl = 'some-url'
+      const client = new AtlasBasicClient({lang, instance: metaSiteInstaceMock, externalBaseUrl})
+
+      try {
+        result = await client.autocomplete(clientId, lang, {} as any)
+      }
+      catch (e) {
+        result = e
+      }
+      expect(result[0].description).toEqual(`${externalBaseUrl}${ATLAS_WEB_BASE_URL}`)
+    })
+
+    it('should use only atlas base url if no externalBaseUrl exists',async () => {
+      let result
+      const client = new AtlasBasicClient({lang, instance: metaSiteInstaceMock})
+
+      try {
+        result = await client.autocomplete(clientId, lang, {} as any)
+      }
+      catch (e) {
+        result = e
+      }
+      expect(result[0].description).toEqual(ATLAS_WEB_BASE_URL)
+    })
   })
   describe('autocomplete', () => {
     const successfulResult = [{
@@ -24,7 +74,7 @@ describe('AtlasBasicClient', () => {
 
     it('should call atlass getPlace and return a promise', async () => {
       let result
-      const client = new AtlasBasicClient(lang, metaSiteInstaceMock)
+      const client = new AtlasBasicClient({lang, instance: metaSiteInstaceMock})
       const predictRequest = {
         input: predictRequestMock.input,
         componentRestrictions: {
@@ -42,7 +92,7 @@ describe('AtlasBasicClient', () => {
 
     it('should set the country filter to atlas autocomplete request', async () => {
       let result
-      const client = new AtlasBasicClient(lang, metaSiteInstaceMock)
+      const client = new AtlasBasicClient({lang, instance: metaSiteInstaceMock})
       const predictRequest = {
         input: predictRequestMock.input,
         componentRestrictions: {
@@ -72,7 +122,7 @@ describe('AtlasBasicClient', () => {
 
     it('should call geocode and return a promise', async () => {
       let result
-      const client = new AtlasBasicClient(lang, metaSiteInstaceMock)
+      const client = new AtlasBasicClient({lang, instance: metaSiteInstaceMock})
       try {
         result = await client.geocode(clientId, lang, getPlaceRequestMock)
       }
@@ -86,7 +136,7 @@ describe('AtlasBasicClient', () => {
   describe('invalid auth headers', () => {
     it('Should fail autocomplete with invalid auth header', async () => {
       let result
-      const client = new AtlasBasicClient(lang, '')
+      const client = new AtlasBasicClient({lang, instance: ''})
       try {
         result = await client.autocomplete(clientId, lang, predictRequestMock)
       }
@@ -98,7 +148,7 @@ describe('AtlasBasicClient', () => {
 
     it('Should fail geocode with invalid auth header', async () => {
       let result
-      const client = new AtlasBasicClient(lang, '')
+      const client = new AtlasBasicClient({lang, instance: ''})
       try {
         result = await client.geocode(clientId, lang, getPlaceRequestMock)
       }
