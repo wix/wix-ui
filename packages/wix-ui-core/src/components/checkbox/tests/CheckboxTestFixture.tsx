0563f9dc32df9f091eb4ea53a2e451a1715ca7f2
Bar Boaron <barb@wix.com>
2021-02-21 17:40:25 +0200

    code refactoring

--- packages/wix-ui-core/src/components/checkbox/tests/CheckboxTestFixture.tsx
+++ packages/wix-ui-core/src/components/checkbox/tests/CheckboxTestFixture.tsx
@@ -11,55 +11,23 @@ export class CheckboxTestFixture extends React.Component {
     isOnFocusByKeyboardCalled: false,
   };
 
-  createCheckbox(props: CheckboxProps = {}) {
-    return (
-      <Checkbox
-        key={`${props.value}`}
-        checkedIcon={<CheckboxChecked />}
-        indeterminateIcon={<CheckboxIndeterminate />}
-        checked={this.state.checkedIdx === props.value}
-        onChange={() => this.setState({ checkedIdx: props.value })}
-        data-hook={`checkbox-story-${props.value}`}
-        {...props}
-      />
-    );
-  }
-
   render() {
     return (
-      <div style={{ fontSize: '20px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <h3>Checkbox</h3>
-          {['1', '2', '3', '4'].map((value) => this.createCheckbox({ value }))}
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <h3>Checkbox - disabled</h3>
-          {['unchecked', 'checked'].map((value, idx) =>
-            this.createCheckbox({
-              checked: !!idx,
-              value: '1',
-              disabled: true,
-            })
-          )}
-        </div>
-        <div>
+      <div>
           <Checkbox
             onFocusByKeyboard={() =>
               this.setState({ isOnFocusByKeyboardCalled: true })
             }
             checkedIcon={<CheckboxChecked />}
-            indeterminateIcon={<CheckboxIndeterminate />}
             onChange={() => {}}
             data-hook={`checkbox-first`}
           />
           <Checkbox
             checkedIcon={<CheckboxChecked />}
-            indeterminateIcon={<CheckboxIndeterminate />}
             onChange={() => {}}
             checked={this.state.isOnFocusByKeyboardCalled}
             data-hook={`checkbox-second`}
           />
-        </div>
       </div>
     );
   }
