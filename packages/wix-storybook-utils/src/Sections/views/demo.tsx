4df540183652b69d4265406d7ed40f2605d25800
Zivilekin <zivilek@wix.com>
2021-04-14 11:56:18 +0300

    code review changes

--- packages/wix-storybook-utils/src/Sections/views/demo.tsx
+++ packages/wix-storybook-utils/src/Sections/views/demo.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
 import { DemoSection } from '../../typings/story-section';
+import { sectionWithSiblings } from '../section-with-siblings';
+import { demo as demoConfig } from '../';
 import styles from '../styles.scss';
 
-export const demo: (props: DemoSection) => React.ReactNode = ({ demo }) => (
-  <div className={styles.demo}>{demo}</div>
+const demoView = (props: DemoSection) => (
+  <div className={styles.demo}>{props.demo}</div>
+);
+
+export const demo = (props: DemoSection) => (
+  <>
+    {sectionWithSiblings(
+      demoConfig({
+        title: props.title || 'Demo',
+      }),
+      demoView(props),
+      true,
+    )}
+  </>
 );
