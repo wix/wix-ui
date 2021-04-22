c6c119ca8b489851525656b7907cec8589028a4d
Mykolas Šindeikis <mykolass@wix.com>
2021-04-22 13:51:43 +0300

    introduce story page driver

---
+++ packages/wix-storybook-utils/src/Sections/views/story-page.driver.ts
@@ -0,0 +1,141 @@
+//import { SectionType } from '../../typings/story-section';
+import { storyPage } from './story-page';
+import { SectionType } from '../../typings/story-section';
+
+const _structure = `<div style={{ height: '100px' }}><div/><div/><div/></div>`;
+
+export const createStoryBuilder = (content?: any, config?: any) => {
+  const defaultContent = {
+    description: 'This is description',
+    do: ['this', 'that'],
+    dont: ['one', 'two'],
+    featureExamples: [
+      {
+        title: 'Structure',
+        description:
+          'Radio button consists of a radio itself and a label on the side.',
+        example: '_structure',
+      },
+    ],
+    commonUseCaseExamples: [],
+    examples: { _structure },
+  };
+
+  const defaultConfig = {
+    config: {
+      repoBaseURL: '',
+    },
+    story: defaultContent,
+    exampleImport: 'import',
+    storyName: 'Story',
+  };
+
+  const outputContent = content || defaultContent;
+  const outputConfig = config || defaultConfig;
+
+  return {
+    input: JSON.stringify(storyPage(buildInput(outputContent), outputConfig)),
+    output: JSON.stringify(buildOutput(outputContent, outputConfig)),
+  };
+};
+
+const buildInput = (content: any) => ({
+  type: SectionType.StoryPage,
+  content: {
+    description: content.description,
+    do: content.do,
+    dont: content.dont,
+    featureExamples: content.featureExamples,
+    commonUseCaseExamples: content.commonUseCaseExamples,
+  },
+  examples: content.example,
+});
+
+const buildOutput = (content: any, config: any) => {
+  const featureExamples = content.featureExamples.map((example, index) => ({
+    type: 'example',
+    key: index,
+    title: example.title,
+    text: example.description,
+    source: content.examples[example.example],
+    compact: true,
+  }));
+
+  const commonUseCaseExamples = content.commonUseCaseExamples.map(
+    (example, index) => ({
+      type: 'example',
+      key: index,
+      title: example.title,
+      text: example.description,
+      source: content.examples[example.example],
+      compact: true
+    }),
+  );
+  return [
+    { type: 'header', sourceUrl: '/Story' },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          title: 'Design',
+          type: 'tab',
+          sections: [
+            {
+              type: 'description',
+              text: content.description || 'This is description',
+              title: 'Usage',
+            },
+            {
+              type: 'doDont',
+              do: { list: content.do },
+              dont: { list: content.dont },
+            },
+            { title: 'Import', type: 'importExample', source: 'import' },
+            { type: 'divider' },
+            { type: 'title', title: 'Variations' },
+            ...featureExamples,
+            { type: 'title', title: 'Common Use Cases' },
+            ...commonUseCaseExamples,
+            { type: 'divider' },
+            { type: 'title', title: 'Feedback' },
+            { type: 'description' },
+          ],
+        },
+        { title: 'API', type: 'tab', sections: [{ type: 'api' }] },
+        { title: 'Testkit', type: 'tab', sections: [{ type: 'testkit' }] },
+      ],
+    },
+  ];
+};
+
+//addContent, addDoDont, addFeatureExamples, addCommonUseCasesExamples,  addDemo, addTabConfig
+
+// const defaultStoryProps = {
+//   type: SectionType.StoryPage,
+//   content: {},
+// };
+
+// const defaultStoryConfig = {
+//   metadata: {
+//     displayName: 'test',
+//     props: {},
+//   },
+//   config: {
+//     importFormat: '',
+//     moduleName: '',
+//     repoBaseURL: '',
+//   },
+//   storyName: 'Story',
+//   component: () => ({}),
+//   story: storyProps,
+//   exampleImport: 'import',
+// } as any;
+
+// const defaultExample = {
+//   type: 'example',
+//   key: 0,
+//   title: 'Structure',
+//   text: 'Radio button consists of a radio itself and a label on the side.',
+//   source: "<div style={{ height: '100px' }}><div/><div/><div/></div>",
+//   compact: true,
+// };
--- packages/wix-storybook-utils/src/Sections/views/story-page.test.tsx
+++ packages/wix-storybook-utils/src/Sections/views/story-page.test.tsx
@@ -1,135 +1,23 @@
-import { storyPage } from './story-page';
-
-import { SectionType } from '../../typings/story-section';
-
-const _structure = `<div style={{ height: '100px' }}><div/><div/><div/></div>`;
-
-const createStoryBuilder = (
-  storyProps?: any,
-  storyConfig?: any,
-  output?: any,
-) => {
-  const defaultStoryProps = {
-    type: SectionType.StoryPage,
-    content: {
-      description: 'This is description',
-      do: ['this', 'that'],
-      dont: ['one', 'two'],
-      featureExamples: [
-        {
-          title: 'Structure',
-          description:
-            'Radio button consists of a radio itself and a label on the side.',
-          example: '_structure',
-        },
-      ],
-      commonUseCaseExamples: [],
-    },
-    examples: { _structure },
-  };
-
-  const defaultStoryConfig = {
-    metadata: {
-      displayName: 'test',
-      props: {},
-    },
-    config: {
-      importFormat: '',
-      moduleName: '',
-      repoBaseURL: '',
-    },
-    storyName: 'Story',
-    component: () => ({}),
-    story: storyProps,
-    exampleImport: 'import',
-  } as any;
-
-  const defaultOutput = [
-    { type: 'header', sourceUrl: '/Story' },
-    {
-      type: 'tabs',
-      tabs: [
-        {
-          title: 'Design',
-          type: 'tab',
-          sections: [
-            {
-              type: 'description',
-              text: 'This is description',
-              title: 'Usage',
-            },
-            {
-              type: 'doDont',
-              do: { list: ['this', 'that'] },
-              dont: { list: ['one', 'two'] },
-            },
-            { title: 'Import', type: 'importExample', source: 'import' },
-            { type: 'divider' },
-            { type: 'title', title: 'Variations' },
-            {
-              type: 'example',
-              key: 0,
-              title: 'Structure',
-              text:
-                'Radio button consists of a radio itself and a label on the side.',
-              source:
-                "<div style={{ height: '100px' }}><div/><div/><div/></div>",
-              compact: true,
-            },
-            { type: 'title', title: 'Common Use Cases' },
-            { type: 'divider' },
-            { type: 'title', title: 'Feedback' },
-            { type: 'description' },
-          ],
-        },
-        { title: 'API', type: 'tab', sections: [{ type: 'api' }] },
-        { title: 'Testkit', type: 'tab', sections: [{ type: 'testkit' }] },
-      ],
-    },
-  ];
-  const props = storyProps || defaultStoryProps;
-  const config = storyConfig || defaultStoryConfig;
-  let result = output || defaultOutput;
-  return {
-    input: JSON.stringify(storyPage(props, config)),
-    output: JSON.stringify(result),
-    addFeatureExample: (example: any) => {
-      storyProps = storyProps.content.featureExamples = [example] as any;
-
-      const modifiedTabs = result[1].tabs.map(tab => {
-        if (tab.title === 'Design') {
-          return tab.sections.findIndex(section => section.type);
-        }
-
-        return tab;
-      });
-      result = [result[0], modifiedTabs];
-      return createStoryBuilder(storyProps, defaultStoryConfig, result);
-    },
-  };
-};
-
-//addContent, addDoDont, addFeatureExamples, addCommonUseCasesExamples,  addDemo, addTabConfig
+import { createStoryBuilder } from './story-page.driver';
 
 describe('StoryPage', () => {
-  // it('should render simple storyPage', () => {
-  //   const { input, output } = createStoryBuilder();
-
-  //   expect(input).toBe(output);
-  // });
-
-  // it('should render story page with given feature example', () => {
-  //   const featureExample = {
-  //     title: 'title',
-  //     description: 'description',
-  //     example: '<div/>',
-  //     layout: 'lanscape',
-  //   };
-
-  //   const { input, output } = createStoryBuilder().addFeatureExample(
-  //     featureExample,
-  //   );
-
-  //   expect(input).toBe(output);
-  // });
+  it('should render simple storyPage', () => {
+    const { input, output } = createStoryBuilder();
+    expect(input).toBe(output);
+  });
 });
+
+// it('should render story page with given feature example', () => {
+//   const featureExample = {
+//     title: 'title',
+//     description: 'description',
+//     example: '<div/>',
+//     layout: 'lanscape',
+//   };
+
+//   const { input, output } = createStoryBuilder().addFeatureExample(
+//     featureExample,
+//   );
+
+//   expect(input).toBe(output);
+// });
