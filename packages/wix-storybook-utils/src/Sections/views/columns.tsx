d58c991bfb2224c80aecd94ddf02767f298124d6
Mykolas Šindeikis <mykolass@wix.com>
2021-04-23 15:01:46 +0300

    Fix code section not getting storyconfig when used inside column

--- packages/wix-storybook-utils/src/Sections/views/columns.tsx
+++ packages/wix-storybook-utils/src/Sections/views/columns.tsx
@@ -5,13 +5,17 @@ import { isTab } from '../extract-tabs';
 import { sectionWithSiblings } from '../section-with-siblings';
 
 import styles from './styles.scss';
+import { StoryConfig } from '../../typings/story-config';
 
-const renderColumn = column => {
-  const view = getView(column.type)(column);
+const renderColumn = (column, storyConfig) => {
+  const view = getView(column.type)(column, storyConfig);
   return isTab(column) ? view : sectionWithSiblings(column, view);
 };
 
-export const columns: (a: ColumnsSection) => React.ReactNode = ({ items }) => {
+export const columns: (a: ColumnsSection, b: StoryConfig) => React.ReactNode = (
+  { items },
+  storyConfig,
+) => {
   if (!items.length) {
     return null;
   }
@@ -19,7 +23,7 @@ export const columns: (a: ColumnsSection) => React.ReactNode = ({ items }) => {
   return (
     <div className={styles.layout}>
       {items.map((column, i) => (
-        <div key={`col-${i}`}>{renderColumn(column)}</div>
+        <div key={`col-${i}`}>{renderColumn(column, storyConfig)}</div>
       ))}
 
       {items.length === 1 && <div />}
