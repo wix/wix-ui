aa6fca6dde9a13cb306bc72d997667c6607767d0
egidijusja <egidijusja@wix.com>
2021-04-27 13:40:52 +0300

    added wix storybook webpack plugin

--- packages/wix-storybook-utils/.storybook/webpack.config.js
+++ packages/wix-storybook-utils/.storybook/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const wixStorybookConfig = require('@wix/yoshi/config/webpack.config.storybook');
+const WixStorybookWebpackPlugin = require('./wixStorybookWebpackPlugin');
 
 const makeTestkitTemplate = platform =>
   `import { <%= utils.toCamel(component.displayName) %>TestkitFactory } from 'wix-style-react/dist${platform}';`;
@@ -11,15 +12,9 @@ const testkitsWarning = `
 module.exports = ({ config }) => {
   const newConfig = wixStorybookConfig(config);
 
-  newConfig.resolve.alias = {
-    ...newConfig.resolve.alias,
-    'wix-storybook-utils': path.resolve(__dirname, '..', 'src'),
-  };
-
-  newConfig.module.rules.push({
+  config.plugins.push(new WixStorybookWebpackPlugin({
     test: /\.story\.[j|t]sx?$/,
-    loader: './src/loader',
-    options: {
+    loaderOptions: {
       storyConfig: {
         importFormat: "import { %componentName } from '%moduleName'",
         moduleName: 'wix-storybook-utils',
@@ -45,7 +40,12 @@ module.exports = ({ config }) => {
         feedbackText: 'You can help us improve this component by providing feedback, asking questions or leaving any  other comments via `#wix-style-ux` or `#wix-style-react` Slack channels or GitHub. Found a bug? Please report it to: <a href="https://goo.gl/forms/wrVuHnyBrEISXUPF2" target="_blank">goo.gl/forms/wrVuHnyBrEISXUPF2</a>'
       },
     },
-  });
+  }));
+
+  newConfig.resolve.alias = {
+    ...newConfig.resolve.alias,
+    'wix-storybook-utils': path.resolve(__dirname, '..', 'src'),
+  };
 
   return newConfig;
 };
---
+++ packages/wix-storybook-utils/.storybook/wixStorybookWebpackPlugin.js
@@ -0,0 +1,23 @@
+class WixStorybookWebpackPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    const { test, loaderOptions } = this.options;
+
+    compiler.hooks.compilation.tap('WixStorybookWebpackPlugin', compilation => {
+      compilation.hooks.normalModuleLoader.tap('WixStorybookWebpackPlugin', (_, module) => {
+        if (test.test(module.userRequest)) {
+          module.loaders.push({
+            loader: require.resolve('../src/loader/index.js'),
+            options: loaderOptions,
+          });
+        }
+      });
+    });
+  }
+}
+
+module.exports = WixStorybookWebpackPlugin;
+
