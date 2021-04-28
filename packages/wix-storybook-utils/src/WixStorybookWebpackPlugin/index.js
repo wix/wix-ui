695927475c7b8936049a62aaa1caee62453764fc
egidijusja <egidijusja@wix.com>
2021-04-28 16:45:10 +0300

    added webpack plugin for storybook utils

--- packages/wix-storybook-utils/src/WixStorybookWebpackPlugin/index.js
+++ packages/wix-storybook-utils/src/WixStorybookWebpackPlugin/index.js
@@ -1,4 +1,7 @@
+const { default: InjectPlugin, ENTRY_ORDER } = require('webpack-inject-plugin');
+
 const storyPattern = /\.story\.[j|t]sx?$/;
+const propTypesParserInjector = () => "import 'wix-storybook-utils/propTypesParser'; console.log('smth')";
 
 class WixStorybookWebpackPlugin {
   constructor(options) {
@@ -6,18 +9,23 @@ class WixStorybookWebpackPlugin {
   }
 
   apply(compiler) {
+    const { storyConfig } = this.options;
 
     compiler.hooks.compilation.tap('WixStorybookWebpackPlugin', compilation => {
+
       compilation.hooks.normalModuleLoader.tap('WixStorybookWebpackPlugin', (_, module) => {
+
         if (storyPattern.test(module.userRequest)) {
           module.loaders.push({
             loader: require.resolve('../loader/index.js'),
-            options: { storyConfig: this.options },
+            options: { storyConfig },
           });
         }
       });
     });
+
+    new InjectPlugin(propTypesParserInjector, { entryOrder: ENTRY_ORDER.First }).apply(compiler);
   }
 }
-  
+
 module.exports = WixStorybookWebpackPlugin;
---
+++ packages/wix-storybook-utils/src/propTypesParser/index.js
@@ -0,0 +1,1 @@
+export { default } from 'parse-prop-types';
