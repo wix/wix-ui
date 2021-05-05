f32cbd7507dd3fd4f2bea108f9363d4d67c9263b
bamai <57489465+bamai@users.noreply.github.com>
2021-05-05 17:29:34 +0300

    [wix-ui-icons-common] - add wix-ui-tpa icons (#2094)
    * add svg files
    
    * rename svg files
    
    * add metadata
    
    * remove scv file
    
    * fix storybook page
    
    * update metadata
    
    * add tests and update storybook
    
    * small fixes
    
    * fix build
    
    * fix tpaCategoryList
    
    * fix storybook styles and small errors
    
    * update storybook sizes
    
    * add Social category and reorganize storybook
    
    * update table titles
    
    * update Social category
    
    * update tsconfing

---
+++ packages/wix-ui-icons-common/stories/wix-ui-tpa/WixUiTpaCategoryList.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import CategoryList from "../components/category-list";
+import * as iconComponents from '../../src/wix-ui-tpa/dist';
+import tpaIconsMetadata from '../../src/wix-ui-tpa/metadata';
+import { IconMetadata } from "../../src/types";
+import { TpaTableRow } from '../types';
+import { classes } from "./WixUiTpaCategoryList.st.css";
+
+const tableHeaderTitles = [
+  "16x16",
+  "Icon Name",
+  "12x12",
+  "Icon Name",
+  "20x20",
+  "Icon Name",
+  "8x8",
+  "Icon Name",
+  "48x48",
+  "Icon Name",
+  "6x6",
+  "Icon Name",
+  "Use for",
+];
+
+const searchKeys = ["title", "sizes.20", "sizes.24", "sizes.30", "tags", "aliases"];
+
+const WixUiTpaCategoryList: React.FC = () => {
+
+  const mapIconToRow = (
+    {
+      description,
+      sizes,
+    }: IconMetadata): TpaTableRow => {
+    const Icon = iconComponents[sizes[24]]
+    const SmallIcon = iconComponents[sizes[20]]
+    const LargeIcon = iconComponents[sizes[30]]
+    const XSmallIcon = iconComponents[sizes[16]]
+    const XLargeIcon = iconComponents[sizes[60]]
+    const XXSmallIcon = iconComponents[sizes[12]]
+    return [
+      Icon && <Icon/>,
+      sizes[24],
+      SmallIcon && <SmallIcon/>,
+      sizes[20],
+      LargeIcon && <LargeIcon/>,
+      sizes[30],
+      XSmallIcon && <XSmallIcon/>,
+      sizes[16],
+      XLargeIcon && <XLargeIcon/>,
+      sizes[60],
+      XXSmallIcon && <XXSmallIcon/>,
+      sizes[12],
+      description,
+    ]
+  }
+
+  return (
+    <CategoryList
+      dataHook="icon-list"
+      className={classes.tableList}
+      iconsMetadata={tpaIconsMetadata}
+      {...{
+        tableHeaderTitles,
+        searchKeys,
+        mapIconToRow,
+      }}
+    />
+  )
+};
+export default WixUiTpaCategoryList
---
+++ packages/wix-ui-icons-common/stories/wix-ui-tpa/index.story.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import {
+  header,
+  tabs,
+  tab,
+  description,
+  importExample,
+  title,
+  divider,
+} from "wix-storybook-utils/Sections";
+import IconsExample from "../components/icons-example";
+import * as iconComponents from "../../src/wix-ui-tpa/dist";
+import API_Table from "../APITable";
+import WixUiTpaCategoryList from './WixUiTpaCategoryList'
+import { WUT_CATEGORY } from '../constants'
+
+export default {
+  category: WUT_CATEGORY,
+  storyName: WUT_CATEGORY,
+  component: () => (
+    <IconsExample dataHook="icon-list" {...{ iconComponents }} />
+  ),
+
+  sections: [
+    header({
+      sourceUrl:
+        "https://github.com/wix/wix-ui/tree/master/packages/wix-ui-icons-common",
+    }),
+
+    tabs([
+      tab({
+        title: "Icon List",
+        sections: [
+          importExample(
+            "import Duplicate from 'wix-ui-icons-common/wix-ui-tpa/Duplicate';"
+          ),
+          divider(),
+          title("Categories"),
+          description({
+            text:
+              "The usage of each icon type is determined by intention and size. Icons should be used strictly according to the description.",
+          }),
+          <WixUiTpaCategoryList />,
+        ],
+      }),
+      tab({
+        title: "API",
+        sections: [description({ title: "Props", text: API_Table })],
+      }),
+    ]),
+  ],
+};
