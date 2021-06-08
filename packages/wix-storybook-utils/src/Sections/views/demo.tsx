6a20c60d9fd2c8aba634c65a9537400f24cdfee2
egidijusja <egidijusja@wix.com>
2021-06-08 14:03:23 +0300

    fixed performance issue for demo section (#2121)

--- packages/wix-storybook-utils/src/Sections/views/demo.tsx
+++ packages/wix-storybook-utils/src/Sections/views/demo.tsx
@@ -3,8 +3,11 @@ import { DemoSection } from '../../typings/story-section';
 import styles from '../styles.scss';
 
 export const demo = (props: DemoSection) => {
-  const component =
-    typeof props.component === 'function' ? props.component() : props.component;
+  const DemoComponent = props.component;
 
-  return <div className={styles.demo}>{component}</div>;
+  return (
+    <div className={styles.demo}>
+      {typeof DemoComponent === 'function' ? <DemoComponent /> : DemoComponent}
+    </div>
+  );
 };
