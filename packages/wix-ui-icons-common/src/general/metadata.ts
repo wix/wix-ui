c826f47defd11b9f6c06f9313f402a1a4e5560ee
ZilvinasMaciulis <44393332+ZilvinasMaciulis@users.noreply.github.com>
2021-07-01 11:10:22 +0300

    User Search | New Icon (#2132)
    * User Search | New Icon
    
    * Update metadata.ts
    
    * Update metadata.ts
    
    * Update metadata.ts
    
    * Update metadata.ts
    
    * Update metadata.ts
    
    * Update metadata.ts
    
    Co-authored-by: Živilė Kindurytė <zivilek@wix.com>

--- packages/wix-ui-icons-common/src/general/metadata.ts
+++ packages/wix-ui-icons-common/src/general/metadata.ts
@@ -1107,6 +1107,14 @@ sizes: { "18": "GetStartedSmall", "24": "GetStarted" },
     tags: ["permission"],
     sizes: { "18": "AdminAccessSmall", "24": "AdminAccess" },
   },
+	{
+    title: "UserSearch",
+    category: "Users",
+    description:
+      "Static indicator - marks that background checke was conducted on user. Button - search for user",
+    tags: ["contact, candidate"],
+    sizes: { "18": "UserSearchSmall", "24": "UserSearch" },
+  },
   {
     title: "DateAndTime",
     category: "Date & Time",
