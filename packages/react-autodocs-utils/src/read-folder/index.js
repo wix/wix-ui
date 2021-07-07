d5a59ed6992c9d67b90156a600927b3b3de1df09
argshook <argshook@gmail.com>
2021-07-07 12:56:14 +0300

    support `recursive` option in read-folder

--- packages/react-autodocs-utils/src/read-folder/index.js
+++ packages/react-autodocs-utils/src/read-folder/index.js
@@ -1,11 +1,33 @@
 /* global Promise */
 
-const { readdir } = require('fs');
+const { readdir } = require('fs').promises;
+const path = require('path');
 const dirname = require('../dirname');
-const promise = require('../promises/promise');
 
-const promiseReaddir = promise(readdir);
+const defaultOptions = {
+  recursive: false,
+};
 
-const readFolder = path => promiseReaddir(dirname(path), 'utf8');
+const readFolder = (rootPath, options = defaultOptions) => {
+  const dirnameOfRoot = dirname(rootPath);
+  const recursion = async (folderPath) => {
+    const pathToRead = path.join(dirnameOfRoot, folderPath);
+    const entries = await readdir(pathToRead, { encoding: 'utf8', withFileTypes: true });
+
+    if (options.recursive) {
+      const files = await Promise.all(
+        entries.map((entry) => {
+          const resolvedPath = path.join(folderPath, entry.name);
+          return entry.isDirectory() ? recursion(resolvedPath, { ...options, recursive: true }) : resolvedPath;
+        })
+      );
+      return files.flat();
+    }
+
+    return entries.map((entry) => entry.name);
+  };
+
+  return recursion('.');
+};
 
 module.exports = readFolder;
